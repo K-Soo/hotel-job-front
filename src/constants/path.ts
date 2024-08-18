5e8d0cb19f896bd143bbb3d8152f55984588f843
K-Soo <kanabun102@naver.com>
2024-08-18 12:45:48 +0900

    feat: constants/path

---
+++ src/constants/path.ts
@@ -0,0 +1,38 @@
+const path = {
+  HOME: "/", //홈
+
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up", //기업 가입
+  SIGN_UP_COMPLETE: "/sign-up/complete",
+
+  TALENT_POOL: "/talent", // 인재정보 메인 페이지
+
+  TALENT_POOL_HOTEL: "/talent/hotel", // 호텔 인재정보
+  TALENT_POOL_PENSION: "/talent/pension", // 펜션 인재정보
+  TALENT_POOL_PART_TIME: "/talent/part-time", // 알바 or 파출부 인재정보
+
+  SEARCH: "/search",
+
+  CHECKOUT: "/checkout",
+
+  ACCOUNT: "/account", //내 정보
+
+  SERVICE_QNA: "/account/qna", //1:1문의
+  SERVICE_QNA_CREATE: "/account/qna/create", //1:1문의 등록
+
+  SERVICE_LAUNCHING: "/service/launching", //입점/제휴 문의
+  SERVICE_NOTICE: "/service/notice", //공지사항
+  SERVICE_FAQ: "/service/faq", //FAQ
+
+  SERVICE_TERMS_AGREEMENT: "/service/terms/agreement", //이용약관
+  SERVICE_TERMS_PRIVACY: "/service/terms/privacy", //개인 정보 취급방침
+
+  ACCOUNT_RECOVER_USER: "/account/recover/user", //이메일 찾기
+  ACCOUNT_RECOVER_RESET: "/account/recover/reset", //비밀번호 찾기
+  ACCOUNT_RECOVER_RESET_SUCCESS: "/account/recover/reset/success", //비밀번호 찾기 결과
+
+  ACCOUNT_POINT: "/account/point", //포인트
+  ACCOUNT_USER: "/account/user", //회원정보 수정
+} as const;
+
+export default path;
