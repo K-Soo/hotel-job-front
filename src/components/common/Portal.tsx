bdcff0450b8cbff3809666abe5b82b2d0ac252cb
K-Soo <kanabun102@naver.com>
2024-12-10 12:40:26 +0900

    feat: Add Portal component for rendering children into a DOM node

---
+++ src/components/common/Portal.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import dynamic from "next/dynamic";
+
+interface IPortal {
+  children: React.ReactNode;
+}
+
+const Portal = ({ children }: IPortal) => {
+  if (window && typeof window !== "object") {
+    return <></>;
+  }
+
+  const element = document.getElementById("portal");
+
+  if (!element) {
+    return <></>;
+  }
+
+  return element && children ? ReactDOM.createPortal(children, element) : null;
+};
+
+export default dynamic(() => Promise.resolve(Portal), {
+  ssr: false,
+});
