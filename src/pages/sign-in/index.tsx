49ed354bec9e25501b4675452aeef4dbe36c2114
K-Soo <kanabun102@naver.com>
2024-12-10 12:41:18 +0900

    feat: Add layout wrapper to SignInPage for consistent page structure

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,5 +1,14 @@
+import Layout, { Header, Footer, Main } from "@/components/layout";
 import SignInContainer from "@/containers/signInContainer";
 
 export default function SignInPage() {
   return <SignInContainer />;
 }
+
+SignInPage.getLayout = (page: React.ReactElement) => {
+  return (
+    <Layout>
+      <Main>{page}</Main>
+    </Layout>
+  );
+};
