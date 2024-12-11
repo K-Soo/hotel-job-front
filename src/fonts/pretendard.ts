26bdc07ffd23c4dfaaf75e6e8136ebcaa9fb20e3
K-Soo <kanabun102@naver.com>
2024-12-11 12:55:30 +0900

    feat: Add Pretendard font integration for improved typography

---
+++ src/fonts/pretendard.ts
@@ -0,0 +1,9 @@
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  // src: "../fonts/PretendardVariable.woff2",
+  src: '../../public/fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+});
+
+export default pretendard;
