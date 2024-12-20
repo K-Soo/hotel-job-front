8509826049e33c92482c07f387fcea6fc8829875
K-Soo <kanabun102@naver.com>
2024-12-20 19:20:29 +0900

    feat: Add AppThemeProvider component for theming support

---
+++ src/styles/AppThemeProvider.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import media from './media';
+import colors from './colors';
+
+interface ThemeProps {
+  children: React.ReactNode;
+}
+
+const theme: DefaultTheme = {
+  colors,
+  media,
+};
+
+export default function Theme({ children }: ThemeProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+    </ThemeProvider>
+  );
+}
