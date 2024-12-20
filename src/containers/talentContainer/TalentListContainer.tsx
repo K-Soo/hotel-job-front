239f2c0893c777aaf5569cdd6c201ef8999c4445
K-Soo <kanabun102@naver.com>
2024-12-20 19:24:27 +0900

    feat: TalentListContainer에 useFetchQuery 훅 추가 및 데이터 가져오기 설정

--- src/containers/talentContainer/TalentListContainer.tsx
+++ src/containers/talentContainer/TalentListContainer.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React from 'react';
+import TalentList from '@/components/talent/TalentList';
+import useFetchQuery from '@/hooks/useFetchQuery';
+import { Get } from '@/apis';
+import queryKeys from '@/constants/queryKeys';
 
 export default function TalentListContainer() {
-  return <>TalentListContainer</>;
+  const {} = useFetchQuery({
+    queryKey: [queryKeys.TALENT_LIST],
+    queryFn: Get.getTalentList,
+    options: {
+      enabled: true,
+      // throwOnError: true,
+      // staleTime: 1000 * 60 * 5,
+      // gcTime: 1000 * 60 * 10,
+      staleTime: 0,
+      gcTime: 0,
+    },
+    requestQuery: {
+      page: '1',
+      limit: '2',
+    },
+  });
+
+  return <TalentList />;
 }
