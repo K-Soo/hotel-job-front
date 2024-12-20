34814c0bb7dd55bc536cf886620ed8fe6ccea70c
K-Soo <kanabun102@naver.com>
2024-12-20 19:22:07 +0900

    feat: yup을 사용하여 스키마 유효성 검사 추가

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,1 +1,11 @@
+import * as yup from 'yup';
+
 export const schema = {};
+
+const schemas = yup
+  .object()
+  .shape({
+    name: yup.string().required(),
+    age: yup.number().required(),
+  })
+  .required();
