ec2fbeae96dcc8cdc7553e0b73cdac109cf7308a
K-Soo <kanabun102@naver.com>
2024-12-20 19:23:48 +0900

    feat: TalentContainer에 TalentFilter 컴포넌트 추가

--- src/containers/talentContainer/index.tsx
+++ src/containers/talentContainer/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import Talent from "@/components/talent";
-import TalentListContainer from "@/containers/talentContainer/TalentListContainer";
+import React from 'react';
+import Talent from '@/components/talent';
+import TalentListContainer from '@/containers/talentContainer/TalentListContainer';
+import TalentFilter from '@/components/talent/TalentFilter';
 
 export default function TalentContainer() {
   return (
-    <>
-      <Talent />
+    <Talent>
+      <TalentFilter />
       <TalentListContainer />
-    </>
+    </Talent>
   );
 }
