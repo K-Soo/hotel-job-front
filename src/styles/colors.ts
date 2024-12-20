aa384852c74ece5189ea8940acd87845b62e41d4
K-Soo <kanabun102@naver.com>
2024-12-20 19:24:54 +0900

    feat: 색상 변수 추가 및 colors.ts 파일 생성

---
+++ src/styles/colors.ts
@@ -0,0 +1,21 @@
+const colors = {
+  blue100: '#3182F7', //default
+  blue200: '#2272ec', //active
+  blue300: 'rgba(100, 167, 255, 0.69)', //disabled
+
+  white100: '#FFFFFF', // 밝은 배경
+  black100: '#212529', // 어두운 배경
+
+  gray: '#f9fafb',
+  gray100: '#f2f4f6',
+  gray200: '#e5e8eb',
+  gray300: '#d1d6db',
+  gray400: '#b0b8c1',
+  gray500: '#8b95a1',
+  gray600: '#6b7684',
+  gray700: '#4e5968',
+  gray800: '#333d4b',
+  gray900: '#191f28',
+};
+
+export default colors;
