7d47e09f80d769a890e807ba6f57421a642a7f5e
K-Soo <kanabun102@naver.com>
2024-12-20 19:20:35 +0900

    style: Update GlobalStyles for improved layout and accessibility

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -2,28 +2,37 @@ import { createGlobalStyle } from 'styled-components';
 import Reset from 'styled-reset';
 import pretendard from '@/fonts/pretendard';
 
+// BottomNavigation z-index: 5;
 const GlobalStyle = createGlobalStyle`
   ${Reset}
 
   /* CSS Variables */
   :root {
-    --background-color: #f7fafc;
+    --background-color: #ffffff;
     --text-color: #171717;
     --font-family: ${pretendard.style.fontFamily}, Arial, sans-serif;
   }
 
   /* Global Element Styles */
-  * {
+  *, *::before, *::after {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
 
-  /* Structural Element Styles */
+  html {
+    overflow-x: hidden;
+  }
   body {
     background-color: var(--background-color);
     color: var(--text-color);
     font-family: var(--font-family);
+    overflow-x: hidden;
+  }
+
+  button {
+    all: unset;
+    box-sizing: border-box;
   }
 
   a {
