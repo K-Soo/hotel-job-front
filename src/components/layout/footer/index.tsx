2da0fcbf0d017f78e9511357cc9cf3d9314dc188
K-Soo <kanabun102@naver.com>
2024-12-20 19:21:48 +0900

    feat: Footer 컴포넌트 추가 및 스타일 개선

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,17 +1,84 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Link from 'next/link';
+import path from '@/constants/path';
 
-interface FooterProps {}
-
-export function Footer({}: FooterProps) {
-  return <S.Footer>Footer</S.Footer>;
+export function Footer() {
+  return (
+    <S.Footer>
+      <div className="container">
+        <S.Category>
+          <li className="item">회사소개</li>
+          <li className="item">
+            <Link href={path.SUPPORT_NOTICE} prefetch={false}>
+              고객센터
+            </Link>
+          </li>
+          <li className="item">
+            <Link href={path.POLICY_TERMS} prefetch={false}>
+              서비스 이용약관
+            </Link>
+          </li>
+          <li className="item">
+            <Link href={path.POLICY__PRIVACY} prefetch={false}>
+              개인정보 처리방침
+            </Link>
+          </li>
+        </S.Category>
+        <S.Information>
+          <p className="item">이메일: kanabun607@gmail.com</p>
+          <p className="item">사업자등록번호: 107-37-90473 | 대표자: 고원호</p>
+          <p className="item">통신판매업 신고번호: 2024-12-11222</p>
+          <p className="item">경기도 남양주시 두물로27번길 17</p>
+          <strong className="item company">COPYRIGHT ⓒ HOTEL JOB ALL RIGHTS RESERVED.</strong>
+        </S.Information>
+      </div>
+    </S.Footer>
+  );
 }
 
 const S = {
   Footer: styled.footer`
-    margin-top: 50px;
-    height: 100px;
-    background-color: #fff;
-    border: 1px solid rgba(0, 0, 0, 0.08);
-    background-color: black;
+    background-color: ${(props) => props.theme.colors.gray};
+    width: 100%;
+    padding: 50px 0 50px 0;
+    .container {
+      max-width: 1024px;
+      width: 100%;
+      height: 100%;
+      margin: 0 auto;
+      ${(props) => props.theme.media.tablet`
+      padding: 0 15px;
+    `};
+    }
+  `,
+  Category: styled.ul`
+    display: flex;
+    margin: 0 auto;
+    font-size: 13px;
+    margin-bottom: 20px;
+    .item {
+      cursor: pointer;
+      padding-right: 30px;
+      color: ${(props) => props.theme.colors.gray600};
+      &:last-child {
+        padding-right: 0;
+      }
+      &:hover {
+        text-decoration: underline;
+        color: ${(props) => props.theme.colors.black100};
+      }
+    }
+  `,
+  Information: styled.div`
+    font-size: 13px;
+    .item {
+      padding: 2px 0;
+      color: ${(props) => props.theme.colors.gray600};
+    }
+    .company {
+      display: inline-block;
+      margin-top: 5px;
+      color: ${(props) => props.theme.colors.gray900};
+    }
   `,
 };
