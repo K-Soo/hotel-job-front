c880957e45c2feba616adc1e7405ec1ac8f7d52b
K-Soo <kanabun102@naver.com>
2024-12-20 19:17:50 +0900

    fix: Update string quotes and improve error handling in interceptorHelper

--- src/apis/interceptorHelper.ts
+++ src/apis/interceptorHelper.ts
@@ -1,19 +1,20 @@
-import axios, { AxiosResponse } from "axios";
-import { instance, Post } from "@/apis";
+import axios, { AxiosResponse } from 'axios';
+import { instance, Post } from '@/apis';
 
-const ALLOW_AUTHENTICATION_PATH = ["/auth/sign-in", "/oauth/kakao", "/auth/refresh"];
+const ALLOW_AUTHENTICATION_PATH = ['/auth/sign-in', '/oauth/kakao', '/auth/refresh'];
 
 const handleSuccessResponse = (config: AxiosResponse) => {
   if (!config.config?.url) {
     return;
   }
+  console.log('config.config?.url: ', config.config?.url);
   // 지정된 API 경로에 대해서만 토큰을 설정합니다.
   if (ALLOW_AUTHENTICATION_PATH.includes(config.config.url)) {
     const accessToken = config.data?.result?.accessToken;
     if (!accessToken) {
-      throw new Error("Access token is missing");
+      throw new Error('Access token is missing');
     }
-    instance.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    instance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
   }
 };
 
@@ -24,19 +25,19 @@ const handleRequestAccessToken = async (originalRequest: any) => {
       throw new Error();
     }
     const newAccessToken = response.result.accessToken;
-    instance.defaults.headers.common["Authorization"] = `Bearer ${newAccessToken}`;
-    originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+    instance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
+    originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
     return instance(originalRequest);
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      console.error("refresh-token error : ", error.response?.data);
+      console.error('refresh-token error : ', error.response?.data);
     }
   }
 };
 
 const handleInvalidRefreshToken = () => {
-  alert("로그인 정보가 만료되었습니다.");
-  window.location.href = "/sign-in";
+  alert('로그인 정보가 만료되었습니다.');
+  window.location.href = '/sign-in';
 };
 
 export const interceptorHelper = { handleSuccessResponse, handleRequestAccessToken, handleInvalidRefreshToken };
