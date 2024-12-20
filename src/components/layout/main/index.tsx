a511cdc427e10a3c7b85914805c7df779e85bacf
K-Soo <kanabun102@naver.com>
2024-12-20 19:20:22 +0900

    feat: Add maxWidth prop to Main component for flexible layout

--- src/components/layout/main/index.tsx
+++ src/components/layout/main/index.tsx
@@ -1,22 +1,27 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 interface MainProps {
+  maxWidth?: string;
   children: React.ReactNode;
 }
 
-export function Main({ children }: MainProps) {
-  return <S.Main>{children}</S.Main>;
+export function Main({ maxWidth, children }: MainProps) {
+  return <S.Main $maxWidth={maxWidth}>{children}</S.Main>;
 }
 
 const S = {
-  Main: styled.main`
+  Main: styled.main<{ $maxWidth?: string }>`
     flex: 1;
     height: 100%;
-    max-width: 1080px;
+    max-width: ${(props) => (props.$maxWidth ? props.$maxWidth : '1024px')};
     width: 100%;
     display: flex;
     flex-direction: column;
     margin: 0 auto;
+    padding-top: 30px;
+    ${(props) => props.theme.media.tablet`
+      padding: 30px 15px 0 15px;
+    `};
     & > section {
       flex: 1;
     }
