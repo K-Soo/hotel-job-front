806f1b4aefb12844f26c2acfc39fb99f5201e818
K-Soo <kanabun102@naver.com>
2024-12-20 19:21:13 +0900

    feat: Consent 컴포넌트에 체크박스 추가 및 상태 변경 핸들러 구현

--- src/components/common/Consent.tsx
+++ src/components/common/Consent.tsx
@@ -1,9 +1,24 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Radio from '@/components/common/style/Radio';
+import CheckBox from '@/components/common/style/CheckBox';
 
 interface ConsentProps {}
 
 export default function Consent({}: ConsentProps) {
-  return <S.Consent>Consent</S.Consent>;
+  const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    console.log('name: ', name);
+    console.log('checked: ', checked);
+  };
+
+  return (
+    <S.Consent>
+      <CheckBox name="allAgree" onChange={handleChangeCheckbox} label="전체 동의" margin="5px" />
+      <CheckBox name="personalInfoAgree" onChange={handleChangeCheckbox} label="서비스이용 동의" margin="5px" />
+      <CheckBox name="serviceTermsAgree" onChange={handleChangeCheckbox} label="개인정보 수집동의" margin="5px" />
+      <CheckBox name="marketingAgree" onChange={handleChangeCheckbox} label="마케팅 동의" margin="5px" />
+    </S.Consent>
+  );
 }
 
 const S = {
