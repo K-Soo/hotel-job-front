277404931d44d38267f9728e9a3750e20488f8d2
K-Soo <kanabun102@naver.com>
2024-12-23 03:23:14 +0900

    feat: 인증 컴포넌트에서 Recoil 대신 커스텀 훅 사용으로 코드 개선

--- src/auth/AuthenticationComponent.tsx
+++ src/auth/AuthenticationComponent.tsx
@@ -1,20 +1,19 @@
-import React from "react";
-import { Internal, Post } from "@/apis";
-import { useSetRecoilState, useRecoilValue } from "recoil";
-import { authAtom, authSelector } from "@/recoil/auth";
-import { useRouter } from "next/router";
-import { EXCLUDED_PATHS } from "@/constants/path";
+import React from 'react';
+import { Internal, Post } from '@/apis';
+import { useRouter } from 'next/router';
+import { EXCLUDED_PATHS } from '@/constants/path';
+import useAuth from '@/hooks/useAuth';
 
 export default function AuthenticationComponent() {
-  const authSelectorValue = useRecoilValue(authSelector);
-  const setAuthAtom = useSetRecoilState(authAtom);
   const router = useRouter();
+
+  const { isAuthenticated, setAuthAtomState } = useAuth();
 
   React.useEffect(() => {
     if (EXCLUDED_PATHS.includes(router.pathname)) {
       return;
     }
-    if (authSelectorValue.isLogin) {
+    if (isAuthenticated) {
       return;
     }
     (async () => {
@@ -22,21 +21,21 @@ export default function AuthenticationComponent() {
         const cookieExist = await Internal.checkRefreshCookie();
         if (!cookieExist) return;
 
-        const response = await Post.getUserInfo({});
-        console.log("GLOBAL 유저정보 API : ", response);
+        const response = await Post.getUserInfo();
+        console.log('유저정보 API : ', response);
         if (!response.success) {
           throw new Error();
         }
-        setAuthAtom({
+        setAuthAtomState({
           provider: response.result.provider,
           role: response.result.role,
-          status: "AUTHENTICATED",
+          status: 'AUTHENTICATED',
         });
       } catch (error) {
-        console.log("auth error: ", error);
+        console.log('auth error: ', error);
       }
     })();
-  }, [authSelectorValue.isLogin, router.pathname]);
+  }, [isAuthenticated, router.pathname]);
 
   return null;
 }
