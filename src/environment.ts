5d57512e23497b5c3e3345dff3c2fbb7bb6340d5
K-Soo <kanabun102@naver.com>
2024-12-22 14:16:01 +0900

    feat: 환경 변수에 따라 로컬, 개발, 프로덕션 상태 추가

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,8 @@
 const environment = {
-  isProduction: process.env.NEXT_PUBLIC_IS_PRODUCTION === "true",
+  isLocal: process.env.NEXT_PUBLIC_APP_ENV === 'local',
+  isDev: process.env.NEXT_PUBLIC_APP_ENV === 'development',
+  isProd: process.env.NEXT_PUBLIC_APP_ENV === 'production',
+
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   apiUrl: process.env.NEXT_PUBLIC_API_URL,
 
