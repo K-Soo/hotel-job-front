5b034af2f953b100d767e1378088ee79c96a57f2
K-Soo <kanabun102@naver.com>
2024-12-22 14:18:49 +0900

    feat: 사용자 및 장바구니 아이콘 추가

--- src/icons/svg/index.ts
+++ src/icons/svg/index.ts
@@ -1,4 +1,6 @@
-import Back from "./back.svg";
-import Search from "./search.svg";
+import Back from './back.svg';
+import Search from './search.svg';
+import User from './user.svg';
+import Cart from './cart.svg';
 
-export { Back, Search };
+export { Back, Search, User, Cart };
