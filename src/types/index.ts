63d928cee2f5a7ead8055b3da7fcc2cb950d12d7
K-Soo <kanabun102@naver.com>
2024-12-22 14:17:24 +0900

    feat: 이력서 등록 폼 및 관련 타입 정의 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,59 @@
+import * as API from '@/types/API';
+import { educationLevel, careerLevel, job, position, salaryType } from '@/constants/resume';
+import { city } from '@/constants/location';
+
 export type ProviderType = 'local' | 'kakao';
 export type RoleType = 'ADMIN' | 'EMPLOYER' | 'JOB_SEEKER';
+export type ResumeType = 'FILE' | 'GENERAL'; //파일, 일반
+export type CareerLevel = keyof typeof careerLevel;
+export type EducationLevel = keyof typeof educationLevel;
+export type Job = keyof typeof job;
+export type Position = keyof typeof position;
+export type City = keyof typeof city;
+export type SalaryType = keyof typeof salaryType;
 
 export type TalentListItem = {};
+
+export type Experience = {
+  companyName: string;
+  isEmployed: boolean; //재직중
+  responsibility: string; //주요업무
+  job: Job;
+  position: Position;
+  startDate: Date;
+  endDate: Date;
+  city: City;
+  salaryType: SalaryType; //급여 유형
+  baseSalary: number; //급여 금액
+  allowance: number; //수당
+  reasonForLeaving: string; //퇴사사유
+};
+
+export type Language = {};
+
+export type License = {};
+
+export type Military = {};
+
+export interface defaultResponse {
+  success: boolean;
+  message: string;
+}
+
+export interface SignInForm extends API.SignInRequest {}
+
+export interface ResumeRegisterForm {
+  resumeType: ResumeType;
+  careerLevel: CareerLevel;
+  title: string;
+  summary: string;
+  education: EducationLevel;
+  isGraduated: boolean; //졸업여부
+  experiences: Experience[];
+  languages: Language[];
+  introduction: string; //자기소개
+  licenses: License;
+  military: Military;
+  isRequiredAgreement: boolean;
+  isOptionalAgreement: boolean;
+}
