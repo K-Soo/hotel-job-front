cb5ead3bc0a891c293b8977b1b73c92f68b78b20
K-Soo <kanabun102@naver.com>
2024-12-22 14:17:58 +0900

    feat: 색상 팔레트 업데이트 및 새로운 블루 색상 추가

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,10 +1,17 @@
 const colors = {
-  blue100: '#3182F7', //default
-  blue200: '#2272ec', //active
-  blue300: 'rgba(100, 167, 255, 0.69)', //disabled
-
   white100: '#FFFFFF', // 밝은 배경
   black100: '#212529', // 어두운 배경
+
+  blue50: '#e8f3ff',
+  blue100: '#c9e2ff',
+  blue200: '#90c2ff',
+  blue300: '#64a8ff', //disabled
+  blue400: '#4593fc',
+  blue500: '#3182f6', //primary
+  blue600: '#2272eb', //hover
+  blue700: '#1b64da',
+  blue800: '#1957c2',
+  blue900: '#194aa6',
 
   gray: '#f9fafb',
   gray100: '#f2f4f6',
