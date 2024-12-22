62eacaae8980f749eb9ea5dc2c94b1f2e89a8945
K-Soo <kanabun102@naver.com>
2024-12-22 14:18:05 +0900

    feat: FormDevTools 컴포넌트 추가 및 react-hook-form의 DevTool 통합

---
+++ src/components/common/FormDevTools.tsx
@@ -0,0 +1,19 @@
+import dynamic from 'next/dynamic';
+import { Control, FieldValues } from 'react-hook-form';
+import Portal from '@/components/common/Portal';
+
+const Devtool = dynamic<FieldValues>(() => import('@hookform/devtools').then((module) => module.DevTool), {
+  ssr: false,
+});
+
+interface IFormDevTool {
+  control?: Control<any, unknown>;
+}
+
+export default function FormDevTools({ control }: IFormDevTool) {
+  return (
+    <Portal>
+      <Devtool control={control} />
+    </Portal>
+  );
+}
