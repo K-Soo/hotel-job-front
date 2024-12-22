05037bf6e12acc49bc0f9b6cd3d61adec37ba0f9
K-Soo <kanabun102@naver.com>
2024-12-23 03:23:08 +0900

    feat: 이력서 등록 컨테이너 컴포넌트 추가

---
+++ src/containers/userResumeRegisterContainer/index.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import UserResumeRegister from '@/components/userResumeRegister';
+import ResumeBottomController from '@/components/common/resume/ResumeBottomController';
+import SignIn from '@/components/signIn';
+import Logo from '@/components/common/Logo';
+import SignInTabs from '@/components/signIn/SignInTabs';
+import { signInTabsOptions } from '@/constants/tabs';
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
+import { ResumeRegisterForm } from '@/types';
+import { schema } from '@/utils';
+import { yupResolver } from '@hookform/resolvers/yup';
+import FormDevTools from '@/components/common/FormDevTools';
+import CompanyForm from '@/components/signIn/CompanyForm';
+import GeneralForm from '@/components/signIn/GeneralForm';
+import { Post } from '@/apis';
+import useAuth from '@/hooks/useAuth';
+
+export default function UserResumeRegisterContainer() {
+  const methods = useForm<ResumeRegisterForm>({
+    resolver: yupResolver(schema.resumeRegister),
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      resumeType: 'GENERAL',
+      careerLevel: 'NEWBIE',
+      title: '',
+      summary: '',
+      experiences: [],
+    },
+  });
+
+  // const methods = useForm<ResumeRegisterForm>({
+  //   resolver: yupResolver(schema.resumeRegister),
+  //   mode: 'onSubmit',
+  //   reValidateMode: 'onChange',
+  //   defaultValues: {
+  //     resumeType: 'GENERAL',
+  //     careerLevel: 'NEWBIE',
+  //     title: '',
+  //     summary: '',
+  //     education: undefined,
+  //     isGraduated: true,
+  //     experiences: [],
+  //     languages: [],
+  //     introduction: '',
+  //   },
+  // });
+
+  console.log('methods: ', methods.watch());
+  const onSubmit: SubmitHandler<ResumeRegisterForm> = async (data) => {
+    try {
+    } catch (error) {
+    } finally {
+    }
+  };
+
+  const handleClickAdd = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const { name } = event.currentTarget;
+    const getValues = methods.getValues('experiences');
+
+    methods.setValue('experiences', [
+      ...getValues,
+      {
+        companyName: '',
+        // isEmployed: false,
+        responsibility: '',
+        job: undefined,
+        // city: 'NONE',
+        startDate: new Date(),
+        endDate: new Date(),
+        position: 'NONE',
+        salaryType: undefined,
+        // baseSalary: 0,
+        // allowance: 0,
+        // reasonForLeaving: '',
+      },
+    ]);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <UserResumeRegister handleClickAdd={handleClickAdd}>
+        <ResumeBottomController onSubmit={onSubmit} />
+      </UserResumeRegister>
+      <FormDevTools control={methods.control} />
+    </FormProvider>
+  );
+}
