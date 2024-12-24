b09d1d1685e198008d61c9c0157fb085d8e2d9e9
K-Soo <kanabun102@naver.com>
2024-12-24 21:18:32 +0900

    style: 코드에서 import 문을 일관된 따옴표 스타일로 변경

--- src/containers/recruitContainer/index.tsx
+++ src/containers/recruitContainer/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Recruit from "@/components/recruit";
-import RecruitListContainer from "@/containers/recruitContainer/RecruitListContainer";
-import RecruitCategory from "@/components/recruit/RecruitCategory";
-import SearchForm from "@/components/common/SearchForm";
-import PopularKeyword from "@/components/common/PopularKeyword";
+import React from 'react';
+import Recruit from '@/components/recruit';
+import RecruitListContainer from '@/containers/recruitContainer/RecruitListContainer';
+import RecruitCategory from '@/components/recruit/RecruitCategory';
+import SearchForm from '@/components/common/SearchForm';
+import PopularKeyword from '@/components/common/PopularKeyword';
 
 export default function RecruitContainer() {
   return (
