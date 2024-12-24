48b98089da65ed0a6af822116b0761ccc51c8b61
K-Soo <kanabun102@naver.com>
2024-12-24 21:19:50 +0900

    feat: useAuth 훅에서 인증 상태 반환 값 추가 및 코드 정리

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,17 +1,19 @@
-import { authAtom } from "@/recoil/auth";
-import { useRecoilState } from "recoil";
+import { authAtom } from '@/recoil/auth';
+import { useRecoilState } from 'recoil';
 
 export default function useAuth() {
   const [authAtomState, setAuthAtomState] = useRecoilState(authAtom);
 
-  const isAuthenticated = authAtomState.status === "AUTHENTICATED";
-  const isAuthIdle = authAtomState.status === "IDLE";
-  const isAuthFailure = authAtomState.status === "AUTHENTICATION_FAILURE";
+  const isAuthenticated = authAtomState.status === 'AUTHENTICATED';
+  const isAuthIdle = authAtomState.status === 'IDLE';
+  const isAuthFailure = authAtomState.status === 'AUTHENTICATION_FAILURE';
 
   return {
     isAuthenticated,
     isAuthIdle,
     isAuthFailure,
+
+    authAtomState,
     setAuthAtomState,
     authStatus: authAtomState.status,
   };
