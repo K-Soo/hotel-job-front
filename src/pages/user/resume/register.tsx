29c6fc7cce26a67f3e3c297761899d7f3a5bbe28
K-Soo <kanabun102@naver.com>
2024-12-24 21:21:08 +0900

    feat: 사용자 이력서 등록 페이지에 레이아웃 및 인증 추가

--- src/pages/user/resume/register.tsx
+++ src/pages/user/resume/register.tsx
@@ -1,3 +1,19 @@
+import Layout, { Main, Header, DesktopNavigation, MobileNavigation } from '@/components/layout';
+import UserResumeRegisterContainer from '@/containers/userResumeRegisterContainer';
 export default function UserResumeRegisterPage() {
-  return <>register</>;
+  return <UserResumeRegisterContainer />;
 }
+
+UserResumeRegisterPage.getLayout = (page: React.ReactElement) => {
+  return (
+    <Layout>
+      <Header>
+        <DesktopNavigation />
+        <MobileNavigation title="이력서 등록" />
+      </Header>
+      <Main>{page}</Main>
+    </Layout>
+  );
+};
+
+UserResumeRegisterPage.authentication = true;
