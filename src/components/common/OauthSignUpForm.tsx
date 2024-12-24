62ba557c3768acb1066ae4b7ff456bf9dc06a094
K-Soo <kanabun102@naver.com>
2024-12-25 04:21:37 +0900

    feat: OauthSignUpForm에 로고 추가 및 제목 스타일 개선

--- src/components/common/OauthSignUpForm.tsx
+++ src/components/common/OauthSignUpForm.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import Logo from '@/components/common/Logo';
 interface OauthSignUpFormProps {
   children: React.ReactNode;
 }
@@ -7,7 +7,10 @@ interface OauthSignUpFormProps {
 export default function OauthSignUpForm({ children }: OauthSignUpFormProps) {
   return (
     <S.OauthSignUpForm>
-      <h6>제목</h6>
+      <div className="sign-up-header">
+        <Logo size="middle" margin="0 0 15px 0" />
+      </div>
+      <h2 className="sign-up-title">이용약관 동의</h2>
       {children}
     </S.OauthSignUpForm>
   );
@@ -18,7 +21,16 @@ const S = {
     max-width: 500px;
     margin: 0 auto;
     width: 100%;
-    border: 1px solid red;
     height: 100%;
+    .sign-up-header {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .sign-up-title {
+      margin: 30px 0 15px 0;
+      font-size: 18px;
+      font-weight: 500;
+    }
   `,
 };
