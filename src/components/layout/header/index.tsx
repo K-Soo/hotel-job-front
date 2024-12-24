7d305732c32e2be96286c3873c6b73af50ef4daf
K-Soo <kanabun102@naver.com>
2024-12-24 21:16:11 +0900

    feat: Header 컴포넌트에서 스크롤 관련 기능 제거 및 스타일 수정

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,23 +1,20 @@
 import styled from 'styled-components';
 
-import { motion, useScroll } from 'framer-motion';
-
 interface HeaderProps {
   children: React.ReactNode;
 }
 
 export function Header({ children }: HeaderProps) {
-  const { scrollYProgress, scrollX, scrollY } = useScroll();
-
-  return <S.Header initial={{ borderBottom: '0px solid transparent' }}>{children}</S.Header>;
+  return <S.Header>{children}</S.Header>;
 }
 
 const S = {
-  Header: styled(motion.header)`
+  Header: styled.header`
     position: sticky;
-    top: -40px;
-    background-color: rgba(255, 255, 255);
+    top: -35px;
     box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.1);
+    z-index: 5;
+    background-color: ${(props) => props.theme.colors.white};
     ${(props) => props.theme.media.tablet`
       top: 0;
       padding: 0 15px;
