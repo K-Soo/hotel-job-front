bbd6b6c4ec49c5660ca1b239715b2ccb22d8ffed
K-Soo <kanabun102@naver.com>
2024-12-25 00:27:51 +0900

    feat: UserResumeContainer에서 UserAsideMenu를 UserTemplate 및 UserTitle로 변경

--- src/containers/userResumeContainer/index.tsx
+++ src/containers/userResumeContainer/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import UserResume from '@/components/userResume';
-import UserAsideMenu from '@/components/common/UserAsideMenu';
+import UserTemplate from '@/components/common/user/UserTemplate';
+import UserTitle from '@/components/common/user/UserTitle';
 
 export default function UserResumeContainer() {
   return (
     <UserResume>
-      <UserAsideMenu />
+      <UserTemplate>
+        <UserTitle title="이력서 목록" />
+      </UserTemplate>
     </UserResume>
   );
 }
