5565c9420afe36927a7a6659488f8dd92200d95b
K-Soo <kanabun102@naver.com>
2024-12-24 21:16:03 +0900

    feat: Layout 컴포넌트에 DaumPost 추가 및 포털 기능 구현

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,6 +3,7 @@ import Loading from '@/components/common/Loading';
 import Portal from '@/components/common/Portal';
 import { useRecoilValue } from 'recoil';
 import { loadingAtom } from '@/recoil/loading';
+import { daumPostAtom } from '@/recoil/daumPost';
 
 export { Footer } from '@/components/layout/footer';
 export { Main } from '@/components/layout/main';
@@ -10,18 +11,27 @@ export { Header } from '@/components/layout/header';
 export { DesktopNavigation } from '@/components/layout/header/desktopNavigation';
 export { MobileNavigation } from '@/components/layout/header/mobileNavigation';
 
+import DaumPost from '@/components/common/DaumPost';
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
   const loadingAtomValue = useRecoilValue(loadingAtom);
+  const daumPostAtomValue = useRecoilValue(daumPostAtom);
 
   return (
     <S.Layout>
       {loadingAtomValue.isLoading && (
         <Portal>
           <Loading />
+        </Portal>
+      )}
+
+      {daumPostAtomValue.isOpen && (
+        <Portal>
+          <DaumPost />
         </Portal>
       )}
 
