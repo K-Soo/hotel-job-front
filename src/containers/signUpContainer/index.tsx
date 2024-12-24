21ffa2e641d37ee1abd33923092e9d78d1ac2718
K-Soo <kanabun102@naver.com>
2024-12-25 04:22:12 +0900

    feat: SignUpContainer에 단계별 폼 처리 및 제출 기능 추가

--- src/containers/signUpContainer/index.tsx
+++ src/containers/signUpContainer/index.tsx
@@ -1,6 +1,42 @@
-import React from "react";
-import SignUp from "@/components/signUp";
+import React from 'react';
+import SignUp from '@/components/signUp';
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
+import { schema } from '@/utils';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SignUpForm } from '@/types';
+import FormDevTools from '@/components/common/FormDevTools';
+import SignUpCompanyForm from '@/components/signUp/SignUpCompanyForm';
+import SignUpGeneralForm from '@/components/signUp/SignUpGeneralForm';
 
 export default function SignUpContainer() {
-  return <SignUp />;
+  const [step, setStep] = React.useState('STEP_1');
+
+  const methods = useForm<SignUpForm>({
+    resolver: yupResolver(schema.signUpSchema),
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      userId: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<SignUpForm> = async (data) => {
+    try {
+    } catch (error) {
+    } finally {
+    }
+  };
+
+  const handleNextStep = () => {};
+
+  return (
+    <FormProvider {...methods}>
+      <SignUp onSubmit={onSubmit}>
+        {step === 'STEP_1' && <SignUpCompanyForm setStep={setStep} />}
+        {step === 'STEP_2' && <SignUpGeneralForm />}
+      </SignUp>
+      <FormDevTools control={methods.control} />
+    </FormProvider>
+  );
 }
