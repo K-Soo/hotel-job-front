9c8706ba8c9f52256c9fb5f9b505a1d4d2f47417
K-Soo <kanabun102@naver.com>
2024-12-25 04:22:03 +0900

    feat: Main 컴포넌트에 padding prop 추가 및 기본 패딩 값 설정

--- src/components/layout/main/index.tsx
+++ src/components/layout/main/index.tsx
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 interface MainProps {
   maxWidth?: string;
+  padding?: string;
   children: React.ReactNode;
 }
 
-export function Main({ maxWidth, children }: MainProps) {
+export function Main({ maxWidth, padding, children }: MainProps) {
   return <S.Main $maxWidth={maxWidth}>{children}</S.Main>;
 }
 
 const S = {
-  Main: styled.main<{ $maxWidth?: string }>`
+  Main: styled.main<{ $maxWidth?: string; $padding?: string }>`
     flex: 1;
     height: 100%;
     max-width: ${(props) => (props.$maxWidth ? props.$maxWidth : '1024px')};
@@ -18,10 +19,10 @@ const S = {
     display: flex;
     flex-direction: column;
     margin: 0 auto;
-    padding-top: 30px;
     ${(props) => props.theme.media.laptop`
       padding: 30px 15px 0 15px;
     `};
+    padding: ${(props) => (props.$padding ? props.$padding : '30px 0 0 0')};
     & > section {
       flex: 1;
     }
