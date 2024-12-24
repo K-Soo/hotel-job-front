4e7ed4675ccb3487591e3225c89903c0cb8bc1fe
K-Soo <kanabun102@naver.com>
2024-12-24 21:18:15 +0900

    feat: 사용자 관련 경로 상수 추가 및 불필요한 경로 제거

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -5,8 +5,6 @@ const path = {
   SIGN_UP: '/sign-up', //기업 가입
   SIGN_UP_COMPLETE: '/sign-up/complete',
 
-  ACCOUNT: '/account', // 마이페이지
-
   RECRUIT: '/recruit', // 채용정보 메인 페이지
   RECRUIT_HOTEL: '/recruit/hotel', // 호텔 채용정보
   RECRUIT_PENSION: '/recruit/pension', // 펜션 채용정보
@@ -32,12 +30,14 @@ const path = {
   POLICY_TERMS: '/policy/terms', //서비스 이용약관
   POLICY__PRIVACY: '/policy/privacy', //개인정보 처리방침
 
+  USER: '/user', //마이페이지
+  USER_RESUME_REGISTER: '/user/resume/register', // 이력서 등록
+  USER_USER: '/user/profile', //프로필 페이지
+  USER_POINT: '/user/point', //포인트
+
+  ACCOUNT_RECOVER_RESET_SUCCESS: '/account/recover/reset/success', //비밀번호 찾기 결과
   ACCOUNT_RECOVER_USER: '/account/recover/user', //이메일 찾기
   ACCOUNT_RECOVER_RESET: '/account/recover/reset', //비밀번호 찾기
-  ACCOUNT_RECOVER_RESET_SUCCESS: '/account/recover/reset/success', //비밀번호 찾기 결과
-
-  ACCOUNT_POINT: '/account/point', //포인트
-  ACCOUNT_USER: '/account/user', //회원정보 수정
 } as const;
 
 export default path;
