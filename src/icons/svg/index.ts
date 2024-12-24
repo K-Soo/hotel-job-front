d61ffab0c5a3a8939c169a48b324318928c31163
K-Soo <kanabun102@naver.com>
2024-12-24 21:19:57 +0900

    feat: Plus24x24 아이콘 추가

--- src/icons/svg/index.ts
+++ src/icons/svg/index.ts
@@ -2,5 +2,6 @@ import Back from './back.svg';
 import Search from './search.svg';
 import User from './user.svg';
 import Cart from './cart.svg';
+import Plus24x24 from './plus24x24.svg';
 
-export { Back, Search, User, Cart };
+export { Back, Search, User, Cart, Plus24x24 };
