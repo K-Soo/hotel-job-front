966d2d5fbe12f4151ad511bde898e369e011f57d
K-Soo <kanabun102@naver.com>
2024-12-24 21:21:04 +0900

    feat: 사용자 이력서 페이지에 레이아웃 및 인증 추가

--- src/pages/user/resume/index.tsx
+++ src/pages/user/resume/index.tsx
@@ -1,3 +1,19 @@
+import Layout, { Main, Header, DesktopNavigation, MobileNavigation } from '@/components/layout';
+import UserResumeContainer from '@/containers/userResumeContainer';
 export default function UserResumePage() {
-  return <>index</>;
+  return <UserResumeContainer />;
 }
+
+UserResumePage.getLayout = (page: React.ReactElement) => {
+  return (
+    <Layout>
+      <Header>
+        <DesktopNavigation />
+        <MobileNavigation />
+      </Header>
+      <Main>{page}</Main>
+    </Layout>
+  );
+};
+
+UserResumePage.authentication = true;
