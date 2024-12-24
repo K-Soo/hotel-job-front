5600b266b4fa9b66b6093b8fe8638224432c8af3
K-Soo <kanabun102@naver.com>
2024-12-24 21:20:08 +0900

    feat: 리크루트 페이지에 네비게이션 컴포넌트 추가 및 레이아웃 개선

--- src/pages/recruit/index.tsx
+++ src/pages/recruit/index.tsx
@@ -1,5 +1,6 @@
-import Layout, { Header, Footer, Main } from "@/components/layout";
-import RecruitContainer from "@/containers/recruitContainer";
+import Layout, { Header, Footer, Main, DesktopNavigation, MobileNavigation } from '@/components/layout';
+import RecruitContainer from '@/containers/recruitContainer';
+import BottomNavigation from '@/components/common/BottomNavigation';
 
 export default function RecruitPage() {
   return <RecruitContainer />;
@@ -8,9 +9,13 @@ export default function RecruitPage() {
 RecruitPage.getLayout = (page: React.ReactElement) => {
   return (
     <Layout>
-      <Header />
+      <Header>
+        <DesktopNavigation />
+        <MobileNavigation />
+      </Header>
       <Main>{page}</Main>
       <Footer />
+      <BottomNavigation />
     </Layout>
   );
 };
