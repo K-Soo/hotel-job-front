a38089808c1e47805633a135a31402d69ae5c8c3
K-Soo <kanabun102@naver.com>
2024-12-24 21:16:35 +0900

    feat: MobileNavigation 컴포넌트에 뒤로가기 및 프로필 아이콘 추가, 스타일 개선

--- src/components/layout/header/mobileNavigation/index.tsx
+++ src/components/layout/header/mobileNavigation/index.tsx
@@ -1,9 +1,30 @@
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
-interface MobileNavigationProps {}
+interface MobileNavigationProps {
+  backIcon?: boolean;
+  profileIcon?: boolean;
+  path?: string;
+  title?: string;
+}
 
-export function MobileNavigation({}: MobileNavigationProps) {
-  return <S.MobileNavigation>MobileNavigation</S.MobileNavigation>;
+export function MobileNavigation({ title, path, backIcon, profileIcon }: MobileNavigationProps) {
+  const router = useRouter();
+
+  const handleClickBack = () => {
+    if (!path) return;
+    router.push(path);
+  };
+
+  return (
+    <S.MobileNavigation>
+      <div className="left" onClick={handleClickBack}>
+        {backIcon && <i>back</i>}
+      </div>
+      <div className="title">{title && <h3>{title}</h3>}</div>
+      <div className="right">{profileIcon && <i>profile</i>}</div>
+    </S.MobileNavigation>
+  );
 }
 
 const S = {
@@ -11,7 +32,27 @@ const S = {
     display: none;
     height: 50px;
     ${(props) => props.theme.media.tablet`
-      display: block;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     `}
+    .left {
+      flex-basis: 50px;
+      height: 100%;
+    }
+    .title {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      flex-basis: calc(100% - 100px);
+      font-size: 16px;
+      font-weight: 500;
+    }
+    .right {
+      flex-basis: 50px;
+      height: 100%;
+    }
   `,
 };
