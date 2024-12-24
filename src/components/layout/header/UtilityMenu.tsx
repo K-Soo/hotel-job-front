2169dc6db47ed839a65480e6e4c83f0a7631b89e
K-Soo <kanabun102@naver.com>
2024-12-24 21:16:20 +0900

    feat: UtilityMenu 컴포넌트의 높이를 50px에서 35px로 변경

--- src/components/layout/header/UtilityMenu.tsx
+++ src/components/layout/header/UtilityMenu.tsx
@@ -4,16 +4,12 @@ import Logo from '@/components/common/Logo';
 interface UtilityMenuProps {}
 
 export default function UtilityMenu({}: UtilityMenuProps) {
-  return (
-    <S.UtilityMenu>
-      <Logo size="small" />
-    </S.UtilityMenu>
-  );
+  return <S.UtilityMenu></S.UtilityMenu>;
 }
 
 const S = {
   UtilityMenu: styled.div`
-    height: 50px;
+    height: 35px;
     max-width: 1024px;
     margin: 0 auto;
     display: flex;
