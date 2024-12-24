d31dae25cabd67fb7056a14f0b2159271d2b515d
K-Soo <kanabun102@naver.com>
2024-12-25 04:21:46 +0900

    feat: MobileNavigation 컴포넌트에 로고 및 홈 아이콘 추가, backUrl prop 수정

--- src/components/layout/header/mobileNavigation/index.tsx
+++ src/components/layout/header/mobileNavigation/index.tsx
@@ -1,26 +1,32 @@
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
-
+import Logo from '@/components/common/Logo';
+import path from '@/constants/path';
 interface MobileNavigationProps {
   backIcon?: boolean;
   profileIcon?: boolean;
-  path?: string;
+  logoIcon?: boolean;
+  homeIcon?: boolean;
+  backUrl?: string;
   title?: string;
 }
 
-export function MobileNavigation({ title, path, backIcon, profileIcon }: MobileNavigationProps) {
+export function MobileNavigation({ title, backUrl, backIcon, profileIcon, logoIcon, homeIcon }: MobileNavigationProps) {
   const router = useRouter();
 
   const handleClickBack = () => {
-    if (!path) return;
-    router.push(path);
+    if (!backUrl) return;
+    router.push(backUrl);
   };
 
   return (
     <S.MobileNavigation>
       <div className="left" onClick={handleClickBack}>
         {backIcon && <i>back</i>}
+        {homeIcon && <i onClick={() => router.push(path.HOME)}>home</i>}
+        {logoIcon && <Logo size="small" margin="0" />}
       </div>
+
       <div className="title">{title && <h3>{title}</h3>}</div>
       <div className="right">{profileIcon && <i>profile</i>}</div>
     </S.MobileNavigation>
@@ -39,6 +45,8 @@ const S = {
     .left {
       flex-basis: 50px;
       height: 100%;
+      display: flex;
+      align-items: center;
     }
     .title {
       height: 100%;
