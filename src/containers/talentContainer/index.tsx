de76d4ed8a08b6256d0be5da469aebf993638318
K-Soo <kanabun102@naver.com>
2024-12-24 21:18:40 +0900

    feat: 공통 스타일 버튼 컴포넌트 추가

--- src/containers/talentContainer/index.tsx
+++ src/containers/talentContainer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Talent from '@/components/talent';
 import TalentListContainer from '@/containers/talentContainer/TalentListContainer';
 import TalentFilter from '@/components/talent/TalentFilter';
+import Button from '@/components/common/style/Button';
 
 export default function TalentContainer() {
   return (
