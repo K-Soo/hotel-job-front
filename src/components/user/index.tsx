85ec6345b880fc6e9af8fb448f9b63f2a67a378c
K-Soo <kanabun102@naver.com>
2024-12-24 21:16:59 +0900

    feat: User 컴포넌트 추가 및 기본 레이아웃 설정

---
+++ src/components/user/index.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+interface UserProps {
+  children: React.ReactNode;
+}
+
+export default function User({ children }: UserProps) {
+  return (
+    <S.User>
+      {children}
+
+      <div className="container">
+        <div>서머리</div>
+        <div>컨텐츠</div>
+      </div>
+    </S.User>
+  );
+}
+
+const S = {
+  User: styled.section`
+    display: flex;
+  `,
+};
