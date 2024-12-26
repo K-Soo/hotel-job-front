286784288423c021eba6397194dcdcefafa02b45
K-Soo <kanabun102@naver.com>
2024-12-27 04:10:09 +0900

    feat: 새로운 SVG 아이콘 추가 (검색24x24, 아래 화살표14x14, 위치24x24)

--- src/icons/svg/index.ts
+++ src/icons/svg/index.ts
@@ -1,8 +1,10 @@
 import Back from './back.svg';
-import Search from './search.svg';
+import Search24x24 from './search24x24.svg';
 import User from './user.svg';
 import Cart from './cart.svg';
 import Plus24x24 from './plus24x24.svg';
 import ArrowRight16x16 from './arrowRight16x16.svg';
+import ArrowBottom14x14 from './arrowBottom14x14.svg';
+import Location24x24 from './location24x24.svg';
 
-export { Back, Search, User, Cart, Plus24x24, ArrowRight16x16 };
+export { Back, Search24x24, User, Cart, Plus24x24, ArrowRight16x16, Location24x24, ArrowBottom14x14 };
