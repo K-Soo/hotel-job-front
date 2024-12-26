d466b0f4404378895d792b5d9ff49660cacd01b3
K-Soo <kanabun102@naver.com>
2024-12-27 04:10:27 +0900

    refactor: SignUpPage에서 사용하지 않는 컴포넌트 제거

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,7 +1,5 @@
 import SignUpContainer from '@/containers/signUpContainer';
 import Layout, { Header, Footer, Main, DesktopNavigation, MobileNavigation } from '@/components/layout';
-import HomeContainer from '@/containers/homeContainer';
-import BottomNavigation from '@/components/common/BottomNavigation';
 
 export default function SignUpPage() {
   return <SignUpContainer />;
