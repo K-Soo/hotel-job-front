5ff43fdab6f553e26dddc99b92f575b4a87717ee
K-Soo <kanabun102@naver.com>
2024-12-26 15:52:47 +0900

    feat: 정책 정보 업데이트 및 새로운 필드 추가

--- src/constants/policy.ts
+++ src/constants/policy.ts
@@ -1,8 +1,11 @@
 export const policy = {
   company: '셀레스타라',
   name: '고원호',
-  email: 'help.hoteljob@gmail.com',
+  email: 'help.celestara@gmail.com',
   domain: 'www.hotel-job-connect.com',
   application: '호텔잡',
   phone: '010-6479-7825',
+  address: '경기도 남양주시 두물로27번길 17, 202',
+  businessNumber: '107-37-90473',
+  mailOrderNumber: '2024-별내-2240',
 };
