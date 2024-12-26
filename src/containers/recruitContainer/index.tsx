94712706d78e3983a4d7fd0462e78f82b1c59631
K-Soo <kanabun102@naver.com>
2024-12-27 04:09:47 +0900

    feat: RecruitContainer에 필터 패널 및 섹션 제목 추가

--- src/containers/recruitContainer/index.tsx
+++ src/containers/recruitContainer/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import Recruit from '@/components/recruit';
+import RecruitSectionTitle from '@/components/recruit/RecruitSectionTitle';
+import RecruitFilterButton from '@/components/recruit/RecruitFilterButton';
+import RecruitFilterPanel from '@/components/recruit/RecruitFilterPanel';
 import RecruitListContainer from '@/containers/recruitContainer/RecruitListContainer';
-import RecruitCategory from '@/components/recruit/RecruitCategory';
-import SearchForm from '@/components/common/SearchForm';
-import PopularKeyword from '@/components/common/PopularKeyword';
+import RecruitSearchPanel from '@/components/common/RecruitSearchPanel';
+import { recruitOrderFilterOptions } from '@/constants/tabs';
+import Tabs from '@/components/common/Tabs';
+import Line from '@/components/common/Line';
 
 export default function RecruitContainer() {
   return (
     <Recruit>
-      {/* <SearchForm /> */}
-      {/* <PopularKeyword /> */}
-      <RecruitCategory />
+      <RecruitSearchPanel />
+
+      <RecruitFilterPanel />
+
+      <Line margin="20px 0" />
+
+      <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '0 0 20px 0' }}>
+        <Tabs
+          tabsOptions={recruitOrderFilterOptions}
+          width="200px"
+          height="35px"
+          fontSize="13px"
+          fontColor="gray"
+          backgroundColor="#FFFFFF"
+        />
+      </div>
+
+      <RecruitSectionTitle title="일반 채용" count={0} />
       <RecruitListContainer />
     </Recruit>
   );
