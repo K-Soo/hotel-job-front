3c3302ce79685024bdee94fd270457722d1f312d
K-Soo <kanabun102@naver.com>
2024-12-27 04:09:56 +0900

    feat: useResponsive 훅 추가하여 반응형 디자인 지원

---
+++ src/hooks/useResponsive.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+
+export default function useResponsive() {
+  const isMobileQuery = useMediaQuery({ query: '(max-width: 500px)' });
+  const isTabletQuery = useMediaQuery({ query: '(min-width: 501px) and (max-width: 768px)' });
+  const isLaptopQuery = useMediaQuery({ query: '(max-width: 1024px)' });
+
+  const [isMobile, setIsMobile] = React.useState(false);
+  const [isTablet, setIsTablet] = React.useState(true);
+  const [isLaptop, setIsLaptop] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMobile(isMobileQuery);
+    setIsTablet(isTabletQuery);
+    setIsLaptop(isLaptopQuery);
+  }, [isMobileQuery, isTabletQuery, isLaptopQuery]);
+
+  return { isMobile, isTablet, isLaptop };
+}
