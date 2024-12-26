857e442e75b30afe570e37ab1d056386129290c3
K-Soo <kanabun102@naver.com>
2024-12-27 04:09:28 +0900

    feat: SignUpCompanyForm 컴포넌트 수정 및 단계 변경

--- src/components/signUp/SignUpCompanyForm.tsx
+++ src/components/signUp/SignUpCompanyForm.tsx
@@ -3,7 +3,6 @@ import FormInput from '@/components/common/form/FormInput';
 import Button from '@/components/common/style/Button';
 import { SignUpForm } from '@/types';
 import { useRouter } from 'next/router';
-import path from '@/constants/path';
 
 interface SignUpCompanyFormProps {
   setStep: React.Dispatch<React.SetStateAction<string>>;
@@ -17,12 +16,10 @@ export default function SignUpCompanyForm({ setStep }: SignUpCompanyFormProps) {
       <h2 className="sign-up-title">업체 정보</h2>
       <FormInput<SignUpForm> required label="사업자등록번호" name="password" placeholder="사업자등록번호" />
       <FormInput<SignUpForm> required label="상호명" name="password" placeholder="상호명" />
-      <FormInput<SignUpForm> required label="대표자" name="password" placeholder="대표자" />
-      <FormInput<SignUpForm> required label="사업장 주소" name="password" placeholder="사업장 주소" />
-      <FormInput<SignUpForm> required label="사업장 상세주소" name="password" placeholder="사업장 상세주소" />
+
       <div className="button-group">
         <Button label="뒤로" variant="secondary" margin="0 15px 0 0" onClick={() => setStep('STEP_1')} />
-        <Button label="다음" variant="primary" onClick={() => setStep('STEP_3')} />
+        <Button label="다음" variant="primary" onClick={() => setStep('STEP_2')} />
       </div>
     </S.SignUpCompanyForm>
   );
