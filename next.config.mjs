30ef241d9c869231d557dbbe5d8566c9660eaf9f
K-Soo <kanabun102@naver.com>
2024-12-27 03:18:28 +0900

    feat: 환경 설정을 위한 .env-cmdrc.json 파일 추가 및 의존성 패키지 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import withInterceptStdout from "next-intercept-stdout";
+import withInterceptStdout from 'next-intercept-stdout';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,11 +10,11 @@ const nextConfig = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
 
     return config;
   },
 };
 
-export default withInterceptStdout(nextConfig, (text) => (text.includes("Duplicate atom key") ? "" : text));
+export default withInterceptStdout(nextConfig, (text) => (text.includes('Duplicate atom key') ? '' : text));
