61fc3948eabadf38bf8892c7fb5b0ae5d62e4c62
K-Soo <kanabun102@naver.com>
2024-12-27 17:35:47 +0900

    feat: footer 컴포넌트에 이메일 및 연락처 정보 추가

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -25,10 +25,11 @@ export function Footer() {
           </li>
         </S.Category>
         <S.Information>
-          <p className="item">이메일: {policy.email}</p>
           <p className="item">사업자등록번호: {policy.businessNumber} | 대표자: 고원호</p>
           <p className="item">통신판매업 신고번호: {policy.mailOrderNumber}</p>
           <p className="item">{policy.address}</p>
+          <p className="item">이메일: {policy.email}</p>
+          <p className="item">연락처: {policy.phone}</p>
           <strong className="item company">COPYRIGHT ⓒ HOTEL JOB ALL RIGHTS RESERVED.</strong>
         </S.Information>
       </div>
