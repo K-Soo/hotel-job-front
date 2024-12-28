bc8ecf51cb06a5299f1f9a98a5d30a7b8788a053
K-Soo <kanabun102@naver.com>
2024-12-28 14:51:36 +0900

    feat: EmployerProductCheckoutContainer 컴포넌트 추가

---
+++ src/containers/employerProductCheckoutContainer/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import EmployerProductCheckout from '@/components/employerProductCheckout';
+
+export default function EmployerProductCheckoutContainer() {
+  return <EmployerProductCheckout />;
+}
