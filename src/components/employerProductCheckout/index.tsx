c6c6d768934742c098357ac26130038460ff22a1
K-Soo <kanabun102@naver.com>
2024-12-28 14:50:15 +0900

    feat: EmployerProductCheckout 컴포넌트 추가

---
+++ src/components/employerProductCheckout/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface EmployerProductCheckoutProps {}
+
+export default function EmployerProductCheckout({}: EmployerProductCheckoutProps) {
+  return <S.EmployerProductCheckout>EmployerProductCheckout</S.EmployerProductCheckout>;
+}
+
+const S = {
+  EmployerProductCheckout: styled.section``,
+};
