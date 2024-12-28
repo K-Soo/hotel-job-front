55de6d9343b66db88bdd6b1e193758afda7f52a9
K-Soo <kanabun102@naver.com>
2024-12-29 04:08:47 +0900

    feat: Line 컴포넌트에 색상 속성 추가

--- src/components/common/Line.tsx
+++ src/components/common/Line.tsx
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 interface LineProps {
   margin?: string;
+  color?: string;
 }
 
-export default function Line({ margin }: LineProps) {
-  return <S.Line $margin={margin} />;
+export default function Line({ margin, color }: LineProps) {
+  return <S.Line $margin={margin} $color={color} />;
 }
 
 const S = {
-  Line: styled.div<{ $margin?: string }>`
-    border-top: 1px solid ${(props) => props.theme.colors.grayOpacity200};
+  Line: styled.div<{ $margin?: string; $color?: string }>`
+    border-top: 1px solid ${(props) => (props.$color ? props.$color : props.theme.colors.grayOpacity200)};
     margin: ${(props) => (props.$margin ? props.$margin : '20px 0')};
   `,
 };
