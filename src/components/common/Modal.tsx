3c92624a2ba75e73e60edda16f24325388c64c8b
K-Soo <kanabun102@naver.com>
2024-12-29 04:08:55 +0900

    feat: Modal 컴포넌트 추가

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface ModalProps {}
+
+export default function Modal({}: ModalProps) {
+  return <S.Modal>Modal</S.Modal>;
+}
+
+const S = {
+  Modal: styled.div``,
+};
