46c05a343ab1610e9482974f551115f5758eaa20
K-Soo <kanabun102@naver.com>
2024-12-29 04:13:34 +0900

    feat: NextPageWithLayout에 allowedRoles 속성 추가

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,9 +1,11 @@
-import { AppProps } from "next/app";
-import { NextPage } from "next";
-import { DehydratedState } from "@tanstack/react-query";
+import { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import { DehydratedState } from '@tanstack/react-query';
+import { RoleType } from '@/types';
 
 type NextPageWithLayout = NextPage & {
   authentication?: boolean;
+  allowedRoles?: RoleType[];
   getLayout?: (page: React.ReactElement) => React.ReactNode;
 };
 
