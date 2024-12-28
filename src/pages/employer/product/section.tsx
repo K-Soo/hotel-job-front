064cd65c170d41f6fce11b4b8e74070a57cd13b2
K-Soo <kanabun102@naver.com>
2024-12-28 14:51:57 +0900

    feat: 고용주 제품 섹션 페이지 컴포넌트 추가

---
+++ src/pages/employer/product/section.tsx
@@ -0,0 +1,15 @@
+import Layout, { EmployerMain, EmployerHeader } from '@/components/layout';
+import EmployerProductSectionContainer from '@/containers/employerProductSectionContainer';
+
+export default function EmployerProductSectionPage() {
+  return <EmployerProductSectionContainer />;
+}
+
+EmployerProductSectionPage.getLayout = (page: React.ReactElement) => {
+  return (
+    <Layout>
+      <EmployerHeader />
+      <EmployerMain>{page}</EmployerMain>
+    </Layout>
+  );
+};
