d243cc31cb8a6fbef10a60676ba19e12a3ef56a8
K-Soo <kanabun102@naver.com>
2024-12-29 04:09:51 +0900

    feat: Select 컴포넌트에 여백 속성 추가 및 아이콘 표시 방식 개선

--- src/components/common/style/Select.tsx
+++ src/components/common/style/Select.tsx
@@ -1,68 +1,70 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import FormError from '@/components/common/form/FormError';
-import { useFormContext, Path, FieldValues } from 'react-hook-form';
 import { motion } from 'framer-motion';
-import { get } from 'lodash';
+import Icon from '@/icons/Icon';
 interface SelectProps {
+  margin?: string;
   options: { label: string; value: string }[];
 }
 
-export default function Select({ options }: SelectProps) {
+export default function Select({ margin, options }: SelectProps) {
   return (
-    <S.Select>
-      <StyledMotionSelect name="" id="">
+    <S.Select $margin={margin}>
+      <StyledSelect name="">
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
           </option>
         ))}
-      </StyledMotionSelect>
+      </StyledSelect>
+      <Icon className="select-icon" name="ArrowRight16x16" width="16px" height="16px" />
     </S.Select>
   );
 }
 
-const StyledLabel = styled.label<{ required?: boolean }>`
-  display: block;
-  margin-bottom: 3px;
-  font-size: 15px;
-  cursor: default;
-  white-space: nowrap;
-  margin-right: 15px;
-  color: ${({ theme }) => theme.colors.gray700};
-  ${(props) =>
-    props.required &&
-    css`
-      &::after {
-        content: '*';
-        margin-left: 2px;
-        vertical-align: top;
-        color: crimson;
-      }
-    `};
-`;
-
-const StyledMotionSelect = styled(motion.select)`
-  border: 1px solid ${({ theme }) => theme.colors.gray300};
-  display: block;
-  height: 40px;
+const StyledSelect = styled.select`
+  height: 100%;
   width: 100%;
-  padding-left: 10px;
-  border-radius: 5px;
-  font-size: 16px;
   cursor: pointer;
-  /* &::placeholder {
-    color: ${(props) => props.theme.colors.gray400};
-    font-size: 14px;
+  padding-left: 10px;
+  display: flex;
+  align-items: center;
+  border-radius: inherit;
+  background-color: inherit;
+  border: 1px solid ${({ theme }) => theme.colors.gray300};
+  color: ${(props) => props.theme.colors.black500};
+  font-size: 14px;
+  &:hover {
+    transition: 0.3s;
+    border: 1px solid ${({ theme }) => theme.colors.blue100};
+  }
+  &:focus {
+    border: 1px solid ${(props) => props.theme.colors.blue500};
   }
   &:disabled {
     background-color: ${(props) => props.theme.colors.gray100};
-  } */
+  }
 `;
 
 const S = {
-  Select: styled.div`
+  Select: styled(motion.div)<{ $margin?: string }>`
+    margin: ${(props) => props.$margin || '0'};
+    background-color: ${(props) => props.theme.colors.white};
     height: 40px;
     width: 100%;
+    border-radius: 4px;
+    position: relative;
+    :hover {
+      background-color: ${(props) => props.theme.colors.blue};
+    }
+    .select-icon {
+      position: absolute;
+      right: 10px;
+      top: 50%;
+      transform: rotate(90deg);
+      transform: translateY(-50%) rotate(90deg);
+      color: ${({ theme }) => theme.colors.gray300};
+      pointer-events: none;
+    }
   `,
 };
