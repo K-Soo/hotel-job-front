a0f3416c2217e31151e355e7b7b0a927015b9f91
K-Soo <kanabun102@naver.com>
2024-12-29 04:08:30 +0900

    feat: 역할 기반 접근 제어 추가 및 로딩 컴포넌트로 변경

--- src/auth/GuardComponent.tsx
+++ src/auth/GuardComponent.tsx
@@ -4,15 +4,18 @@ import { useRouter } from 'next/router';
 import path from '@/constants/path';
 import { Internal } from '@/apis';
 import useAuth from '@/hooks/useAuth';
+import { RoleType } from '@/types';
+import Loading from '@/components/common/Loading';
 
 interface GuardComponentProps {
+  allowedRoles: RoleType[] | undefined;
   children: React.ReactNode;
 }
 
-export default function GuardComponent({ children }: GuardComponentProps) {
+export default function GuardComponent({ allowedRoles, children }: GuardComponentProps) {
   const [showLoading, setShowLoading] = React.useState(false);
   const router = useRouter();
-  const { isAuthFailure, isAuthIdle, isAuthenticated, authStatus } = useAuth();
+  const { isAuthFailure, isAuthIdle, isAuthenticated, authStatus, role } = useAuth();
 
   React.useEffect(() => {
     const timer = setTimeout(() => {
@@ -23,30 +26,25 @@ export default function GuardComponent({ children }: GuardComponentProps) {
   }, []);
 
   React.useEffect(() => {
-    if (isAuthenticated) {
-      return;
-    }
-    (async () => {
-      try {
-        const cookieExist = await Internal.checkRefreshCookie();
-        if (!cookieExist) {
-          router.replace(path.HOME);
-        }
-      } catch (error) {
-        console.log('auth error: ', error);
-      }
-    })();
-  }, [authStatus, router]);
-
-  React.useEffect(() => {
     if (isAuthFailure) {
       router.replace(path.SIGN_IN);
+      return;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authStatus]);
+
+    if (!allowedRoles) {
+      alert('접근 권한이 없습니다.');
+      router.replace(path.SIGN_IN);
+      return;
+    }
+
+    if (role && !allowedRoles.includes(role)) {
+      alert('접근 권한이 없습니다.');
+      router.replace(path.SIGN_IN);
+    }
+  }, [allowedRoles, role]);
 
   if (isAuthIdle && showLoading) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   if (isAuthenticated) {
