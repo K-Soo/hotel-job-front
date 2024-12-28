db98ab3da67a7429f2c59848dc3624a0f841f3ff
K-Soo <kanabun102@naver.com>
2024-12-29 04:22:21 +0900

    feat: Layout 컴포넌트에서 동적 임포트를 사용하여 SSR 비활성화

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,15 +1,11 @@
 import styled from 'styled-components';
-import Loading from '@/components/common/Loading';
 import { useRecoilValue } from 'recoil';
 import { loadingAtom } from '@/recoil/loading';
 import { daumPostAtom } from '@/recoil/daumPost';
 import { bottomSheetAtom } from '@/recoil/bottomSheet';
-import AccountBottomSheet from '@/components/common/AccountBottomSheet';
-import DaumPost from '@/components/common/DaumPost';
-import Alert from '@/components/common/Alert';
-import Confirm from '@/components/common/Confirm';
 import Modal from '@/components/common/Modal';
 import { alertWithConfirmSelector } from '@/recoil/alertWithConfirm';
+import dynamic from 'next/dynamic';
 
 export { Footer } from '@/components/layout/footer';
 export { Main } from '@/components/layout/main';
@@ -19,6 +15,12 @@ export { DesktopNavigation } from '@/components/layout/header/desktopNavigation'
 export { MobileNavigation } from '@/components/layout/header/mobileNavigation';
 export { EmployerHeader } from '@/components/layout/header/employerHeader';
 export { EmployerAside } from '@/components/layout/aside/EmployerAside';
+
+const DynamicNoSSRLoading = dynamic(() => import('@/components/common/Loading'), { ssr: false });
+const DynamicNoSSRDaumPost = dynamic(() => import('@/components/common/DaumPost'), { ssr: false });
+const DynamicNoSSRAccountBottomSheet = dynamic(() => import('@/components/common/AccountBottomSheet'), { ssr: false });
+const DynamicNoSSRAlert = dynamic(() => import('@/components/common/Alert'), { ssr: false });
+const DynamicNoSSRConfirm = dynamic(() => import('@/components/common/Confirm'), { ssr: false });
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -32,14 +34,14 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <S.Layout>
-      {loadingAtomValue.isLoading && <Loading />}
+      {loadingAtomValue.isLoading && <DynamicNoSSRLoading />}
 
-      {daumPostAtomValue.isOpen && <DaumPost />}
+      {daumPostAtomValue.isOpen && <DynamicNoSSRDaumPost />}
 
-      {bottomSheetAtomValue.isOpen && <AccountBottomSheet />}
+      {bottomSheetAtomValue.isOpen && <DynamicNoSSRAccountBottomSheet />}
 
-      {alertWithConfirmSelectorValue.type === 'ALERT' && <Alert />}
-      {alertWithConfirmSelectorValue.type === 'CONFIRM' && <Confirm />}
+      {alertWithConfirmSelectorValue.type === 'ALERT' && <DynamicNoSSRAlert />}
+      {alertWithConfirmSelectorValue.type === 'CONFIRM' && <DynamicNoSSRConfirm />}
 
       {children}
     </S.Layout>
