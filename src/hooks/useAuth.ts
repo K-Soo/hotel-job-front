ec49b16de80b223b7199ee9138ab92fc5862b200
K-Soo <kanabun102@naver.com>
2024-12-29 04:10:38 +0900

    feat: useAuth 훅에 역할(role) 상태 추가

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -13,6 +13,7 @@ export default function useAuth() {
     isAuthIdle,
     isAuthFailure,
 
+    role: authAtomState.role,
     authAtomState,
     setAuthAtomState,
     authStatus: authAtomState.status,
