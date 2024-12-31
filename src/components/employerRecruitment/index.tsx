c31cc7be52f11fb4b47fa0879d3ebb4df09bb7aa
K-Soo <kanabun102@naver.com>
2024-12-31 17:17:48 +0900

    feat: 새로운 EmployerRecruitment 컴포넌트 추가

---
+++ src/components/employerRecruitment/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface EmployerRecruitmentProps {}
+
+export default function EmployerRecruitment({}: EmployerRecruitmentProps) {
+  return <S.EmployerRecruitment>index</S.EmployerRecruitment>;
+}
+
+const S = {
+  EmployerRecruitment: styled.section``,
+};
