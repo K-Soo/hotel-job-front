68f6fac841846ae4924a192d83cfcb7892e35391
K-Soo <kanabun102@naver.com>
2024-12-31 17:20:03 +0900

    feat: 고용주 후보 페이지 레이아웃 추가

--- src/pages/employer/candidate/index.tsx
+++ src/pages/employer/candidate/index.tsx
@@ -1,6 +1,17 @@
+import Layout, { EmployerMain, EmployerHeader } from '@/components/layout';
+
 export default function EmployerCandidatePage() {
   return <>index</>;
 }
+
+EmployerCandidatePage.getLayout = (page: React.ReactElement) => {
+  return (
+    <Layout>
+      <EmployerHeader />
+      <EmployerMain>{page}</EmployerMain>
+    </Layout>
+  );
+};
 
 EmployerCandidatePage.authentication = true;
 EmployerCandidatePage.allowedRoles = ['EMPLOYER'];
