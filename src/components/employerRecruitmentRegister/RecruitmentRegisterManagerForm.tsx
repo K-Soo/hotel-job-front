3aec603e14ee0dbf5a2363b08d2a2228b02fd008
K-Soo <kanabun102@naver.com>
2025-01-02 05:23:45 +0900

    feat: RecruitmentRegisterManagerForm 컴포넌트 추가 및 기본 구조 구현

---
+++ src/components/employerRecruitmentRegister/RecruitmentRegisterManagerForm.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import HorizontalFormWrapper from '@/components/common/form/HorizontalFormWrapper';
+import FormInputB from '@/components/common/form/FormInputB';
+import FormCheckbox from '@/components/common/form/FormCheckbox';
+
+interface RecruitmentRegisterManagerFormProps {}
+
+export default function RecruitmentRegisterManagerForm({}: RecruitmentRegisterManagerFormProps) {
+  return (
+    <S.RecruitmentRegisterManagerForm>
+      <HorizontalFormWrapper>
+        <FormInputB<any> required label="담당자명" name="a" placeholder="담당자명" maxWidth="400px" />
+        <FormCheckbox label="비공개" name="담당자명" visibleIcon={false} margin="0 0 0 15px" />
+      </HorizontalFormWrapper>
+
+      <HorizontalFormWrapper>
+        <FormInputB<any> required label="연락처" name="a" placeholder="연락처" maxWidth="400px" />
+        <FormCheckbox label="비공개" name="연락처" visibleIcon={false} margin="0 0 0 15px" />
+      </HorizontalFormWrapper>
+
+      <HorizontalFormWrapper>
+        <FormInputB<any> required label="이메일" name="a" placeholder="이메일" maxWidth="400px" />
+        <FormCheckbox label="비공개" name="이메일" visibleIcon={false} margin="0 0 0 15px" />
+      </HorizontalFormWrapper>
+    </S.RecruitmentRegisterManagerForm>
+  );
+}
+
+const S = {
+  RecruitmentRegisterManagerForm: styled.div``,
+};
