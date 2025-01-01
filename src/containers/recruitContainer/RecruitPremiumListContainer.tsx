a21faa4daaa078a2a7399470e418850fbb5b57cb
K-Soo <kanabun102@naver.com>
2025-01-02 05:24:44 +0900

    feat: 채용 프리미엄 리스트 컨테이너의 모집 배열 길이 6에서 9로 수정

--- src/containers/recruitContainer/RecruitPremiumListContainer.tsx
+++ src/containers/recruitContainer/RecruitPremiumListContainer.tsx
@@ -4,7 +4,7 @@ import RecruitPremiumList from '@/components/recruit/RecruitPremiumList';
 import RecruitPremiumCard from '@/components/recruit/RecruitPremiumCard';
 
 export default function RecruitPremiumListContainer() {
-  const recruitArray = Array.from({ length: 6 });
+  const recruitArray = Array.from({ length: 9 });
   const { isTablet } = useResponsive();
 
   return (
