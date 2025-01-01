11bca5596501f0d89659e0ce6d83d347e7cbbd49
K-Soo <kanabun102@naver.com>
2025-01-02 05:22:51 +0900

    feat: BusinessInfo 컴포넌트 추가 및 기본 구조 구현

---
+++ src/components/employer/BusinessInfo.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface BusinessInfoProps {}
+
+export default function BusinessInfo({}: BusinessInfoProps) {
+  return <S.BusinessInfo>BusinessInfo</S.BusinessInfo>;
+}
+
+const S = {
+  BusinessInfo: styled.div``,
+};
