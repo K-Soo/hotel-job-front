07ff330f860e84353890a23052354a986d437e94
K-Soo <kanabun102@naver.com>
2025-01-02 05:20:08 +0900

    feat: FormCheckbox 컴포넌트 개선 및 체크 상태 시 스타일 추가

--- src/components/common/form/FormCheckbox.tsx
+++ src/components/common/form/FormCheckbox.tsx
@@ -3,7 +3,6 @@ import { useFormContext, Path, FieldValues } from 'react-hook-form';
 import FormError from '@/components/common/form/FormError';
 import { get } from 'lodash';
 import React from 'react';
-
 interface FormCheckboxProps<T> {
   name: Path<T>;
   label: string;
@@ -16,7 +15,7 @@ interface FormCheckboxProps<T> {
 export default function FormCheckbox<T extends FieldValues>({
   label,
   name,
-  visibleIcon,
+  visibleIcon = true,
   margin,
   required,
   optional,
@@ -32,9 +31,9 @@ export default function FormCheckbox<T extends FieldValues>({
 
   return (
     <>
-      <S.FormCheckbox $margin={margin}>
+      <S.FormCheckbox $margin={margin} $active={!!watchValue}>
         <div className="wrapper">
-          <input id={`FormCheckbox-${name}`} type="checkbox" checked={watchValue} {...register(name)} />
+          <input id={`FormCheckbox-${name}`} type="checkbox" checked={!!watchValue} {...register(name)} />
           <label htmlFor={`FormCheckbox-${name}`}>
             <p className="label-text">
               {required && <span className="label-text__required">[필수]</span>}
@@ -43,7 +42,7 @@ export default function FormCheckbox<T extends FieldValues>({
             </p>
           </label>
         </div>
-        {true && <i className="view">보기</i>}
+        {visibleIcon && <i className="view">보기</i>}
       </S.FormCheckbox>
       {error && <FormError errors={errors} name={name} />}
     </>
@@ -51,13 +50,14 @@ export default function FormCheckbox<T extends FieldValues>({
 }
 
 const S = {
-  FormCheckbox: styled.div<{ $margin?: string }>`
+  FormCheckbox: styled.div<{ $margin?: string; $active?: boolean }>`
     margin: ${(props) => props.$margin || 0};
     display: flex;
     align-items: center;
     justify-content: space-between;
     user-select: none;
     font-size: 13px;
+    cursor: pointer;
     .view {
       white-space: nowrap;
       margin-left: 10px;
@@ -74,6 +74,7 @@ const S = {
       display: flex;
       align-items: center;
       justify-content: space-between;
+
       line-height: 1.2;
       :hover {
         color: ${(props) => props.theme.colors.blue700};
@@ -81,6 +82,13 @@ const S = {
       .label-text {
         font-weight: 300;
         cursor: pointer;
+        word-break: keep-all;
+        ${(props) =>
+          props.$active &&
+          `
+          color: ${props.theme.colors.blue700};
+          font-weight: 400;
+        `};
         &__required {
           color: ${(props) => props.theme.colors.blue700};
           padding-right: 2px;
@@ -127,6 +135,21 @@ const S = {
       input[type='checkbox']:checked + label:before {
         background-color: ${(props) => props.theme.colors.blue700};
         border: 2px solid ${(props) => props.theme.colors.blue700};
+      }
+
+      /* 체크된 상태 */
+      input[type='checkbox']:checked + label:after {
+        content: '';
+        position: absolute;
+        left: 3px;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 16px;
+        height: 17px;
+        background-image: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M9 16.2l-5.2-5.2L3 12.8l6 6 12-12-1.4-1.4-10.6 10.8z"/></svg>');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
       }
     }
   `,
