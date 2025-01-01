d340da9c539f33ddee503420f9b460933a312886
K-Soo <kanabun102@naver.com>
2025-01-02 05:25:28 +0900

    feat: 고용주 계정 페이지에 컨테이너 컴포넌트 추가

--- src/pages/employer/account/index.tsx
+++ src/pages/employer/account/index.tsx
@@ -1,7 +1,7 @@
 import Layout, { EmployerMain, EmployerHeader } from '@/components/layout';
-
+import EmployerAccountContainer from '@/containers/employerAccountContainer';
 export default function EmployerAccountPage() {
-  return <>index</>;
+  return <EmployerAccountContainer />;
 }
 
 EmployerAccountPage.getLayout = (page: React.ReactElement) => {
