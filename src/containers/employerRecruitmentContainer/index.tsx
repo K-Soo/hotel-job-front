f48b7a986b5581c3321a47caf7b08a4fbeffecc8
K-Soo <kanabun102@naver.com>
2025-01-02 05:24:30 +0900

    feat: 고용주 채용 컨테이너에 상태 표시줄 및 필터 버튼 추가

--- src/containers/employerRecruitmentContainer/index.tsx
+++ src/containers/employerRecruitmentContainer/index.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import EmployerRecruitment from '@/components/employerRecruitment';
+import RecruitmentStatusBar from '@/components/employerRecruitment/RecruitmentStatusBar';
+import RecruitmentTable from '@/components/employerRecruitment/RecruitmentTable';
+import { useRouter } from 'next/router';
+import Select from '@/components/common/style/Select';
+import { productUseDateOptions } from '@/constants/options';
+import EmployerTemplateForm from '@/components/common/employer/EmployerTemplateForm';
+import RecruitmentFilterButton from '@/components/employerRecruitment/RecruitmentFilterButton';
 
 export default function EmployerRecruitmentContainer() {
-  return <EmployerRecruitment />;
+  const router = useRouter();
+
+  const handleClickRecruitmentItem = (value: number) => {
+    router.push(`/employer/recruitment/${value}`);
+  };
+
+  return (
+    <EmployerRecruitment>
+      <RecruitmentStatusBar />
+      <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '20px 0' }}>
+        <RecruitmentFilterButton label="삭제" margin="0 15px 0 0" />
+        <RecruitmentFilterButton label="삭제" margin="0 15px 0 0" />
+        <RecruitmentFilterButton label="삭제" />
+      </div>
+
+      <RecruitmentTable>
+        <RecruitmentTable.Header />
+        <RecruitmentTable.Body handleClickRecruitmentItem={handleClickRecruitmentItem} />
+      </RecruitmentTable>
+
+      <div>page</div>
+    </EmployerRecruitment>
+  );
 }
