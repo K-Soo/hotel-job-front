9cd3c0a691a7978b00b3c2dc3e23e17694e48f89
K-Soo <kanabun102@naver.com>
2025-01-02 05:20:45 +0900

    feat: HorizontalFormWrapper 컴포넌트에 레이블, 필수 여부 및 테두리 속성 추가

--- src/components/common/form/HorizontalFormWrapper.tsx
+++ src/components/common/form/HorizontalFormWrapper.tsx
@@ -1,16 +1,45 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface HorizontalFormWrapperProps {
   children: React.ReactNode;
+  label?: string;
+  required?: boolean;
+  border?: string;
 }
 
-export default function HorizontalFormWrapper({ children }: HorizontalFormWrapperProps) {
-  return <S.HorizontalFormWrapper>{children}</S.HorizontalFormWrapper>;
+export default function HorizontalFormWrapper({ label, required, border, children }: HorizontalFormWrapperProps) {
+  return (
+    <S.HorizontalFormWrapper border={border}>
+      {label && <S.FormLabel required={required}>{label}</S.FormLabel>}
+      {children}
+    </S.HorizontalFormWrapper>
+  );
 }
 
 const S = {
-  HorizontalFormWrapper: styled.div`
-    padding: 15px 0;
+  HorizontalFormWrapper: styled.div<{ border?: string }>`
+    padding: 15px 0 15px 15px;
     border-bottom: 1px solid ${(props) => props.theme.colors.gray300};
+    border-bottom: ${(props) => props.border};
+    display: flex;
+    align-items: center;
+    min-height: 70px;
+  `,
+  FormLabel: styled.span<{ required?: boolean }>`
+    color: ${({ theme }) => theme.colors.gray700};
+    white-space: nowrap;
+    font-size: 14px;
+    flex-basis: 150px;
+    flex-shrink: 0;
+    ${(props) =>
+      props.required &&
+      css`
+        &::after {
+          content: '*';
+          margin-left: 2px;
+          vertical-align: top;
+          color: crimson;
+        }
+      `};
   `,
 };
