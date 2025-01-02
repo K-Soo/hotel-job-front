4b5f7c195c41e64bd10cda94bfb862175db3d9fe
K-Soo <kanabun102@naver.com>
2025-01-03 04:53:58 +0900

    feat: 인증 성공 메시지 및 채용 공고 등록 안내 추가

--- src/recoil/alertWithConfirm/index.tsx
+++ src/recoil/alertWithConfirm/index.tsx
@@ -2,10 +2,12 @@ import { atom, selector } from 'recoil';
 
 const TITLE_TEXT = {
   TITLE_1: '본인 인증으로 모든 서비스에 액세스하세요!',
+  TITLE_2: '회사 정보가 성공적으로 등록되었습니다!',
 } as const;
 
 const DESCRIPTION_TEXT = {
   DESC_1: '지금 인증만 하면 첫 공고를 무료로 등록할 수 있는 특별 쿠폰을 드립니다. 빠르게 시작하세요!',
+  DESC_2: '이제 채용 공고를 등록해 더 많은 인재를 만나보세요. 🎉',
 };
 
 type TTextKeys = keyof typeof TITLE_TEXT;
