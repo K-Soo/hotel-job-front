92325470be7165326a7dad7e3d993a0b3e036025
K-Soo <kanabun102@naver.com>
2025-01-03 04:53:51 +0900

    feat: 유효성 검사에 공백 체크 및 한글 입력 제한 추가

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -19,18 +19,48 @@ const MORE_File = (more: number) => `${more}개 이상 등록해주세요.`;
 const LESS_File = (less: number) => `${less}개까지 등록 가능합니다.`;
 
 const EMAIL_VALID_TEXT = '이메일 형식을 확인해주세요';
-const KO_VALID_TEXT = '한글만 입력가능합니다(자음,모음 제외)';
+const KO_VALID_TEXT = '한글만 입력가능합니다.';
 const KO_VALID_TEXT_1 = '모음 입력제한';
 const KO_VALID_TEXT_2 = '자음 입력제한';
-const KO_VALID_TEXT_3 = '한글은 허용되지않습니다';
-const KO_VALID_TEXT_4 = '한글입력만 허용됩니다';
-const FIRST_SPACES_VALID_TEXT = '첫글자 공백';
-const LAST_SPACES_VALID_TEXT = '마지막글자 공백';
+const FIRST_SPACES_VALID_TEXT = '앞 공백 있음';
+const LAST_SPACES_VALID_TEXT = '뒤 공백 있음';
 const SPECIAL_VALID_TEXT_1 = '특수문자는 허용되지않습니다';
 const NUMBER_VALID_TEXT = '숫자만 입력 가능합니다.';
+
+const SPACES_VALID_TEXT_1 = '공백불가';
 
 export const validation = {
   USER_ID: yup.string().required().matches(regex.userId, '아이디를 형식을 확인해주세요.'),
   PASSWORD: yup.string().required().matches(regex.password, '비밀번호 형식을 확인해주세요.'),
   PASSWORD_CONFIRM: yup.string().required(),
+  PHONE: yup.string().required().matches(regex.phone, '010으로 시작하는 "-"를 제외한 숫자'),
+  REQUIRED_EMAIL: () =>
+    yup
+      .string()
+      .required()
+      .email(EMAIL_VALID_TEXT)
+      .matches(regex.FIRST_SPACE, FIRST_SPACES_VALID_TEXT)
+      .matches(regex.LAST_SPACE, LAST_SPACES_VALID_TEXT)
+      .min(5, MORE_TEXT(5))
+      .max(40, LESS_TEXT(40)),
+  //필수 - 앞뒤 공백체크
+  REQUIRED_TEXT_1: ({ minLength, maxLength }: { minLength: number; maxLength: number }) =>
+    yup
+      .string()
+      .required()
+      .matches(regex.FIRST_SPACE, FIRST_SPACES_VALID_TEXT)
+      .matches(regex.LAST_SPACE, LAST_SPACES_VALID_TEXT)
+      .min(minLength, MORE_TEXT(minLength))
+      .max(maxLength, LESS_TEXT(maxLength)),
+  //필수 - 공백불가, 한글만가능
+  REQUIRED_TEXT_2: ({ minLength, maxLength }: { minLength: number; maxLength: number }) =>
+    yup
+      .string()
+      .required()
+      .matches(regex.gather, KO_VALID_TEXT_1)
+      .matches(regex.consonant, KO_VALID_TEXT_2)
+      .matches(regex.korean, KO_VALID_TEXT)
+      .matches(regex.allSpace, SPACES_VALID_TEXT_1)
+      .min(minLength, MORE_TEXT(minLength))
+      .max(maxLength, LESS_TEXT(maxLength)),
 };
