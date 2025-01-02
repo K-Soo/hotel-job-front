cb3902a040cad8b0ff2491fa3de8157a3750e487
K-Soo <kanabun102@naver.com>
2025-01-02 17:28:02 +0900

    feat: EmployerMain 컴포넌트에서 footer 제거 및 스타일 수정

--- src/components/layout/main/EmployerMain.tsx
+++ src/components/layout/main/EmployerMain.tsx
@@ -16,37 +16,21 @@ export function EmployerMain({ children }: EmployerMainProps) {
   return (
     <S.EmployerMain>
       {!isExcludedAsidePath && <EmployerAside />}
-      <div className="main-container">
-        {children}
-        <footer className="employer-footer">©2024 celestara</footer>
-      </div>
+      {children}
     </S.EmployerMain>
   );
 }
 
 const S = {
   EmployerMain: styled.main`
-    min-height: calc(100vh - 60px);
+    min-height: calc(100vh - 60px - 50px);
     display: flex;
     background-color: ${(props) => props.theme.colors.white};
-    .main-container {
+    & > section {
       flex: 1;
-      display: flex;
-      flex-direction: column;
-      & > section {
-        flex: 1;
-        padding: 30px;
-        /* overflow-y: auto; */
-      }
-      .employer-footer {
-        border-top: 1px solid ${(props) => props.theme.colors.gray200};
-        height: 50px;
-        display: flex;
-        align-items: center;
-        font-size: 11px;
-        padding: 0 15px;
-        color: ${(props) => props.theme.colors.gray500};
-      }
+      padding: 30px;
+      border-left: 1px solid ${(props) => props.theme.colors.gray200};
+      /* overflow-y: auto; */
     }
   `,
 };
