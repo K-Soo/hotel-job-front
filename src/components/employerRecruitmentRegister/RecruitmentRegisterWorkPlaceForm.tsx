d8d6a08a9c722acf8e3e73580e81a99b52a34a9b
K-Soo <kanabun102@naver.com>
2025-01-02 17:27:42 +0900

    feat: RecruitmentRegisterWorkPlaceForm에서 객실수 필드 및 주소 필드 이름 수정

--- src/components/employerRecruitmentRegister/RecruitmentRegisterWorkPlaceForm.tsx
+++ src/components/employerRecruitmentRegister/RecruitmentRegisterWorkPlaceForm.tsx
@@ -2,23 +2,24 @@ import styled from 'styled-components';
 import HorizontalFormWrapper from '@/components/common/form/HorizontalFormWrapper';
 import FormInputB from '@/components/common/form/FormInputB';
 import Button from '@/components/common/style/Button';
+import FormNumberInput from '@/components/common/form/FormNumberInput';
 
 interface RecruitmentRegisterWorkPlaceFormProps {}
 
 export default function RecruitmentRegisterWorkPlaceForm({}: RecruitmentRegisterWorkPlaceFormProps) {
   return (
     <S.RecruitmentRegisterWorkPlaceForm>
-      <HorizontalFormWrapper>
-        <FormInputB<any> required label="객실수" name="a" placeholder="객실수" maxWidth="230px" />
+      <HorizontalFormWrapper label="객실수">
+        <FormNumberInput<any> name="객실수" maxWidth="100px" unit="개" maxLength={4} />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper>
-        <FormInputB<any> required label="기본주소" name="a" placeholder="기본주소" maxWidth="500px" disabled />
+        <FormInputB<any> required label="기본주소" name="기본주소" placeholder="기본주소" maxWidth="500px" disabled />
         <Button label="검색" variant="primary" height="40px" width="100px" margin="0 0 0 15px" />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper>
-        <FormInputB<any> required label="상세주소" name="a" placeholder="상세주소" maxWidth="620px" />
+        <FormInputB<any> required label="상세주소" name="상세주소" placeholder="상세주소" maxWidth="620px" />
       </HorizontalFormWrapper>
     </S.RecruitmentRegisterWorkPlaceForm>
   );
