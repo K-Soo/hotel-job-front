5edcaf47de885dfca9a4c5639a12a1c680c72453
K-Soo <kanabun102@naver.com>
2025-01-03 04:53:35 +0900

    feat: 서버 사이드 렌더링을 위한 인증 로직 추가 및 사용자 역할에 따른 리다이렉트 처리

--- src/pages/employer/setup/company.tsx
+++ src/pages/employer/setup/company.tsx
@@ -1,9 +1,56 @@
+import React from 'react';
 import Layout, { EmployerMain, EmployerHeader, EmployerFooter } from '@/components/layout';
 import EmployerSetupCompanyContainer from '@/containers/employerSetupCompanyContainer';
+import path from '@/constants/path';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { Post } from '@/apis';
 
 export default function EmployerSetupCompanyPage() {
   return <EmployerSetupCompanyContainer />;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  const cookieString = context.req ? context.req.headers?.cookie : undefined;
+
+  const requestHeader = {
+    headers: {
+      Cookie: cookieString,
+    },
+    withCredentials: true,
+  };
+
+  try {
+    const responseAccessToken = await Post.requestAccessToken({}, requestHeader);
+    if (!responseAccessToken) throw new Error();
+
+    const responseUserInfo = await Post.authMe({}, requestHeader);
+    if (!responseUserInfo) throw new Error();
+    if (responseUserInfo.result.role !== 'EMPLOYER') {
+      return {
+        redirect: {
+          destination: path.HOME,
+          permanent: false,
+        },
+      };
+    }
+    if (responseUserInfo.result.companyVerificationStatus === 'VERIFIED') {
+      return {
+        redirect: {
+          destination: path.EMPLOYER,
+          permanent: false,
+        },
+      };
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 EmployerSetupCompanyPage.getLayout = (page: React.ReactElement) => {
   return (
