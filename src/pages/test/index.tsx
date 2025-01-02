5879bfc5d882482387b2ddcfdd455ad4000b56a7
K-Soo <kanabun102@naver.com>
2025-01-03 04:53:37 +0900

    feat: 상태 API 호출 기능 추가 및 불필요한 코드 제거

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,28 +1,26 @@
 import axios from 'axios';
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
-import { Get } from '@/apis';
+import environment from '@/environment';
 
 export default function TestPage() {
-  const fetchPosts = async () => {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    return data;
+  const statusUrl = `${environment.nationBusinessUrl}?serviceKey=${environment.nationTextServiceKey}&returnType=JSON`;
+
+  const statusBody = {
+    b_no: ['1073790473'],
   };
 
-  const { data, isError, isLoading, status, isSuccess } = useQuery({
-    queryFn: () => fetchPosts(),
-    queryKey: ['fetchPosts'],
-    staleTime: 1000 * 60 * 5,
-    gcTime: 1000 * 60 * 5,
-    placeholderData: keepPreviousData,
-  });
-  console.log('status: ', status);
-  console.log('isLoading: ', isLoading);
-
-  console.log('테스트 API : ', data);
+  const fetchStatus = async () => {
+    try {
+      const response = await axios.post(statusUrl, statusBody);
+      console.log('response: ', response);
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
 
   return (
     <>
-      <button>유저정보</button>
+      {/* <button onClick={() => fetchPosts()}>유저정보</button> */}
+      <button onClick={() => fetchStatus()}>상태</button>
     </>
   );
 }
