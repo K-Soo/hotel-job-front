da74ecbdd627b81d08ad9ff1a5fd4a20edad22ad
K-Soo <kanabun102@naver.com>
2025-01-03 21:42:09 +0900

    feat: FormError 컴포넌트에 오류 유형에 따른 스타일 적용 추가

--- src/components/common/form/FormError.tsx
+++ src/components/common/form/FormError.tsx
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 import { ErrorMessage } from '@hookform/error-message';
 import { FieldErrors, FieldValues } from 'react-hook-form';
+import { get } from 'lodash';
 
 interface FormErrorProps {
   name: string;
@@ -10,18 +11,28 @@ interface FormErrorProps {
 }
 
 export default function FormError({ name, errors, margin, style }: FormErrorProps) {
+  const type = get(errors, `${name}.type`);
+
   return (
-    <S.FormError $margin={margin} style={style}>
+    <S.FormError $margin={margin} style={style} $type={type}>
       <ErrorMessage errors={errors} name={name} />
     </S.FormError>
   );
 }
 
 const S = {
-  FormError: styled.div<{ $margin?: string }>`
+  FormError: styled.div<{ $margin?: string; $type?: any }>`
     margin: ${(props) => (props.$margin ? props.$margin : '1.5px 0 0 0')};
     height: 12px;
     font-size: 11px;
     color: crimson;
+
+    ${(props) =>
+      props.$type === 'available' &&
+      css`
+        font-size: 13px;
+        color: #36b700;
+        font-weight: 500;
+      `};
   `,
 };
