e66d283194cf2e1600e93896e77aa0612971c6e9
K-Soo <kanabun102@naver.com>
2025-01-03 21:51:27 +0900

    feat: SOCIAL_URL 상수 정의를 위한 social.ts 파일 삭제

--- src/constants/social.ts
+++
@@ -1,9 +0,0 @@
-import environment from "@/environment";
-
-interface ISocialUrl {
-  [index: string]: string;
-}
-
-export const SOCIAL_URL: ISocialUrl = {
-  kakao: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${environment.kakaoClientId}&redirect_uri=${environment.kakaoRedirectUrl}`,
-} as const;
