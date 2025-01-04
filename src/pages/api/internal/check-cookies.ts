f8972a327cc70ba3544512c2aae18282c4400c4c
K-Soo <kanabun102@naver.com>
2025-01-04 22:05:57 +0900

    feat: 쿠키 확인 로깅 추가

--- src/pages/api/internal/check-cookies.ts
+++ src/pages/api/internal/check-cookies.ts
@@ -4,6 +4,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const cookies = req.cookies;
+
+    console.log('cookies: ', cookies);
     const isExistRefreshCookie = cookies['refresh_token'] ? true : false;
     res.status(200).json({ result: isExistRefreshCookie });
   } catch (error) {
