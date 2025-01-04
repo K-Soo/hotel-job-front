7674ccd87014b37ea12fe32bcec5620bf0b611b5
K-Soo <kanabun102@naver.com>
2025-01-04 18:10:40 +0900

    feat: health check

--- src/containers/homeContainer/index.tsx
+++ src/containers/homeContainer/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Home from '@/components/home';
 import RecruitSearchPanel from '@/components/common/RecruitSearchPanel';
+import { Get } from '@/apis';
 
 export default function HomeContainer() {
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const response = await Get.getHealth();
+        console.log('health api : ', response);
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    })();
+  }, []);
   return (
     <Home>
       <RecruitSearchPanel />
