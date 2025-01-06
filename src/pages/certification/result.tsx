cdbc7842207fb089678a535652de9301604146d2
K-Soo <kanabun102@naver.com>
2025-01-07 04:44:01 +0900

    feat: 인증 결과 처리 API 및 페이지 추가

--- src/pages/certification/result.tsx
+++ src/pages/certification/result.tsx
@@ -1,3 +1,26 @@
-export default function resultCertificationPage() {
+import React from 'react';
+import { GetServerSideProps, InferGetServerSidePropsType, GetServerSidePropsContext } from 'next';
+
+export default function ResultCertificationPage({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log('props: ', data);
+
+  React.useEffect(() => {
+    if (data) {
+      window.opener?.postMessage(JSON.stringify({ type: 'CERTIFICATION_RESULT', result: data }), '*');
+      // 창 닫기
+      window.close();
+    }
+  }, [data]);
+
   return <>result</>;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  const body = context.req;
+  console.log('body: ', body);
+  return {
+    props: {
+      data: body,
+    },
+  };
+};
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -8,25 +8,6 @@ import { certificationModalAtom } from '@/recoil/certification';
 export default function TestPage() {
   const [certificationModalAtomState, setCertificationModalAtomState] = useRecoilState(certificationModalAtom);
 
-  // const startCertification = async () => {
-  //   try {
-  //     // 백엔드에서 데이터 요청
-  //     const response = await Post.startCertification();
-
-  //     if (response.result.status !== 'success') {
-  //       alert('인증 요청에 실패했습니다.');
-  //       return;
-  //     }
-  //     // 인증 페이지로 라우팅
-  //     router.push({
-  //       pathname: '/certification',
-  //       query: response.result.params,
-  //     });
-  //   } catch (error) {
-  //     console.error('Error starting certification:', error);
-  //   }
-  // };
-
   return (
     <>
       {certificationModalAtomState.isOpen && <CertificationModal />}
