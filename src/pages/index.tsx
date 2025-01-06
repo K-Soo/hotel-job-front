a081e4d7de0e27d242667be06f6aa8900f7f899b
K-Soo <kanabun102@naver.com>
2025-01-07 02:55:08 +0900

    feat: 홈 페이지에서 불필요한 임포트 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import Layout, { Header, Footer, Main, DesktopNavigation, MobileNavigation } from '@/components/layout';
 import HomeContainer from '@/containers/homeContainer';
 import BottomNavigation from '@/components/layout/BottomNavigation';
-import environment from '@/environment';
-import Maintenance from '@/components/common/Maintenance';
+
 export default function HomePage() {
   return <HomeContainer />;
 }
