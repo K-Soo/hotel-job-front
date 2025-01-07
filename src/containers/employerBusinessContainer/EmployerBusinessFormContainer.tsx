5a4ce79360ce3752aef4624b03e550cb4047e134
K-Soo <kanabun102@naver.com>
2025-01-08 06:01:22 +0900

    feat: EmployerBusinessFormContainer에서 BusinessForm 타입을 EmployerBusinessForm으로 변경 및 주소 입력 필드 추가

--- src/containers/employerBusinessContainer/EmployerBusinessFormContainer.tsx
+++ src/containers/employerBusinessContainer/EmployerBusinessFormContainer.tsx
@@ -1,81 +1,45 @@
 import React from 'react';
 import { useForm, FormProvider, SubmitHandler, useFormContext } from 'react-hook-form';
 import { schema } from '@/utils';
-import { BusinessForm } from '@/types';
 import EmployerTemplateForm from '@/components/common/employer/EmployerTemplateForm';
 import FormInputB from '@/components/common/form/FormInputB';
 import HorizontalFormWrapper from '@/components/common/form/HorizontalFormWrapper';
 import Button from '@/components/common/style/Button';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { EmployerBusinessForm } from '@/types';
 
 export default function EmployerBusinessFormContainer() {
   const [modifyMode, setModifyMode] = React.useState<boolean>(false);
 
-  const methods = useForm<BusinessForm>({
-    resolver: yupResolver(schema.businessForm),
-    // disabled: disabled,
-    mode: 'onSubmit',
-    reValidateMode: 'onChange',
-    defaultValues: {},
-  });
-
   return (
-    <FormProvider {...methods}>
-      <EmployerTemplateForm.Content maxWidth="600px">
-        <EmployerTemplateForm.SubTitle title="사업자 정보" />
+    <EmployerTemplateForm.Content maxWidth="600px">
+      <EmployerTemplateForm.SubTitle title="사업자 정보" />
 
-        <HorizontalFormWrapper>
-          <FormInputB<BusinessForm>
-            disabled={modifyMode}
-            readOnly={!modifyMode}
-            label="사업자등록번호"
-            name="businessRegistrationNumber"
-            placeholder="사업자등록번호"
-          />
-        </HorizontalFormWrapper>
+      <HorizontalFormWrapper>
+        <FormInputB<EmployerBusinessForm>
+          readOnly={!modifyMode}
+          label="사업자등록번호"
+          name="businessRegistrationNumber"
+          placeholder="사업자등록번호"
+          mask="999-99-99999"
+        />
+      </HorizontalFormWrapper>
 
-        <HorizontalFormWrapper>
-          <FormInputB<BusinessForm> disabled={modifyMode} readOnly={!modifyMode} label="상호명" name="companyName" placeholder="상호명" />
-        </HorizontalFormWrapper>
+      <HorizontalFormWrapper>
+        <FormInputB<EmployerBusinessForm> readOnly={!modifyMode} label="상호명" name="companyName" placeholder="상호명" />
+      </HorizontalFormWrapper>
 
-        <HorizontalFormWrapper>
-          <FormInputB<BusinessForm>
-            // disabled={modifyMode}
-            readOnly={!modifyMode}
-            required
-            label="대표자명"
-            name="businessOwner"
-            placeholder="대표자명"
-          />
-        </HorizontalFormWrapper>
+      <HorizontalFormWrapper>
+        <FormInputB<EmployerBusinessForm> readOnly={!modifyMode} required label="대표자명" name="businessOwner" placeholder="대표자명" />
+      </HorizontalFormWrapper>
 
-        {/* <HorizontalFormWrapper>
-          <FormInputB<BusinessForm>
-            disabled={modifyMode}
-            readOnly={!modifyMode}
-            label="사업장 주소"
-            name=""
-            placeholder="주소"
-          />
-        </HorizontalFormWrapper> */}
+      <HorizontalFormWrapper>
+        <FormInputB<EmployerBusinessForm> readOnly={!modifyMode} label="사업장 주소" name="address" placeholder="주소" />
+      </HorizontalFormWrapper>
 
-        <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
-          {modifyMode && (
-            <>
-              <Button
-                label="취소"
-                variant="secondary"
-                width="100px"
-                height="35px"
-                onClick={() => setModifyMode(false)}
-                margin="0 15px 0 0"
-              />
-              <Button label="저장" variant="primary" width="100px" height="35px" onClick={() => setModifyMode(true)} />
-            </>
-          )}
-          {!modifyMode && <Button label="수정" variant="secondary100" width="100px" height="35px" onClick={() => setModifyMode(true)} />}
-        </div>
-      </EmployerTemplateForm.Content>
-    </FormProvider>
+      <HorizontalFormWrapper>
+        <FormInputB<EmployerBusinessForm> readOnly={!modifyMode} label="상세주소" name="addressDetail" placeholder="주소" />
+      </HorizontalFormWrapper>
+    </EmployerTemplateForm.Content>
   );
 }
