2d8cff47f21481ec1f033d46a5237f176c355a3a
K-Soo <kanabun102@naver.com>
2025-01-08 06:02:07 +0900

    feat: ErrorBoundary 추가하여 오류 처리 개선

--- src/pages/employer/business/index.tsx
+++ src/pages/employer/business/index.tsx
@@ -1,8 +1,14 @@
+import React from 'react';
 import Layout, { EmployerMain, EmployerHeader, EmployerFooter } from '@/components/layout';
 import EmployerBusinessContainer from '@/containers/employerBusinessContainer';
+import { ErrorBoundary, ErrorComponent } from '@/error';
 
 export default function EmployerBusinessPage() {
-  return <EmployerBusinessContainer />;
+  return (
+    <ErrorBoundary fallback={<ErrorComponent />}>
+      <EmployerBusinessContainer />
+    </ErrorBoundary>
+  );
 }
 
 EmployerBusinessPage.getLayout = (page: React.ReactElement) => {
