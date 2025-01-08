c1643b5a55fc0a79b06490e1f1111dbeae0059ff
K-Soo <kanabun102@naver.com>
2025-01-08 16:59:05 +0900

    feat: 고용주 계정 정보를 가져오는 쿼리 추가 및 폼 초기화 로직 개선

--- src/containers/employerAccountContainer/index.tsx
+++ src/containers/employerAccountContainer/index.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import EmployerAccount from '@/components/employerAccount';
 import { useForm, FormProvider, SubmitHandler, useFormContext } from 'react-hook-form';
+import queryKeys from '@/constants/queryKeys';
+import { Get } from '@/apis';
+import useFetchQuery from '@/hooks/useFetchQuery';
+import { EmployerAccountInfoForm } from '@/types';
 
 export default function EmployerAccountContainer() {
-  const methods = useForm<any>({
+  const methods = useForm<EmployerAccountInfoForm>({
     // disabled: disabled,
     mode: 'onSubmit',
     reValidateMode: 'onChange',
     defaultValues: {},
   });
+
+  const { data, isLoading } = useFetchQuery({
+    queryKey: [queryKeys.EMPLOYER_ACCOUNT],
+    queryFn: Get.employerAccountInfo,
+    options: {
+      throwOnError: true,
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+    },
+  });
+
+  React.useEffect(() => {
+    if (data) {
+      const { result } = data;
+      methods.setValue('userId', result.userId);
+      methods.setValue('nickname', result.nickname);
+      methods.setValue('createdAt', result.createdAt);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  console.log('계정정보 API : ', data);
 
   return (
     <FormProvider {...methods}>
