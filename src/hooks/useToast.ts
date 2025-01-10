1f8b7d74ca245fc0cf744e14d1c61a0ae2924a4f
K-Soo <kanabun102@naver.com>
2025-01-11 08:12:53 +0900

    feat: useToast 훅 추가 및 Recoil을 사용한 토스트 메시지 관리 구현

---
+++ src/hooks/useToast.ts
@@ -0,0 +1,17 @@
+import { useSetRecoilState, useRecoilState } from 'recoil';
+import { toastAtom, ToastItem } from '@/recoil/toast';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function useToast() {
+  const [toasts, setToasts] = useRecoilState(toastAtom);
+
+  const addToast = ({ message, type }: { message: string; type: ToastItem['type'] }) => {
+    if (toasts.length >= 3) {
+      return;
+    }
+
+    setToasts((prev) => [...prev, { id: uuidv4(), message, type }]);
+  };
+
+  return { addToast };
+}
