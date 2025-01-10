5a40fe83cb7a49ef4a5debe89867a8c9061d41d2
K-Soo <kanabun102@naver.com>
2025-01-11 08:12:14 +0900

    feat: RecruitmentRegisterDetailForm에서 경력 조건을 라디오 버튼으로 변경하고 외국인 체크 시 오류 클리어 로직 추가

--- src/components/employerRecruitmentRegister/RecruitmentRegisterDetailForm.tsx
+++ src/components/employerRecruitmentRegister/RecruitmentRegisterDetailForm.tsx
@@ -5,53 +5,97 @@ import FormSelect from '@/components/common/form/FormSelect';
 import FormInput from '@/components/common/form/FormInput';
 import FormNumberInput from '@/components/common/form/FormNumberInput';
 import FormCheckbox from '@/components/common/form/FormCheckbox';
+import FormRadio from '@/components/common/form/FormRadio';
 import { useFormContext } from 'react-hook-form';
 import RecruitmentDetailAdditional from '@/components/employerRecruitmentRegister/RecruitmentDetailAdditional';
 import { educationConditionLevelOptions, optionalPositionOptions } from '@/constants/options';
 import Button from '@/components/common/style/Button';
+import { CreateRecruitmentForm } from '@/types';
+import { experienceCondition } from '@/constants/recruitment';
+import React from 'react';
 
 interface RecruitmentRegisterDetailFormProps {
   setIsOpenJobModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function RecruitmentRegisterDetailForm({ setIsOpenJobModal }: RecruitmentRegisterDetailFormProps) {
-  const { watch } = useFormContext();
+  const { watch, clearErrors } = useFormContext<CreateRecruitmentForm>();
 
-  const departmentValue = watch('department');
-  const positionValue = watch('position');
+  // const departmentValue = watch('department');
+  // const positionValue = watch('position');
+
+  const isCheckedForeigner = watch('recruitmentInfo.nationality.foreigner');
+
+  React.useEffect(() => {
+    if (isCheckedForeigner) {
+      clearErrors('recruitmentInfo.nationality.korean');
+    }
+  }, [isCheckedForeigner, clearErrors]);
 
   return (
     <S.RecruitmentRegisterDetailForm>
-      <HorizontalFormWrapper>
+      {/* <HorizontalFormWrapper>
         <FormInputB<any> required label="직무" name="직무" placeholder="직무" />
         <Button label="선택" variant="primary" height="40px" width="100px" margin="0 0 0 15px" onClick={() => setIsOpenJobModal(true)} />
-      </HorizontalFormWrapper>
+      </HorizontalFormWrapper> */}
 
       <HorizontalFormWrapper label="경력" required>
-        <FormCheckbox label="경력" name="경력" visibleIcon={false} margin="0 30px 0 0" />
-        <FormCheckbox label="신입" name="신입" visibleIcon={false} margin="0 30px 0 0" />
-        <FormCheckbox label="경력무관" name="경력무관" visibleIcon={false} margin="0 30px 0 0" />
+        <FormRadio<CreateRecruitmentForm>
+          label="경력"
+          name="recruitmentInfo.experienceCondition"
+          margin="0 30px 0 0"
+          value={experienceCondition.EXPERIENCED}
+        />
+        <FormRadio label="신입" name="recruitmentInfo.experienceCondition" margin="0 30px 0 0" value={experienceCondition.NEWBIE} />
+        <FormRadio label="경력무관" name="recruitmentInfo.experienceCondition" value={experienceCondition.NOT_REQUIRED} />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper label="국적" required>
-        <FormCheckbox label="내국인" name="내국인" visibleIcon={false} margin="0 30px 0 0" />
-        <FormCheckbox label="외국인" name="외국인" visibleIcon={false} margin="0 30px 0 0" />
-        <FormInputB<any> name="결혼비자" placeholder="예시) H-2, F-2, 결혼비자, 영주권" maxWidth="320px" />
+        <FormCheckbox<CreateRecruitmentForm>
+          label="내국인"
+          name="recruitmentInfo.nationality.korean"
+          visibleIcon={false}
+          margin="0 30px 0 0"
+        />
+        <FormCheckbox<CreateRecruitmentForm>
+          label="외국인"
+          name="recruitmentInfo.nationality.foreigner"
+          visibleIcon={false}
+          margin="0 30px 0 0"
+        />
+      </HorizontalFormWrapper>
+
+      <HorizontalFormWrapper label="비자 조건" required style={{ display: isCheckedForeigner ? 'block' : 'none' }}>
+        <FormInputB<CreateRecruitmentForm>
+          name="recruitmentInfo.nationality.marriageVisa"
+          placeholder="예시) H-2, F-2, 결혼비자, 영주권"
+          maxLength={30}
+        />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper label="모집인원" required>
-        <FormNumberInput<any> name="모집인원" placeholder="모집인원" maxWidth="100px" unit="명" />
+        <FormNumberInput<CreateRecruitmentForm>
+          name="recruitmentInfo.recruitmentCapacity"
+          placeholder="모집인원"
+          maxWidth="100px"
+          unit="명"
+          maxLength={2}
+        />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper label="학력조건">
-        <FormSelect name="educationCondition" options={educationConditionLevelOptions} maxWidth="165px" />
+        <FormSelect<CreateRecruitmentForm>
+          name="recruitmentInfo.educationCondition"
+          options={educationConditionLevelOptions}
+          maxWidth="165px"
+        />
       </HorizontalFormWrapper>
 
-      <HorizontalFormWrapper label="항목추가">
+      {/* <HorizontalFormWrapper label="항목추가">
         <RecruitmentDetailAdditional />
-      </HorizontalFormWrapper>
+      </HorizontalFormWrapper> */}
 
-      {departmentValue && (
+      {/* {departmentValue && (
         <S.AdditionalContainer>
           {departmentValue && (
             <FormInput<any> label="근무부서" name="department.value" placeholder="근무하게 될 부서 또는 근무지" isFocusing />
@@ -63,7 +107,7 @@ export default function RecruitmentRegisterDetailForm({ setIsOpenJobModal }: Rec
         <S.AdditionalContainer>
           {positionValue && <FormSelect name="position" options={optionalPositionOptions} label="직급" />}
         </S.AdditionalContainer>
-      )}
+      )} */}
     </S.RecruitmentRegisterDetailForm>
   );
 }
