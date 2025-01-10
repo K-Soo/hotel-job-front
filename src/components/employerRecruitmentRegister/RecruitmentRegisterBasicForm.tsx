9e54f0dc2eb37fc16019e080c3f56ee060d6126f
K-Soo <kanabun102@naver.com>
2025-01-11 08:12:08 +0900

    feat: RecruitmentRegisterBasicForm에서 공고제목 필드의 타입을 CreateRecruitmentForm으로 변경 및 name 속성 수정

--- src/components/employerRecruitmentRegister/RecruitmentRegisterBasicForm.tsx
+++ src/components/employerRecruitmentRegister/RecruitmentRegisterBasicForm.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import HorizontalFormWrapper from '@/components/common/form/HorizontalFormWrapper';
 import FormInputB from '@/components/common/form/FormInputB';
 import FormCheckbox from '@/components/common/form/FormCheckbox';
+import { CreateRecruitmentForm } from '@/types';
 
 interface RecruitmentRegisterBasicFormProps {}
 
@@ -9,7 +10,7 @@ export default function RecruitmentRegisterBasicForm({}: RecruitmentRegisterBasi
   return (
     <S.RecruitmentRegisterBasicForm>
       <HorizontalFormWrapper>
-        <FormInputB<any> required label="공고제목" name="공고제목" placeholder="공고제목" />
+        <FormInputB<CreateRecruitmentForm> required label="공고제목" name="recruitmentTitle" placeholder="공고제목" maxLength={30} />
       </HorizontalFormWrapper>
     </S.RecruitmentRegisterBasicForm>
   );
