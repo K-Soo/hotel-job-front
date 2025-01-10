9f718703404547a196d37ca5090192ca77019c78
K-Soo <kanabun102@naver.com>
2025-01-11 08:11:45 +0900

    feat: 인증 정보 섹션에 휴대폰 인증 상태 표시 및 인증 버튼 추가

--- src/components/employerAccount/index.tsx
+++ src/components/employerAccount/index.tsx
@@ -4,10 +4,18 @@ import FormInputB from '@/components/common/form/FormInputB';
 import HorizontalFormWrapper from '@/components/common/form/HorizontalFormWrapper';
 import Button from '@/components/common/style/Button';
 import { EmployerAccountInfoForm } from '@/types';
+import { useFormContext } from 'react-hook-form';
+import { certificationModalAtom } from '@/recoil/certification';
+import { useSetRecoilState } from 'recoil';
 
 interface EmployerAccountProps {}
 
 export default function EmployerAccount({}: EmployerAccountProps) {
+  const { watch } = useFormContext<EmployerAccountInfoForm>();
+  const setCertificationModalAtomState = useSetRecoilState(certificationModalAtom);
+
+  const certificationValue = watch('certification');
+
   return (
     <S.EmployerAccount>
       <EmployerTemplateForm height="100%">
@@ -32,15 +40,19 @@ export default function EmployerAccount({}: EmployerAccountProps) {
         <EmployerTemplateForm.Content maxWidth="600px">
           <EmployerTemplateForm.SubTitle title="인증 정보" />
 
-          <HorizontalFormWrapper>
-            <FormInputB<EmployerAccountInfoForm>
-              label="휴대폰"
-              name="accountStatus"
-              placeholder="휴대폰 인증"
-              minWidth="auto"
-              readOnly={true}
+          <HorizontalFormWrapper position="relative">
+            <FormInputB<EmployerAccountInfoForm> label="휴대폰" name="accountStatus" minWidth="auto" readOnly={true} />
+            {!certificationValue && <S.PhoneCertTag>미 인증</S.PhoneCertTag>}
+
+            <Button
+              label="인증"
+              variant="tertiary"
+              width="80px"
+              height="40px"
+              margin="0 0 0 15px"
+              fontSize="14px"
+              onClick={() => setCertificationModalAtomState({ isOpen: true })}
             />
-            <Button label="변경" variant="tertiary" width="80px" height="40px" margin="0 0 0 15px" fontSize="14px" />
           </HorizontalFormWrapper>
 
           <HorizontalFormWrapper>
@@ -55,4 +67,16 @@ export default function EmployerAccount({}: EmployerAccountProps) {
 
 const S = {
   EmployerAccount: styled.section``,
+  PhoneCertTag: styled.span`
+    position: absolute;
+    left: 170px;
+    background-color: crimson;
+    color: ${({ theme }) => theme.colors.white};
+    padding: 2px 12px;
+    height: 25px;
+    border-radius: 15px;
+    font-size: 13px;
+    display: flex;
+    align-items: center;
+  `,
 };
