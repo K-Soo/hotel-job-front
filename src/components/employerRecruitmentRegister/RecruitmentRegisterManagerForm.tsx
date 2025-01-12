a38b4b40634d362305a2fbc53dcf2ea29c176de6
K-Soo <kanabun102@naver.com>
2025-01-12 16:31:15 +0900

    feat: RecruitmentRegisterManagerForm에서 담당자 정보 입력 필드 및 비공개 옵션 추가

--- src/components/employerRecruitmentRegister/RecruitmentRegisterManagerForm.tsx
+++ src/components/employerRecruitmentRegister/RecruitmentRegisterManagerForm.tsx
@@ -2,25 +2,45 @@ import styled from 'styled-components';
 import HorizontalFormWrapper from '@/components/common/form/HorizontalFormWrapper';
 import FormInputB from '@/components/common/form/FormInputB';
 import FormCheckbox from '@/components/common/form/FormCheckbox';
+import { CreateRecruitmentForm } from '@/types';
 
-interface RecruitmentRegisterManagerFormProps {}
-
-export default function RecruitmentRegisterManagerForm({}: RecruitmentRegisterManagerFormProps) {
+export default function RecruitmentRegisterManagerForm() {
   return (
     <S.RecruitmentRegisterManagerForm>
       <HorizontalFormWrapper>
-        <FormInputB<any> required label="담당자명" name="담당자명" placeholder="담당자명" maxWidth="400px" />
-        <FormCheckbox label="비공개" name="담당자명" visibleIcon={false} margin="0 0 0 15px" />
+        <FormInputB<CreateRecruitmentForm>
+          required
+          label="담당자명"
+          name="managerInfo.managerName"
+          placeholder="담당자명"
+          maxWidth="450px"
+          maxLength={10}
+        />
+        <FormCheckbox<CreateRecruitmentForm> label="비공개" name="managerInfo.isNamePrivate" visibleIcon={false} margin="0 0 0 15px" />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper>
-        <FormInputB<any> required label="연락처" name="연락처" placeholder="연락처" maxWidth="400px" />
-        <FormCheckbox label="비공개" name="연락처" visibleIcon={false} margin="0 0 0 15px" />
+        <FormInputB<CreateRecruitmentForm>
+          required
+          label="연락처"
+          name="managerInfo.managerNumber"
+          placeholder="연락처"
+          maxWidth="450px"
+          mask="999-9999-9999"
+        />
+        <FormCheckbox<CreateRecruitmentForm> label="비공개" name="managerInfo.isNumberPrivate" visibleIcon={false} margin="0 0 0 15px" />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper>
-        <FormInputB<any> required label="이메일" name="이메일" placeholder="이메일" maxWidth="400px" />
-        <FormCheckbox label="비공개" name="이메일" visibleIcon={false} margin="0 0 0 15px" />
+        <FormInputB<CreateRecruitmentForm>
+          required
+          label="이메일"
+          name="managerInfo.managerEmail"
+          placeholder="이메일"
+          maxWidth="450px"
+          maxLength={50}
+        />
+        <FormCheckbox<CreateRecruitmentForm> label="비공개" name="managerInfo.isEmailPrivate" visibleIcon={false} margin="0 0 0 15px" />
       </HorizontalFormWrapper>
     </S.RecruitmentRegisterManagerForm>
   );
