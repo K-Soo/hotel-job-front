03cc9673b36ba7132e618943a044460ca1cc69b8
K-Soo <kanabun102@naver.com>
2025-01-12 16:32:55 +0900

    fix: priceComma 함수에서 null 체크 추가

--- src/utils/priceComma.ts
+++ src/utils/priceComma.ts
@@ -1,5 +1,5 @@
 export const priceComma = (x: number | string | undefined): string => {
-  if (x === '' || x === undefined) {
+  if (x === '' || x === undefined || x === null) {
     return '0';
   }
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
