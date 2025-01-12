e28e08dadfd0eec92cc0c4f527b3dc32ff5c842b
K-Soo <kanabun102@naver.com>
2025-01-12 16:31:20 +0900

    feat: RecruitmentRegisterWorkPlaceForm에서 주소 입력 필드 및 검색 버튼 기능 추가

--- src/components/employerRecruitmentRegister/RecruitmentRegisterWorkPlaceForm.tsx
+++ src/components/employerRecruitmentRegister/RecruitmentRegisterWorkPlaceForm.tsx
@@ -3,23 +3,33 @@ import HorizontalFormWrapper from '@/components/common/form/HorizontalFormWrappe
 import FormInputB from '@/components/common/form/FormInputB';
 import Button from '@/components/common/style/Button';
 import FormNumberInput from '@/components/common/form/FormNumberInput';
+import { CreateRecruitmentForm } from '@/types';
+import { useSetRecoilState } from 'recoil';
+import { daumPostAtom } from '@/recoil/daumPost';
 
-interface RecruitmentRegisterWorkPlaceFormProps {}
+export default function RecruitmentRegisterWorkPlaceForm() {
+  const setDaumPostAtom = useSetRecoilState(daumPostAtom);
 
-export default function RecruitmentRegisterWorkPlaceForm({}: RecruitmentRegisterWorkPlaceFormProps) {
   return (
     <S.RecruitmentRegisterWorkPlaceForm>
       <HorizontalFormWrapper label="객실수">
-        <FormNumberInput<any> name="객실수" maxWidth="100px" unit="개" maxLength={4} />
+        <FormNumberInput<CreateRecruitmentForm> name="locationInfo.roomCount" maxWidth="100px" unit="개" maxLength={4} isComma />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper>
-        <FormInputB<any> required label="기본주소" name="기본주소" placeholder="기본주소" maxWidth="500px" disabled />
-        <Button label="검색" variant="primary" height="40px" width="100px" margin="0 0 0 15px" />
+        <FormInputB<CreateRecruitmentForm> required label="기본주소" name="locationInfo.address" placeholder="기본주소" disabled />
+        <Button
+          label="검색"
+          variant="primary"
+          height="40px"
+          width="100px"
+          margin="0 0 0 15px"
+          onClick={() => setDaumPostAtom({ isOpen: true })}
+        />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper>
-        <FormInputB<any> required label="상세주소" name="상세주소" placeholder="상세주소" maxWidth="620px" />
+        <FormInputB<CreateRecruitmentForm> required label="상세주소" name="locationInfo.addressDetail" placeholder="상세주소" />
       </HorizontalFormWrapper>
     </S.RecruitmentRegisterWorkPlaceForm>
   );
