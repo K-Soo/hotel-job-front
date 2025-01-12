46484159a6f385c98316a7f0d447522e7b727a75
K-Soo <kanabun102@naver.com>
2025-01-12 16:31:00 +0900

    refactor: RecruitmentRegister 컴포넌트에서 동적 폼 에디터를 정적 임포트로 변경 및 섹션 제목 수정

--- src/components/employerRecruitmentRegister/index.tsx
+++ src/components/employerRecruitmentRegister/index.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import RecruitmentRegisterProgressMenu from '@/components/employerRecruitmentRegister/RecruitmentRegisterProgressMenu';
 import RecruitmentRegisterBasicForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterBasicForm';
 import RecruitmentRegisterManagerForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterManagerForm';
 import RecruitmentRegisterWorkPlaceForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterWorkPlaceForm';
@@ -7,20 +6,15 @@ import RecruitmentRegisterJobConditionForm from '@/components/employerRecruitmen
 import RecruitmentRegisterDetailForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterDetailForm';
 import EmployerTemplateForm from '@/components/common/employer/EmployerTemplateForm';
 import RecruitmentPolicy from '@/components/common/employer/RecruitmentPolicy';
-import dynamic from 'next/dynamic';
-
-const DynamicFormEditor = dynamic(() => import('@/components/common/form/FormEditor'), { ssr: false });
+import FormEditor from '@/components/common/form/FormEditor';
+import { CreateRecruitmentForm } from '@/types';
 
 interface EmployerRecruitmentRegisterProps {
   setIsOpenJobModal: React.Dispatch<React.SetStateAction<boolean>>;
   children: React.ReactNode;
 }
 
-// let renderCount = 0;
-
 export default function EmployerRecruitmentRegister({ children, setIsOpenJobModal }: EmployerRecruitmentRegisterProps) {
-  // renderCount++;
-
   return (
     <S.EmployerRecruitmentRegister>
       <div className="register-container">
@@ -37,14 +31,14 @@ export default function EmployerRecruitmentRegister({ children, setIsOpenJobModa
               <RecruitmentRegisterDetailForm setIsOpenJobModal={setIsOpenJobModal} />
             </EmployerTemplateForm.Content>
 
-            {/* <EmployerTemplateForm.SubTitle title="상세 모집내용" size="large" id="detail" />
-            <EmployerTemplateForm.Content marginBottom="80px">
-              <DynamicFormEditor />
-            </EmployerTemplateForm.Content>
-
             <EmployerTemplateForm.SubTitle title="근무조건" size="large" id="condition" />
             <EmployerTemplateForm.Content marginBottom="80px">
               <RecruitmentRegisterJobConditionForm />
+            </EmployerTemplateForm.Content>
+
+            <EmployerTemplateForm.SubTitle title="상세 모집내용" size="large" id="detail" />
+            <EmployerTemplateForm.Content marginBottom="80px">
+              <FormEditor<CreateRecruitmentForm> name="content" />
             </EmployerTemplateForm.Content>
 
             <EmployerTemplateForm.SubTitle title="근무지 정보" size="large" id="place" />
@@ -55,7 +49,7 @@ export default function EmployerRecruitmentRegister({ children, setIsOpenJobModa
             <EmployerTemplateForm.SubTitle title="인사담당자 정보" size="large" id="manager" />
             <EmployerTemplateForm.Content>
               <RecruitmentRegisterManagerForm />
-            </EmployerTemplateForm.Content> */}
+            </EmployerTemplateForm.Content>
 
             <RecruitmentPolicy />
           </EmployerTemplateForm>
