db3cacad9fea29b77bc8f3c867fa3d8161ee7231
K-Soo <kanabun102@naver.com>
2025-01-12 16:30:54 +0900

    feat: 고용주 모집 상세 컴포넌트에서 폼 에디터 및 세부 정보 컴포넌트 통합

--- src/components/employerRecruitmentDetail/index.tsx
+++ src/components/employerRecruitmentDetail/index.tsx
@@ -1,16 +1,14 @@
+import { RecruitmentDetailForm } from '@/types';
 import styled from 'styled-components';
-import RecruitmentRegisterProgressMenu from '@/components/employerRecruitmentRegister/RecruitmentRegisterProgressMenu';
-import RecruitmentRegisterBasicForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterBasicForm';
+import FormEditor from '@/components/common/form/FormEditor';
 import RecruitmentDetailBasicForm from '@/components/employerRecruitmentDetail/RecruitmentDetailBasicForm';
-import RecruitmentRegisterManagerForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterManagerForm';
-import RecruitmentRegisterWorkPlaceForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterWorkPlaceForm';
+import RecruitmentDetailWorkPlaceForm from '@/components/employerRecruitmentDetail/RecruitmentDetailWorkPlaceForm';
+import RecruitmentDetailManagerForm from '@/components/employerRecruitmentDetail/RecruitmentDetailManagerForm';
+
 import RecruitmentRegisterJobConditionForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterJobConditionForm';
 import RecruitmentRegisterDetailForm from '@/components/employerRecruitmentRegister/RecruitmentRegisterDetailForm';
 import EmployerTemplateForm from '@/components/common/employer/EmployerTemplateForm';
 import RecruitmentPolicy from '@/components/common/employer/RecruitmentPolicy';
-import dynamic from 'next/dynamic';
-
-const DynamicFormEditor = dynamic(() => import('@/components/common/form/FormEditor'), { ssr: false });
 
 interface EmployerRecruitmentDetailProps {
   setIsOpenJobModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -34,25 +32,25 @@ export default function EmployerRecruitmentDetail({ children, setIsOpenJobModal 
               <RecruitmentRegisterDetailForm setIsOpenJobModal={setIsOpenJobModal} />
             </EmployerTemplateForm.Content>
 
-            {/* <EmployerTemplateForm.SubTitle title="상세 모집내용" size="large" id="detail" />
+            <EmployerTemplateForm.SubTitle title="상세 모집내용" size="large" id="detail" />
             <EmployerTemplateForm.Content marginBottom="80px">
-              <DynamicFormEditor />
-            </EmployerTemplateForm.Content> */}
+              <FormEditor<RecruitmentDetailForm> name="content" />
+            </EmployerTemplateForm.Content>
 
             {/* <EmployerTemplateForm.SubTitle title="근무조건" size="large" id="condition" />
             <EmployerTemplateForm.Content marginBottom="80px">
               <RecruitmentRegisterJobConditionForm />
-            </EmployerTemplateForm.Content>
+            </EmployerTemplateForm.Content> */}
 
             <EmployerTemplateForm.SubTitle title="근무지 정보" size="large" id="place" />
             <EmployerTemplateForm.Content marginBottom="80px">
-              <RecruitmentRegisterWorkPlaceForm />
+              <RecruitmentDetailWorkPlaceForm />
             </EmployerTemplateForm.Content>
 
             <EmployerTemplateForm.SubTitle title="인사담당자 정보" size="large" id="manager" />
             <EmployerTemplateForm.Content>
-              <RecruitmentRegisterManagerForm />
-            </EmployerTemplateForm.Content> */}
+              <RecruitmentDetailManagerForm />
+            </EmployerTemplateForm.Content>
 
             <RecruitmentPolicy />
           </EmployerTemplateForm>
