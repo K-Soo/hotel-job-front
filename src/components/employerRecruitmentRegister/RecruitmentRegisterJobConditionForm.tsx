5317039fc091abb5cd0f07c099350a26f707be6c
K-Soo <kanabun102@naver.com>
2025-01-12 16:31:18 +0900

    feat: RecruitmentRegisterJobConditionForm에서 근무형태 체크박스 및 급여 입력 필드 수정

--- src/components/employerRecruitmentRegister/RecruitmentRegisterJobConditionForm.tsx
+++ src/components/employerRecruitmentRegister/RecruitmentRegisterJobConditionForm.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 import HorizontalFormWrapper from '@/components/common/form/HorizontalFormWrapper';
 import FormCheckbox from '@/components/common/form/FormCheckbox';
@@ -7,29 +8,73 @@ import FormRadio from '@/components/common/form/FormRadio';
 import Button from '@/components/common/style/Button';
 import Icon from '@/icons/Icon';
 import RecruitmentJobConditionAdditional from '@/components/employerRecruitmentRegister/RecruitmentJobConditionAdditional';
+import RecruitmentDetailAdditional from '@/components/employerRecruitmentRegister/RecruitmentDetailAdditional';
 import { useFormContext } from 'react-hook-form';
 import { requiredSalaryTypeOptions } from '@/constants/options';
-import { SalaryType } from '@/types';
+import { CreateRecruitmentForm, SalaryTypeKeys } from '@/types';
 import FormNumberInput from '@/components/common/form/FormNumberInput';
 import MinimumWage from '@/components/common/employer/MinimumWage';
+import { salaryType } from '@/constants';
+import useDidMountEffect from '@/hooks/useDidMountEffect';
 
 interface RecruitmentRegisterJobConditionFormProps {}
 
 export default function RecruitmentRegisterJobConditionForm({}: RecruitmentRegisterJobConditionFormProps) {
-  const { watch } = useFormContext();
+  const { watch, setValue, setFocus, clearErrors } = useFormContext<CreateRecruitmentForm>();
 
-  const salaryTypeValue = watch('salaryType') as SalaryType;
+  const salaryTypeValue = watch('conditionInfo.salaryType') as SalaryTypeKeys;
+  const employmentTypeValue = watch('conditionInfo.employmentType');
+
+  // 근무형태 중 하나라도 선택되면 에러메시지 제거(정규직에만 에러 체크했음)
+  React.useEffect(() => {
+    const validEmploymentType = Object.values(employmentTypeValue).some((value) => value === true);
+    if (validEmploymentType) {
+      clearErrors('conditionInfo.employmentType.FULL_TIME');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [employmentTypeValue]);
+
+  useDidMountEffect(() => {
+    setValue('conditionInfo.salaryAmount', 0);
+    setFocus('conditionInfo.salaryAmount');
+  }, [salaryTypeValue]);
 
   return (
     <S.RecruitmentRegisterJobConditionForm>
       <HorizontalFormWrapper label="근무형태" required>
-        <FormCheckbox label="정규직" name="정규직" visibleIcon={false} margin="0 30px 0 0" />
-        <FormCheckbox label="계약직" name="계약직" visibleIcon={false} margin="0 30px 0 0" />
-        <FormCheckbox label="파출" name="파출" visibleIcon={false} margin="0 30px 0 0" />
-        <FormCheckbox label="아르바이트" name="아르바이트" visibleIcon={false} margin="0 30px 0 0" />
+        <FormCheckbox<CreateRecruitmentForm>
+          label="정규직"
+          name="conditionInfo.employmentType.FULL_TIME"
+          visibleIcon={false}
+          margin="0 30px 0 0"
+        />
+        <FormCheckbox<CreateRecruitmentForm>
+          label="계약직"
+          name="conditionInfo.employmentType.CONTRACT"
+          visibleIcon={false}
+          margin="0 30px 0 0"
+        />
+        <FormCheckbox<CreateRecruitmentForm>
+          label="파출"
+          name="conditionInfo.employmentType.DAILY_WORKER"
+          visibleIcon={false}
+          margin="0 30px 0 0"
+        />
+        <FormCheckbox<CreateRecruitmentForm>
+          label="아르바이트"
+          name="conditionInfo.employmentType.PART_TIME"
+          visibleIcon={false}
+          margin="0 30px 0 0"
+        />
+        <FormCheckbox<CreateRecruitmentForm>
+          label="인턴"
+          name="conditionInfo.employmentType.INTERN"
+          visibleIcon={false}
+          margin="0 30px 0 0"
+        />
       </HorizontalFormWrapper>
 
-      <HorizontalFormWrapper label="북리후생" required>
+      {/* <HorizontalFormWrapper label="북리후생" required>
         <FormTagBox name="ㅁㄴㅇ" placeholder="복리후생" maxWidth="378px" />
         <Button
           label="검색"
@@ -39,22 +84,33 @@ export default function RecruitmentRegisterJobConditionForm({}: RecruitmentRegis
           margin="0 0 0 15px"
           icon={<Icon name="Plus24x24" color="red" margin="0 5px 0 0" width="16px" height="16px" />}
         />
-      </HorizontalFormWrapper>
+      </HorizontalFormWrapper> */}
 
       <HorizontalFormWrapper label="급여" required border="none">
-        <FormArrayRadio options={requiredSalaryTypeOptions} name="salaryType" />
+        <FormArrayRadio<CreateRecruitmentForm> options={salaryType} name="conditionInfo.salaryType" />
       </HorizontalFormWrapper>
 
       <S.PriceBox>
-        <FormNumberInput<any> name="원" maxWidth="140px" unit="원" />
+        <FormNumberInput<CreateRecruitmentForm>
+          name="conditionInfo.salaryAmount"
+          maxWidth="140px"
+          unit="원"
+          isComma
+          margin="0 0 15px 0"
+          maxLength={11}
+        />
         <MinimumWage salaryType={salaryTypeValue} />
       </S.PriceBox>
 
-      <HorizontalFormWrapper label="항목추가">
+      {/* <HorizontalFormWrapper label="항목추가">
         <RecruitmentJobConditionAdditional />
-      </HorizontalFormWrapper>
+      </HorizontalFormWrapper> */}
 
-      <S.AdditionalContainer>
+      {/* <HorizontalFormWrapper label="항목추가">
+        <RecruitmentDetailAdditional />
+      </HorizontalFormWrapper> */}
+
+      {/* <S.AdditionalContainer>
         <div>
           <FormRadio margin="3px" name="jobWorking" label="주 5일 (월~금)" value="WEEKDAYS_5" />
           <FormRadio margin="3px" name="jobWorking" label="주 6일 (월~토)" value="WEEKDAYS_6" />
@@ -78,7 +134,7 @@ export default function RecruitmentRegisterJobConditionForm({}: RecruitmentRegis
           <FormRadio margin="3px" name="jobWorking" label="격일제 (24시간 야간 맞교대)" value="ALTERNATE_NIGHT_SHIFT" />
           <FormRadio margin="3px" name="jobWorking" label="격일제 (24시간 근무 2일 휴무)" value="ALTERNATE_2DAY_OFF" />
         </div>
-      </S.AdditionalContainer>
+      </S.AdditionalContainer> */}
     </S.RecruitmentRegisterJobConditionForm>
   );
 }
