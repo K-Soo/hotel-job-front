e8ef988ed1f9bfecacfaca90a94a488067e4781b
K-Soo <kanabun102@naver.com>
2025-01-13 13:03:17 +0900

    fix: 직무 선택 최대 개수를 3개에서 5개로 변경

--- src/components/common/employer/JobModal.tsx
+++ src/components/common/employer/JobModal.tsx
@@ -51,8 +51,8 @@ export default function JobModal<T extends FieldValues>({ name, setIsOpenJobModa
     const { checked, value } = event.target;
 
     if (checked) {
-      if (selectedJob.length >= 3) {
-        return addToast({ message: '직무는 최대 3개까지 선택 가능합니다.', type: 'warning' });
+      if (selectedJob.length >= 5) {
+        return addToast({ message: '직무는 최대 5개까지 선택 가능합니다.', type: 'warning' });
       }
 
       return setSelectedJob((prev) => [...prev, value as AllJobsKeyValuesKeys]);
