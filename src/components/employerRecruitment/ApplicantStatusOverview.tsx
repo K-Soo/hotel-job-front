2bd1a62a897c14881853528f2ef6ee6248384c16
K-Soo <kanabun102@naver.com>
2025-01-14 05:32:54 +0900

    feat: ApplicantStatusOverview 컴포넌트 추가 및 스타일 정의

---
+++ src/components/employerRecruitment/ApplicantStatusOverview.tsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+interface ApplicantStatusOverviewProps {}
+
+export default function ApplicantStatusOverview({}: ApplicantStatusOverviewProps) {
+  return (
+    <S.ApplicantStatusOverview>
+      <div className="column">
+        <span className="column__title">지원자</span>
+        <strong className="column__count">100</strong>
+      </div>
+      <div className="column">
+        <span className="column__title">열람</span>
+        <strong className="column__count">50</strong>
+      </div>
+      <div className="column">
+        <span className="column__title">미열람</span>
+        <strong className="column__count">50</strong>
+      </div>
+    </S.ApplicantStatusOverview>
+  );
+}
+
+const S = {
+  ApplicantStatusOverview: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 3px;
+    border-radius: 5px;
+    border: 1px solid ${(props) => props.theme.colors.gray300};
+    background-color: ${(props) => props.theme.colors.white};
+    .column {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      font-size: 13px;
+      &__title {
+        color: ${(props) => props.theme.colors.gray700};
+        font-size: 12px;
+      }
+      &__count {
+        padding-top: 2px;
+        font-weight: 500;
+        color: ${(props) => props.theme.colors.black200};
+      }
+    }
+  `,
+};
