30c26df94789df375f088f86ca5032121f71154a
K-Soo <kanabun102@naver.com>
2025-01-13 15:12:56 +0900

    feat: RecruitmentRegisterWorkPlaceForm에서 제출 중 버튼 비활성화 기능 추가

--- src/components/employerRecruitmentRegister/RecruitmentRegisterWorkPlaceForm.tsx
+++ src/components/employerRecruitmentRegister/RecruitmentRegisterWorkPlaceForm.tsx
@@ -6,14 +6,19 @@ import FormNumberInput from '@/components/common/form/FormNumberInput';
 import { CreateRecruitmentForm } from '@/types';
 import { useSetRecoilState } from 'recoil';
 import { daumPostAtom } from '@/recoil/daumPost';
+import { useFormContext } from 'react-hook-form';
 
 export default function RecruitmentRegisterWorkPlaceForm() {
   const setDaumPostAtom = useSetRecoilState(daumPostAtom);
 
+  const {
+    formState: { isSubmitting },
+  } = useFormContext<CreateRecruitmentForm>();
+
   return (
     <S.RecruitmentRegisterWorkPlaceForm>
       <HorizontalFormWrapper label="객실수">
-        <FormNumberInput<CreateRecruitmentForm> name="locationInfo.roomCount" maxWidth="100px" unit="개" maxLength={4} isComma />
+        <FormNumberInput<CreateRecruitmentForm> name="locationInfo.roomCount" maxWidth="100px" unit="개" maxLength={4} />
       </HorizontalFormWrapper>
 
       <HorizontalFormWrapper>
@@ -25,6 +30,7 @@ export default function RecruitmentRegisterWorkPlaceForm() {
           width="100px"
           margin="0 0 0 15px"
           onClick={() => setDaumPostAtom({ isOpen: true })}
+          disabled={isSubmitting}
         />
       </HorizontalFormWrapper>
 
