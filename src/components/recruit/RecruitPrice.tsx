317535abc3e87ea57ee7baa4c9f6cfd22d45b6da
K-Soo <kanabun102@naver.com>
2025-01-15 11:49:21 +0900

    feat: RecruitPrice 컴포넌트에 급여 타입 및 금액 렌더링 기능 추가

--- src/components/recruit/RecruitPrice.tsx
+++ src/components/recruit/RecruitPrice.tsx
@@ -1,20 +1,24 @@
-import styled from 'styled-components';
-
+import { salaryType } from '@/constants';
+import { SalaryTypeKeys } from '@/types';
+import styled, { css } from 'styled-components';
+import { priceComma } from '@/utils';
 interface RecruitPriceProps {
   fonSize?: string;
+  salaryAmount: number;
+  salary: SalaryTypeKeys;
 }
 
-export default function RecruitPrice({ fonSize }: RecruitPriceProps) {
+export default function RecruitPrice({ fonSize, salaryAmount, salary }: RecruitPriceProps) {
   return (
-    <S.RecruitPrice $fonSize={fonSize}>
-      <span className="pay-type">월급</span>
-      <span className="pay-price">5,200,000</span>
+    <S.RecruitPrice $fonSize={fonSize} $salary={salary}>
+      <span className="pay-type">{salaryType[salary]}</span>
+      <span className="pay-price">{priceComma(salaryAmount)}</span>
     </S.RecruitPrice>
   );
 }
 
 const S = {
-  RecruitPrice: styled.div<{ $fonSize?: string }>`
+  RecruitPrice: styled.div<{ $fonSize?: string; $salary: SalaryTypeKeys }>`
     background-color: ${(props) => props.theme.colors.gray100};
     padding: 4px 5px;
     border-radius: 4px;
@@ -23,10 +27,26 @@ const S = {
     .pay-type {
       letter-spacing: 1px;
       padding-right: 5px;
-      color: #ff501b; //월급
-      /* color: #00b0a6; //시급 */
-      /* color: #8050c8; //일급 */
-      /* color: #00a1ef; //연봉 */
+      ${(props) =>
+        props.$salary === 'ANNUAL' &&
+        css`
+          color: #00a1ef; //연봉
+        `};
+      ${(props) =>
+        props.$salary === 'MONTHLY' &&
+        css`
+          color: #ff501b; //월급
+        `};
+      ${(props) =>
+        props.$salary === 'DAILY' &&
+        css`
+          color: #8050c8; //일급
+        `};
+      ${(props) =>
+        props.$salary === 'HOURLY' &&
+        css`
+          color: #00b0a6; //시급
+        `};
     }
     .pay-price {
       letter-spacing: 0.3px;
