5275abfac0057a2dea8b8a46fb21d44640bd4d89
K-Soo <kanabun102@naver.com>
2025-01-15 22:17:19 +0900

    feat: RecruitDetailFavoriteShareBar 컴포넌트에 아이콘 추가 및 스타일 개선

--- src/components/recruitDetail/RecruitDetailFavoriteShareBar.tsx
+++ src/components/recruitDetail/RecruitDetailFavoriteShareBar.tsx
@@ -1,16 +1,29 @@
 import styled from 'styled-components';
+import Icon from '@/icons/Icon';
+import IconDimmed from '@/components/common/IconDimmed';
 
 interface RecruitDetailFavoriteShareBarProps {}
 
 export default function RecruitDetailFavoriteShareBar({}: RecruitDetailFavoriteShareBarProps) {
   return (
     <S.RecruitDetailFavoriteShareBar>
-      <span>즐겨찾기</span>
-      <button>공유하기</button>
+      <IconDimmed width="36px" height="36px" margin="0 15px 0 0">
+        <Icon name="Bookmark24x24" height="24px" width="24px" />
+      </IconDimmed>
+      <IconDimmed width="36px" height="36px">
+        <Icon name="Share24x24" height="24px" width="24px" />
+      </IconDimmed>
     </S.RecruitDetailFavoriteShareBar>
   );
 }
 
 const S = {
-  RecruitDetailFavoriteShareBar: styled.div``,
+  RecruitDetailFavoriteShareBar: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    svg {
+      color: #999;
+    }
+  `,
 };
