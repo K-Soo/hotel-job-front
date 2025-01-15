7fbf4d16a1031e50ddbdc845a6d3bfe5ab291195
K-Soo <kanabun102@naver.com>
2025-01-15 11:48:53 +0900

    feat: IconDimmed 컴포넌트에 onClick 및 padding 속성 추가, 스타일 수정

--- src/components/common/IconDimmed.tsx
+++ src/components/common/IconDimmed.tsx
@@ -1,25 +1,32 @@
+import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 interface IconDimmedProps {
+  onClick?: React.MouseEventHandler<HTMLElement> | undefined;
+  padding?: string;
   children: React.ReactNode;
 }
 
-export default function IconDimmed({ children }: IconDimmedProps) {
-  return <S.IconDimmed whileTap={{ scale: 0.95 }}>{children}</S.IconDimmed>;
+export default function IconDimmed({ onClick, padding, children }: IconDimmedProps) {
+  return (
+    <S.IconDimmed onClick={onClick} whileTap={{ scale: 0.95 }} $padding={padding}>
+      {children}
+    </S.IconDimmed>
+  );
 }
 
 const S = {
-  IconDimmed: styled(motion.span)`
+  IconDimmed: styled(motion.i)<{ $padding?: string }>`
     display: inline-block;
     border-radius: 50%;
-    padding: 4px;
+    padding: ${(props) => props.$padding || '4px'};
     justify-content: center;
     align-items: center;
     font-size: 0;
-    background-color: rgba(242, 244, 246, 0.8);
+    background-color: rgba(242, 244, 246, 0.7);
     &:hover {
-      background-color: rgba(242, 244, 246, 1);
+      background-color: rgba(242, 244, 246, 0.8);
       background-color: ${(props) => props.theme.colors.gray200};
     }
   `,
