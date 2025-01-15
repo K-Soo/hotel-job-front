c8f88733958756618e73ae14557bd2974f3997cb
K-Soo <kanabun102@naver.com>
2025-01-15 17:54:45 +0900

    feat: 채용공고 상세 API 호출 및 로딩 상태 처리 추가

--- src/containers/recruitDetailContainer/index.tsx
+++ src/containers/recruitDetailContainer/index.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import RecruitDetail from '@/components/recruitDetail';
+import { Get } from '@/apis';
+import useFetchQuery from '@/hooks/useFetchQuery';
+import queryKeys from '@/constants/queryKeys';
+import { useRouter } from 'next/router';
 
 export default function RecruitDetailContainer() {
-  return <RecruitDetail />;
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const { data, isLoading, isSuccess } = useFetchQuery({
+    queryKey: [queryKeys.RECRUIT_DETAIL],
+    queryFn: Get.recruitDetail,
+    options: {
+      enabled: !!slug,
+    },
+    requestQuery: {
+      id: slug as string,
+    },
+  });
+  console.log('채용공고 상세 API : ', data);
+
+  if (isLoading) {
+    return <div>ladoing</div>;
+  }
+
+  if (isSuccess && data) {
+    return <RecruitDetail data={data.result} />;
+  }
 }
