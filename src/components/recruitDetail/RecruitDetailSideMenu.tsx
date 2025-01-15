0656f6eba4a3b1fa564fbd79bc7d9504f3b34823
K-Soo <kanabun102@naver.com>
2025-01-16 02:09:04 +0900

    feat: 지원하기 버튼 클릭 시 로그인 안내 및 로그인/회원가입 버튼 추가

--- src/components/recruitDetail/RecruitDetailSideMenu.tsx
+++ src/components/recruitDetail/RecruitDetailSideMenu.tsx
@@ -1,27 +1,49 @@
 import styled from 'styled-components';
 import Button from '@/components/common/style/Button';
-
+import Dimmed from '@/components/common/Dimmed';
+import useAuth from '@/hooks/useAuth';
+import path from '@/constants/path';
+import { useRouter } from 'next/router';
+import useToast from '@/hooks/useToast';
 interface RecruitDetailSideMenuProps {
   managerName: string;
-  managerEmail: string;
   managerNumber: string;
 }
 
-export default function RecruitDetailSideMenu({ managerEmail, managerName, managerNumber }: RecruitDetailSideMenuProps) {
+export default function RecruitDetailSideMenu({ managerName, managerNumber }: RecruitDetailSideMenuProps) {
+  const { isAuthenticated } = useAuth();
+  const { addToast } = useToast();
+  const router = useRouter();
+
+  // TODO  - 지원하기 클릭 시
+  const handleClickApply = () => {
+    if (!isAuthenticated) {
+      addToast({ message: '로그인 후 이용해주세요.', type: 'info' });
+    }
+  };
+
   return (
     <S.RecruitDetailSideMenu>
-      <Button label="지원하기" variant="primary" height="50px" borderRadius="10px" />
+      <Button label="지원하기" variant="primary" height="50px" borderRadius="10px" onClick={handleClickApply} />
       <div className="info-box">
+        {!isAuthenticated && (
+          <Dimmed>
+            <Button
+              label="로그인/회원가입"
+              variant="secondary200"
+              height="40px"
+              width="140px"
+              onClick={() => router.push(path.SIGN_IN)}
+              fontSize="14px"
+            />
+          </Dimmed>
+        )}
         <div className="info-box__item">
           <span>담당자</span>
           <em>{managerName}</em>
         </div>
         <div className="info-box__item">
-          <span>담당자 이메일</span>
-          <em>{managerEmail}</em>
-        </div>
-        <div className="info-box__item">
-          <span>담당자 연락처</span>
+          <span>연락처</span>
           <em>{managerNumber}</em>
         </div>
       </div>
@@ -35,7 +57,6 @@ const S = {
     flex: 0 0 330px;
     position: sticky;
     top: 90px;
-    z-index: 200;
     height: 400px;
     margin-left: 30px;
     background-color: ${({ theme }) => theme.colors.white};
@@ -43,6 +64,7 @@ const S = {
       display: none;
     `};
     .info-box {
+      position: relative;
       margin-top: 30px;
       background-color: ${(props) => props.theme.colors.gray};
       border-radius: 10px;
