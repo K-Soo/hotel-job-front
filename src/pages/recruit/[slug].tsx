2f8f209b895078ff15d4cba169c152c6deb156a0
K-Soo <kanabun102@naver.com>
2025-01-15 17:54:54 +0900

    feat: RecruitDetailPage에 Fragment 추가

--- src/pages/recruit/[slug].tsx
+++ src/pages/recruit/[slug].tsx
@@ -1,9 +1,12 @@
 import Layout, { Header, Footer, Main, DesktopNavigation, MobileNavigation } from '@/components/layout';
-
 import RecruitDetailContainer from '@/containers/recruitDetailContainer';
 
 export default function RecruitDetailPage() {
-  return <RecruitDetailContainer />;
+  return (
+    <>
+      <RecruitDetailContainer />
+    </>
+  );
 }
 
 RecruitDetailPage.getLayout = (page: React.ReactElement) => {
