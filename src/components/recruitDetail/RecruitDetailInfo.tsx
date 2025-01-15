cdf3c382284199ec699f3f58be6afb747f05db93
K-Soo <kanabun102@naver.com>
2025-01-16 02:09:01 +0900

    feat: RecruitDetailInfo 컴포넌트 추가 및 직무, 국적, 모집인원 등 정보 표시 기능 구현

---
+++ src/components/recruitDetail/RecruitDetailInfo.tsx
@@ -0,0 +1,104 @@
+import { AllJobsKeyValuesKeys } from '@/constants/job';
+import { EducationLevelKeys } from '@/types';
+import styled from 'styled-components';
+import { allJobs } from '@/constants/job';
+import { educationLevel } from '@/constants';
+
+interface RecruitDetailInfoProps {
+  jobs: AllJobsKeyValuesKeys[];
+  educationCondition: EducationLevelKeys;
+  recruitmentCapacity: number;
+}
+
+export default function RecruitDetailInfo({ educationCondition, jobs, recruitmentCapacity }: RecruitDetailInfoProps) {
+  return (
+    <S.RecruitDetailInfo>
+      <div className="info-container">
+        <div className="left">
+          <S.Row>
+            <S.RowLabel>직무</S.RowLabel>
+            <S.RowValue>
+              {jobs.map((job) => (
+                <span>{allJobs[job]}</span>
+              ))}
+            </S.RowValue>
+          </S.Row>
+          <S.Row>
+            <S.RowLabel>국적</S.RowLabel>
+            <S.RowValue>국적 무관</S.RowValue>
+          </S.Row>
+
+          <S.Row>
+            <S.RowLabel>모집인원</S.RowLabel>
+            <S.RowValue>{recruitmentCapacity}명</S.RowValue>
+          </S.Row>
+
+          <S.Row>
+            <S.RowLabel>근무 부서</S.RowLabel>
+            <S.RowValue>-</S.RowValue>
+          </S.Row>
+        </div>
+
+        <div className="right">
+          <S.Row>
+            <S.RowLabel>경력</S.RowLabel>
+            <S.RowValue>경력 무관</S.RowValue>
+          </S.Row>
+
+          {/* 외국인 or 국적무관 일때 비자조건 렌더링 */}
+          <S.Row>
+            <S.RowLabel>비자조건</S.RowLabel>
+            <S.RowValue>-</S.RowValue>
+          </S.Row>
+
+          <S.Row>
+            <S.RowLabel>학력조건</S.RowLabel>
+            <S.RowValue>{educationLevel[educationCondition]}</S.RowValue>
+          </S.Row>
+
+          <S.Row>
+            <S.RowLabel>직급</S.RowLabel>
+            <S.RowValue>-</S.RowValue>
+          </S.Row>
+        </div>
+      </div>
+    </S.RecruitDetailInfo>
+  );
+}
+
+const S = {
+  RecruitDetailInfo: styled.div`
+    border-radius: 10px;
+    padding: 10px 15px;
+    background-color: ${(props) => props.theme.colors.gray};
+    margin-bottom: 50px;
+    font-size: 14px;
+    .info-container {
+      display: flex;
+      ${(props) => props.theme.media.tablet`
+        flex-direction: column;
+      `};
+      .left {
+        flex: 50%;
+      }
+      .right {
+        flex: 50%;
+      }
+    }
+  `,
+  Row: styled.div`
+    display: flex;
+    padding: 8px 0;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  `,
+  RowLabel: styled.div`
+    flex-basis: 80px;
+    color: ${(props) => props.theme.colors.black600};
+    font-weight: 300;
+  `,
+  RowValue: styled.div`
+    color: ${(props) => props.theme.colors.black};
+  `,
+};
