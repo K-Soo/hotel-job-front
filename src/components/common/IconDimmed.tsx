09c5244803cf693b5d164343f0f24552d47b0526
K-Soo <kanabun102@naver.com>
2025-01-15 22:17:06 +0900

    feat: IconDimmed 컴포넌트에 width, height, margin props 추가 및 스타일 개선

--- src/components/common/IconDimmed.tsx
+++ src/components/common/IconDimmed.tsx
@@ -6,25 +6,33 @@ interface IconDimmedProps {
   onClick?: React.MouseEventHandler<HTMLElement> | undefined;
   padding?: string;
   children: React.ReactNode;
+  width?: string;
+  height?: string;
+  margin?: string;
 }
 
-export default function IconDimmed({ onClick, padding, children }: IconDimmedProps) {
+export default function IconDimmed({ onClick, padding, height, width, margin, children }: IconDimmedProps) {
   return (
-    <S.IconDimmed onClick={onClick} whileTap={{ scale: 0.95 }} $padding={padding}>
+    <S.IconDimmed onClick={onClick} whileTap={{ scale: 0.95 }} $padding={padding} $width={width} $height={height} $margin={margin}>
       {children}
     </S.IconDimmed>
   );
 }
 
 const S = {
-  IconDimmed: styled(motion.i)<{ $padding?: string }>`
-    display: inline-block;
+  IconDimmed: styled(motion.i)<{ $padding?: string; $width?: string; $height?: string; $margin?: string }>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     border-radius: 50%;
     padding: ${(props) => props.$padding || '4px'};
     justify-content: center;
     align-items: center;
     font-size: 0;
     background-color: rgba(242, 244, 246, 0.7);
+    width: ${(props) => props.$width || 'auto'};
+    height: ${(props) => props.$height || 'auto'};
+    margin: ${(props) => props.$margin || '0'};
     &:hover {
       background-color: rgba(242, 244, 246, 0.8);
       background-color: ${(props) => props.theme.colors.gray200};
