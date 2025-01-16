d273eee3f080231f158c0a355d706f8f407981a3
K-Soo <kanabun102@naver.com>
2025-01-16 19:51:30 +0900

    fix: 상수 이름을 대문자로 변경하여 일관성 유지

--- src/utils/employmentTypeFormat.ts
+++ src/utils/employmentTypeFormat.ts
@@ -1,11 +1,11 @@
 import { RecruitListItem } from '@/types';
-import { employmentType } from '@/constants/recruitment';
+import { EMPLOYMENT_TYPE } from '@/constants/recruitment';
 
 // 고용형태 한글 포멧팅
 export function employmentTypeFormat(employment: RecruitListItem['employmentType']): string {
   const selectedTypes = Object.entries(employment)
     .filter(([, value]) => value === true)
-    .map(([key]) => employmentType[key as keyof RecruitListItem['employmentType']]);
+    .map(([key]) => EMPLOYMENT_TYPE[key as keyof RecruitListItem['employmentType']]);
 
   if (selectedTypes.length > 1) {
     return `${selectedTypes[0]} 외 ${selectedTypes.length - 1}`;
