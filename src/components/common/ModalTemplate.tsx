891e882bc9a937514e11d8365ba01c19a1485bbe
K-Soo <kanabun102@naver.com>
2025-01-16 19:05:02 +0900

    feat: ModalTemplate 컴포넌트 추가

---
+++ src/components/common/ModalTemplate.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface ModalTemplateProps {}
+
+export default function ModalTemplate({}: ModalTemplateProps) {
+  return <S.ModalTemplate>ModalTemplate</S.ModalTemplate>;
+}
+
+const S = {
+  ModalTemplate: styled.div``,
+};
