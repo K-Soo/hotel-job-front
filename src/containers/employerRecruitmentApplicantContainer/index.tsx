de1eef357fd7ede12ce1f8c1c0036bba129d5994
K-Soo <kanabun102@naver.com>
2025-01-19 03:13:14 +0900

    feat: 지원자 목록 조회 기능 추가 및 UI 구성 요소 통합

--- src/containers/employerRecruitmentApplicantContainer/index.tsx
+++ src/containers/employerRecruitmentApplicantContainer/index.tsx
@@ -1,6 +1,45 @@
 import React from 'react';
 import EmployerRecruitmentApplicant from '@/components/employerRecruitmentApplicant';
+import SectionTitle from '@/components/common/employer/SectionTitle';
+import ApplicantStatusPanel from '@/components/employerRecruitmentApplicant/ApplicantStatusPanel';
+import ApplicantTab from '@/components/employerRecruitmentApplicant/ApplicantTab';
+import ApplicantTable from '@/components/employerRecruitmentApplicant/ApplicantTable';
+import useFetchQuery from '@/hooks/useFetchQuery';
+import { Get } from '@/apis';
+import queryKeys from '@/constants/queryKeys';
+import { keepPreviousData } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
 
 export default function EmployerRecruitmentApplicantContainer() {
-  return <EmployerRecruitmentApplicant />;
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const { data, isLoading, isSuccess } = useFetchQuery({
+    queryKey: [queryKeys.RECRUITMENT_LIST],
+    queryFn: Get.getRecruitmentDetailApplicantList,
+    options: {
+      enabled: !!slug,
+      throwOnError: true,
+      staleTime: 60 * 1000 * 5,
+      gcTime: 60 * 1000 * 10,
+      placeholderData: keepPreviousData,
+    },
+    requestQuery: {
+      recruitmentId: slug as string,
+    },
+  });
+  console.log('data: ', data);
+
+  return (
+    <EmployerRecruitmentApplicant>
+      <ApplicantStatusPanel />
+
+      <ApplicantTab />
+
+      <ApplicantTable>
+        <ApplicantTable.Header />
+        <ApplicantTable.Body />
+      </ApplicantTable>
+    </EmployerRecruitmentApplicant>
+  );
 }
