9c41ffba7510edd03c94897edec57c08181d0be5
K-Soo <kanabun102@naver.com>
2025-01-18 18:13:17 +0900

    feat: ResumeProfileSection 컴포넌트에 프로필 정보 입력 필드 추가 및 수정

--- src/components/common/resume/ResumeProfileSection.tsx
+++ src/components/common/resume/ResumeProfileSection.tsx
@@ -3,44 +3,68 @@ import ResumeProfileImage from '@/components/common/resume/ResumeProfileImage';
 import FormInput from '@/components/common/form/FormInput';
 import Line from '@/components/common/Line';
 import Button from '@/components/common/style/Button';
-import { ResumeRegisterForm } from '@/types';
+import { ResumeDetailForm, ResumeRegisterForm, ResumeStatusKey } from '@/types';
 import { useSetRecoilState } from 'recoil';
 import { daumPostAtom } from '@/recoil/daumPost';
+import { useFormContext } from 'react-hook-form';
 
-interface ResumeProfileSectionProps {}
+interface ResumeProfileSectionProps {
+  status: ResumeStatusKey;
+}
 
-export default function ResumeProfileSection({}: ResumeProfileSectionProps) {
+export default function ResumeProfileSection({ status }: ResumeProfileSectionProps) {
+  const { register, watch, setValue, getValues } = useFormContext<ResumeDetailForm>();
+  console.log('getValues: ', getValues('name'));
+
+  const nameValue = getValues('name');
+  const localCodeValue = getValues('localCode');
+  const sexCodeValue = getValues('sexCode');
+  const birthdayValue = getValues('birthday');
+  const phoneValue = getValues('phone');
+
   const setDaumPostState = useSetRecoilState(daumPostAtom);
 
   return (
     <S.ResumeProfileSection>
       <div className="profile-box">
-        <ResumeProfileImage<ResumeRegisterForm> name="title" />
         <div className="profile-form">
-          <FormInput<ResumeRegisterForm> name="title" maxWidth="240px" label="이름" />
-          <FormInput<ResumeRegisterForm> name="title" maxWidth="240px" label="성별" />
-          <FormInput<ResumeRegisterForm> name="title" maxWidth="240px" label="전화번호" />
+          <FormInput<ResumeDetailForm> name="name" maxWidth="300px" readOnly={false} errorPosition="static" placeholder="이름" />
+          <div style={{ display: 'flex' }}>
+            <FormInput<ResumeDetailForm>
+              name="sexCode"
+              maxWidth="80px"
+              readOnly={false}
+              label="성별"
+              margin="0 15px 0 0"
+              errorPosition="static"
+            />
+            <FormInput<ResumeDetailForm>
+              name="birthday"
+              maxWidth="205px"
+              readOnly={false}
+              label="생년월일"
+              placeholder="생년월일"
+              errorPosition="static"
+            />
+          </div>
+          <FormInput<ResumeDetailForm>
+            label="휴대폰"
+            name="phone"
+            maxWidth="300px"
+            readOnly={false}
+            mask="999-9999-9999"
+            errorPosition="static"
+          />
+
+          <FormInput<ResumeDetailForm> name="address" disabled margin="0 0 0 0" maxWidth="300px" label="주소" errorPosition="static" />
+          <FormInput<ResumeDetailForm> name="addressDetail" label="상세주소" maxWidth="300px" errorPosition="static" />
+
+          <FormInput<ResumeDetailForm> name="email" label="이메일" maxWidth="300px" />
         </div>
-      </div>
-      <Line />
-      <FormInput name="name" label="이메일" />
-
-      <div className="zip-box">
-        <FormInput<ResumeRegisterForm> name="title" label="우편번호" maxWidth="135px" disabled />
-        <Button
-          variant="secondary100"
-          label="주소 검색"
-          width="90px"
-          height="40px"
-          margin="5px 0 0 15px"
-          onClick={() => setDaumPostState({ isOpen: true })}
-        />
+        <ResumeProfileImage<ResumeDetailForm> name="profileImage" />
       </div>
 
-      <div className="address-box">
-        <FormInput<ResumeRegisterForm> name="title" label="주소" margin="0 15px 15px 0" maxWidth="240px" disabled />
-        <FormInput<ResumeRegisterForm> name="title" label="상세주소" margin="0 0 15px 0" />
-      </div>
+      {/* <Button variant="secondary100" label="주소 검색" width="90px" height="40px" onClick={() => setDaumPostState({ isOpen: true })} /> */}
     </S.ResumeProfileSection>
   );
 }
@@ -49,13 +73,11 @@ const S = {
   ResumeProfileSection: styled.div`
     .profile-box {
       display: flex;
+      /* border: 1px solid red; */
       .profile-form {
         flex: 1;
+        /* border: 1px solid red; */
       }
-    }
-    .zip-box {
-      display: flex;
-      align-items: center;
     }
     .address-box {
       display: flex;
