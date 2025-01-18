b18212a7cb8f666b875ea2b4bf764b2c59e4a644
K-Soo <kanabun102@naver.com>
2025-01-18 18:14:11 +0900

    feat: 이력서 관련 상수 추가 및 합격 단계 한글화

--- src/constants/resume.ts
+++ src/constants/resume.ts
@@ -17,10 +17,25 @@ export const position = {
   CEO: 'CEO', // 대표
 } as const;
 
-export const licenseStage = {
-  FIRST: 'FIRST', // 1차 합격
-  SECOND: 'SECOND', // 2차 합격
-  WRITTEN: 'WRITTEN', // 필기 합격
-  PRACTICAL: 'PRACTICAL', // 실기 합격
-  FINAL: 'FINAL', // 최종 합격
+export const LICENSE_STAGE = {
+  FIRST: '1차 합격',
+  SECOND: '2차 합격',
+  WRITTEN: '필기 합격',
+  PRACTICAL: '실기 합격',
+  FINAL: '최종 합격',
+} as const;
+
+export const SANCTION_REASON = {
+  NONE: 'NONE', // 제재 없음
+  INAPPROPRIATE_LANGUAGE: 'INAPPROPRIATE_LANGUAGE', //욕설 또는 부적절한 언어 사용
+  FALSE_INFORMATION: 'FALSE_INFORMATION', // 허위 정보 입력
+  ILLEGAL_CONTENT: 'ILLEGAL_CONTENT', // 불법적 콘텐츠 포함
+  POLICY_VIOLATION: 'POLICY_VIOLATION', // 기타 정책 위반
+};
+
+export const RESUME_STATUS = {
+  DRAFT: 'DRAFT', // 작성 중(임시 저장 상태).
+  PUBLISH: 'PUBLISH', // 제출 완료(정상 노출 가능 상태).
+  HIDDEN: 'HIDDEN', // 사용자가 비공개 상태로 설정.
+  ERROR: 'ERROR', // 처리 중 오류 발생.
 };
