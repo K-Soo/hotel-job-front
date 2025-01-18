acba653777731fa9e5c8c0b8ef07e186186bd10f
K-Soo <kanabun102@naver.com>
2025-01-18 18:12:24 +0900

    fix: Confirm 컴포넌트에서 버튼 스타일 및 모바일 폰트 크기 조정

--- src/components/common/Confirm.tsx
+++ src/components/common/Confirm.tsx
@@ -38,7 +38,7 @@ export default function Confirm() {
               name="positive"
               className="primary-button"
               label={alertWithConfirmSelectorValue.confirmLabel}
-              variant="primary"
+              variant="primary100"
               maxWidth="200px"
               height="45px"
               onClick={() => {
@@ -87,7 +87,7 @@ const S = {
       font-weight: 500;
       text-align: center;
       ${(props) => props.theme.media.mobile`
-        font-size: 20px;
+        font-size: 18px;
       `};
     }
     .description {
