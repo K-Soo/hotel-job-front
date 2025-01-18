31444d4a5391d53e502dc64a14843f749a8f9780
K-Soo <kanabun102@naver.com>
2025-01-19 07:46:51 +0900

    feat: 사용자 프로필 API 호출 및 로딩 상태 처리 추가

--- src/containers/userProfileContainer/index.tsx
+++ src/containers/userProfileContainer/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import UserAsideMenu from '@/components/common/user/UserAsideMenu';
 import UserProfile from '@/components/userProfile';
+import ProfileTitle from '@/components/userProfile/ProfileTitle';
 import UserTitle from '@/components/common/user/UserTitle';
 import UserTemplate from '@/components/common/user/UserTemplate';
+import useFetchQuery from '@/hooks/useFetchQuery';
+import { Get } from '@/apis';
+import queryKeys from '@/constants/queryKeys';
+import { keepPreviousData } from '@tanstack/react-query';
+import useAuth from '@/hooks/useAuth';
 
 export default function UserProfileContainer() {
-  return (
-    <UserProfile>
-      <UserTemplate>
-        <UserTitle title="프로필 정보" />
-      </UserTemplate>
-    </UserProfile>
-  );
+  const { authAtomState } = useAuth();
+
+  const { data, isLoading, isSuccess } = useFetchQuery({
+    queryKey: [queryKeys.USER_PROFILE, { nickname: authAtomState.nickname }],
+    queryFn: Get.getApplicantProfile,
+    options: {
+      throwOnError: true,
+      staleTime: 60 * 1000 * 5,
+      gcTime: 60 * 1000 * 10,
+      placeholderData: keepPreviousData,
+    },
+  });
+
+  console.info('프로필 정보 API : ', data);
+
+  return <UserProfile isLoading={isLoading} isSuccess={isSuccess} data={data?.result} />;
 }
