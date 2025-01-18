a6382b2b55ab234d761bf3532534cf8847ddcebe
K-Soo <kanabun102@naver.com>
2025-01-18 18:14:04 +0900

    feat: UserResumeRegister 컴포넌트에 외국어 입력 기능 및 최대 선택 개수 제한 추가

--- src/components/userResumeRegister/index.tsx
+++ src/components/userResumeRegister/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useFormContext } from 'react-hook-form';
+import { useFormContext, useFieldArray } from 'react-hook-form';
 import { ResumeRegisterForm } from '@/types';
 import ResumeSection from '@/components/common/resume/ResumeSection';
 import ResumePolicy from '@/components/common/resume/ResumePolicy';
@@ -10,10 +10,14 @@ import ResumeExperienceForm from '@/components/common/resume/ResumeExperienceFor
 import ResumeLicenseForm from '@/components/common/resume/ResumeLicenseForm';
 
 import FormInput from '@/components/common/form/FormInput';
+import FormInputB from '@/components/common/form/FormInputB';
 import FormSelect from '@/components/common/form/FormSelect';
+import FormMapSelect from '@/components/common/form/FormMapSelect';
 import FormToggle from '@/components/common/form/FormToggle';
-import { salaryTypeOptions } from '@/constants/options';
+import { educationConditionLevelOptions, languageOptions, resumeEducationLevelOptions, salaryTypeOptions } from '@/constants/options';
 import FormArea from '@/components/common/form/FormArea';
+import { LANGUAGE, LANGUAGE_LEVEL } from '@/constants/language';
+import useToast from '@/hooks/useToast';
 
 interface UserResumeRegisterProps {
   children: React.ReactNode;
@@ -22,27 +26,34 @@ interface UserResumeRegisterProps {
 
 export default function UserResumeRegister({ handleClickAdd, children }: UserResumeRegisterProps) {
   const { register, watch, setValue, getValues } = useFormContext<ResumeRegisterForm>();
+  const { fields, remove, append } = useFieldArray<ResumeRegisterForm>({ name: 'languages' });
+  const { addToast } = useToast();
 
-  const handleAddLicense = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const { name } = event.currentTarget;
+  const handleClickAddLanguage = () => {
+    if (fields.length >= 4) {
+      return addToast({ message: '최대 4개까지 선택 가능합니다.', type: 'warning' });
+    }
+    append({ name: null, level: null });
+  };
+
+  const handleClickAddLicense = () => {
     const values = getValues('licenses');
 
     setValue('licenses', [
       ...values,
       {
         licenseName: '',
-        licenseStage: undefined,
-        dateOfCompletion: new Date(),
+        licenseStage: 'FINAL',
       },
     ]);
   };
 
   return (
     <S.UserResumeRegister>
-      <article>
-        <FormInput<ResumeRegisterForm> name="title" placeholder="이력서 제목을 입력해주세요." />
+      <article className="resume-container">
+        <FormInput<ResumeRegisterForm> name="title" placeholder="이력서 제목을 입력해주세요." margin="0 0 15px 0" />
 
-        <FormSelect<ResumeRegisterForm> name="careerLevel" label="신입" options={salaryTypeOptions} required maxWidth="220px" />
+        {/* <FormSelect<ResumeRegisterForm> name="careerLevel" label="신입" options={salaryTypeOptions} required maxWidth="220px" /> */}
 
         <ResumeProfileSection />
 
@@ -50,8 +61,8 @@ export default function UserResumeRegister({ handleClickAdd, children }: UserRes
           <FormArea<ResumeRegisterForm> name="summary" maxLength={200} />
         </ResumeSection>
 
-        <ResumeSection title="학력" isRequired={true} handleClickAdd={handleClickAdd} visibleAddButton={false} name="education">
-          <FormSelect<ResumeRegisterForm> name="education" label="최종학력" options={salaryTypeOptions} required maxWidth="220px" />
+        <ResumeSection title="최종 학력" isRequired={true} handleClickAdd={handleClickAdd} visibleAddButton={false} name="education">
+          <FormSelect<ResumeRegisterForm> name="education" options={resumeEducationLevelOptions} maxWidth="220px" />
           {/* <FormToggle<ResumeRegisterForm> label="재직중" name={``} /> */}
         </ResumeSection>
 
@@ -59,18 +70,37 @@ export default function UserResumeRegister({ handleClickAdd, children }: UserRes
           <ResumeExperienceForm />
         </ResumeSection>
 
-        <ResumeSection title="경력기술서" handleClickAdd={handleClickAdd}>
+        {/* TODO - 경력 기술서 */}
+        {/* <ResumeSection title="경력기술서" handleClickAdd={handleClickAdd}>
           <div></div>
-        </ResumeSection>
+        </ResumeSection> */}
 
-        <ResumeSection title="자격증" handleClickAdd={handleAddLicense} name="licenses">
+        <ResumeSection title="자격증" handleClickAdd={handleClickAddLicense} name="licenses">
           <ResumeLicenseForm />
         </ResumeSection>
 
-        <ResumeSection title="외국어" handleClickAdd={handleClickAdd}>
-          <div></div>
+        <ResumeSection title="외국어" handleClickAdd={handleClickAddLanguage}>
+          {fields.map((field, index) => (
+            <div key={field.id} style={{ display: 'flex' }} className="language-item">
+              <FormMapSelect<ResumeRegisterForm>
+                name={`languages.${index}.name`}
+                options={{ '': '선택', ...LANGUAGE }}
+                required
+                maxWidth="180px"
+                margin="0 15px 15px 0"
+              />
+              <FormMapSelect<ResumeRegisterForm>
+                name={`languages.${index}.level`}
+                options={{ '': '선택', ...LANGUAGE_LEVEL }}
+                required
+                maxWidth="150px"
+                margin="0 15px 15px 0"
+              />
+            </div>
+          ))}
         </ResumeSection>
 
+        {/* 본인인증 -> 성별에따라 분기처리  */}
         <ResumeSection title="병역상태" handleClickAdd={handleClickAdd}>
           <div></div>
         </ResumeSection>
@@ -87,7 +117,7 @@ const S = {
   UserResumeRegister: styled.div`
     display: flex;
     padding-bottom: 100px;
-    article {
+    .resume-container {
       flex: 1;
     }
   `,
