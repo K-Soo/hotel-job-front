4c878a52d91d434dd24a072e5378f1cd652d7e49
K-Soo <kanabun102@naver.com>
2025-01-18 18:15:10 +0900

    feat: 사용자 이력서 등록 페이지에 인증 상태에 따른 로딩 및 스켈레톤 UI 추가

--- src/pages/user/resume/register.tsx
+++ src/pages/user/resume/register.tsx
@@ -1,7 +1,29 @@
+import React from 'react';
 import Layout, { Main, Header, DesktopNavigation, MobileNavigation } from '@/components/layout';
 import UserResumeRegisterContainer from '@/containers/userResumeRegisterContainer';
+import useAuth from '@/hooks/useAuth';
+import LoadingOverlay from '@/components/common/LoadingOverlay';
+import SkeletonUI from '@/components/common/SkeletonUI';
+
 export default function UserResumeRegisterPage() {
+  const { authAtomState } = useAuth();
+
+  React.useEffect(() => {
+    if (authAtomState.certificationStatus !== 'VERIFIED') {
+      // alert('이력서 등록을 위해서는 본인인증이 필요합니다.');
+      // window.location.href = '/user/resume';
+    }
+  }, []);
+
   return <UserResumeRegisterContainer />;
+
+  if (authAtomState.certificationStatus !== 'VERIFIED') {
+    return <SkeletonUI.Recruitment />;
+  }
+
+  if (authAtomState.certificationStatus === 'VERIFIED') {
+    return <UserResumeRegisterContainer />;
+  }
 }
 
 UserResumeRegisterPage.getLayout = (page: React.ReactElement) => {
