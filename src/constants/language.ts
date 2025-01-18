b42101f4c7eefe25e99ce4e820c65b6717d75349
K-Soo <kanabun102@naver.com>
2025-01-18 18:15:49 +0900

    feat: 언어 및 언어 수준 상수를 language.ts 파일에 추가

---
+++ src/constants/language.ts
@@ -0,0 +1,17 @@
+export const LANGUAGE = {
+  KOREAN: '한국어',
+  ENGLISH: '영어',
+  JAPANESE: '일본어',
+  CHINESE: '중국어',
+  GERMAN: '독일어',
+  FRENCH: '프랑스어',
+  RUSSIAN: '러시아어',
+} as const;
+
+export const LANGUAGE_LEVEL = {
+  BEGINNER: '초급',
+  INTERMEDIATE: '중급',
+  ADVANCED: '고급',
+  FLUENT: '유창함',
+  NATIVE: '원어민 수준',
+} as const;
