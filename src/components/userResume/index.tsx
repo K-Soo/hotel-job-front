ee297d7cb6ced8e6fabcd57487ed168cfc8b3765
K-Soo <kanabun102@naver.com>
2025-01-19 07:46:19 +0900

    feat: UserResume 컴포넌트에서 불필요한 임포트 제거

--- src/components/userResume/index.tsx
+++ src/components/userResume/index.tsx
@@ -1,14 +1,10 @@
 import styled from 'styled-components';
-import UserTemplate from '@/components/common/user/UserTemplate';
 import UserAsideMenu from '@/components/common/user/UserAsideMenu';
-import { useRouter } from 'next/router';
-import path from '@/constants/path';
 
 interface UserResumeProps {
   children: React.ReactNode;
 }
 
-//가능동
 export default function UserResume({ children }: UserResumeProps) {
   return (
     <S.UserResume>
