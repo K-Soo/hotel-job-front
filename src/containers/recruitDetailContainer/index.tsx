1ba77d9b756c9489f69fb3ba8d42ad93141e12f7
K-Soo <kanabun102@naver.com>
2025-01-19 00:22:12 +0900

    feat: 지원하기 버튼 상태 및 이력서 제출 기능 개선, 지원 가능 여부 체크 추가

--- src/containers/recruitDetailContainer/index.tsx
+++ src/containers/recruitDetailContainer/index.tsx
@@ -9,12 +9,16 @@ import useToast from '@/hooks/useToast';
 import useAuth from '@/hooks/useAuth';
 import RecruitDetailApplyResumeForm from '@/components/recruitDetail/RecruitDetailApplyResumeForm';
 import Button from '@/components/common/style/Button';
+import { useQueryClient } from '@tanstack/react-query';
 
 export default function RecruitDetailContainer() {
   const [isOpenApplyForm, setIsOpenApplyForm] = React.useState(false);
+  const [selectedResume, setSelectedResume] = React.useState<string | null>(null);
+  const [applyStatus, setApplyStatus] = React.useState<'available' | 'duplicate' | 'idle'>('idle');
+
   const { isAuthenticated } = useAuth();
   const { addToast } = useToast();
-
+  const queryClient = useQueryClient();
   const router = useRouter();
   const { slug } = router.query;
 
@@ -31,18 +35,61 @@ export default function RecruitDetailContainer() {
     },
   });
 
+  const {
+    data: applyCheckData,
+    isLoading: isApplyCheckLoading,
+    isError: isApplyCheckError,
+  } = useFetchQuery({
+    queryKey: [queryKeys.APPLICATION_APPLY_CHECK, { slug }],
+    queryFn: Get.applicationApplyCheck,
+    options: {
+      enabled: !!slug,
+      staleTime: 10 * 60 * 1000,
+      gcTime: 15 * 60 * 1000,
+    },
+    requestQuery: {
+      id: slug as string,
+    },
+  });
+
+  React.useEffect(() => {
+    if (applyCheckData) {
+      if (applyCheckData.result.status === 'available') {
+        setApplyStatus('available');
+      }
+      if (applyCheckData.result.status === 'duplicate') {
+        setApplyStatus('duplicate');
+      }
+    }
+  }, [applyCheckData]);
+
   const handleClickApply = React.useCallback(() => {
     if (!isAuthenticated) {
       return addToast({ message: '로그인 후 이용해주세요.', type: 'info' });
     }
     setIsOpenApplyForm((prev) => !prev);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthenticated]);
 
-  const fetchSubmitApply = async () => {
+  //fetch - 이력서 제출 API
+  const fetchSubmitApply = React.useCallback(async () => {
     try {
-      const response = await Post.applyResume({});
-    } catch (error) {}
-  };
+      const response = await Post.applyResume({
+        recruitId: slug as string,
+        resumeId: selectedResume as string,
+      });
+      console.log('이력서 제출 API : ', response);
+      await queryClient.invalidateQueries({ queryKey: [queryKeys.APPLICATION_APPLY_CHECK], refetchType: 'all' });
+      addToast({ message: '지원이 완료되었습니다.', type: 'success' });
+      setApplyStatus('duplicate');
+    } catch (error) {
+      addToast({ message: '이력서 제출에 실패했습니다.', type: 'error' });
+    } finally {
+      setIsOpenApplyForm(false);
+      setSelectedResume(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedResume]);
 
   console.log('채용공고 상세 API : ', data);
 
@@ -55,10 +102,29 @@ export default function RecruitDetailContainer() {
     return (
       <RecruitDetail data={data.result}>
         <RecruitDetailSideMenu managerName={data.result.managerName} managerNumber={data.result.managerNumber}>
-          {!isOpenApplyForm && (
-            <Button label="지원하기" variant="primary" height="50px" borderRadius="10px" onClick={handleClickApply} fontSize="18px" />
+          {!isOpenApplyForm && !isApplyCheckError && (
+            <Button
+              label={applyStatus === 'available' ? '지원하기' : '지원완료'}
+              variant="primary"
+              height="50px"
+              borderRadius="10px"
+              onClick={handleClickApply}
+              fontSize="18px"
+              disabled={applyStatus === 'duplicate'}
+              isLoading={isApplyCheckLoading}
+            />
           )}
-          {isOpenApplyForm && <RecruitDetailApplyResumeForm setIsOpenApplyForm={setIsOpenApplyForm} isOpenApplyForm={isOpenApplyForm} />}
+
+          {isApplyCheckError && <Button label="지원불가" variant="secondary" height="50px" borderRadius="10px" fontSize="18px" disabled />}
+
+          {isOpenApplyForm && (
+            <RecruitDetailApplyResumeForm
+              selectedResume={selectedResume}
+              setSelectedResume={setSelectedResume}
+              setIsOpenApplyForm={setIsOpenApplyForm}
+              fetchSubmitApply={fetchSubmitApply}
+            />
+          )}
         </RecruitDetailSideMenu>
       </RecruitDetail>
     );
