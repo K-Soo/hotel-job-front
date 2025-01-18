ec32d6262da486d79088178091cf183d1f6e090a
K-Soo <kanabun102@naver.com>
2025-01-18 18:14:29 +0900

    feat: 지원하기 버튼 추가 및 인증 상태에 따른 지원 폼 토글 기능 구현

--- src/containers/recruitDetailContainer/index.tsx
+++ src/containers/recruitDetailContainer/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import RecruitDetail from '@/components/recruitDetail';
-import { Get } from '@/apis';
+import { Get, Post } from '@/apis';
 import useFetchQuery from '@/hooks/useFetchQuery';
 import queryKeys from '@/constants/queryKeys';
 import { useRouter } from 'next/router';
 import RecruitDetailSideMenu from '@/components/recruitDetail/RecruitDetailSideMenu';
+import useToast from '@/hooks/useToast';
+import useAuth from '@/hooks/useAuth';
+import RecruitDetailApplyResumeForm from '@/components/recruitDetail/RecruitDetailApplyResumeForm';
+import Button from '@/components/common/style/Button';
 
 export default function RecruitDetailContainer() {
+  const [isOpenApplyForm, setIsOpenApplyForm] = React.useState(false);
+  const { isAuthenticated } = useAuth();
+  const { addToast } = useToast();
+
   const router = useRouter();
   const { slug } = router.query;
 
@@ -22,16 +30,36 @@ export default function RecruitDetailContainer() {
       id: slug as string,
     },
   });
+
+  const handleClickApply = React.useCallback(() => {
+    if (!isAuthenticated) {
+      return addToast({ message: '로그인 후 이용해주세요.', type: 'info' });
+    }
+    setIsOpenApplyForm((prev) => !prev);
+  }, [isAuthenticated]);
+
+  const fetchSubmitApply = async () => {
+    try {
+      const response = await Post.applyResume({});
+    } catch (error) {}
+  };
+
   console.log('채용공고 상세 API : ', data);
 
+  // TODO - Loading
   if (isLoading) {
-    return <div>ladoing</div>;
+    return <div>isLoading</div>;
   }
 
   if (isSuccess && data) {
     return (
       <RecruitDetail data={data.result}>
-        <RecruitDetailSideMenu managerName={data.result.managerName} managerNumber={data.result.managerNumber} />
+        <RecruitDetailSideMenu managerName={data.result.managerName} managerNumber={data.result.managerNumber}>
+          {!isOpenApplyForm && (
+            <Button label="지원하기" variant="primary" height="50px" borderRadius="10px" onClick={handleClickApply} fontSize="18px" />
+          )}
+          {isOpenApplyForm && <RecruitDetailApplyResumeForm setIsOpenApplyForm={setIsOpenApplyForm} isOpenApplyForm={isOpenApplyForm} />}
+        </RecruitDetailSideMenu>
       </RecruitDetail>
     );
   }
