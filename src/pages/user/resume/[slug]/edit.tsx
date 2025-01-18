6d85a6a2ff0dfd979a94f1edcdd6ab458de43dca
K-Soo <kanabun102@naver.com>
2025-01-18 18:15:14 +0900

    feat: 이력서 수정 페이지 추가 및 인증 상태에 따른 에러 경계 처리

---
+++ src/pages/user/resume/[slug]/edit.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Layout, { Main, Header, DesktopNavigation, MobileNavigation } from '@/components/layout';
+import UserResumeRegisterContainer from '@/containers/userResumeRegisterContainer';
+import useAuth from '@/hooks/useAuth';
+import LoadingOverlay from '@/components/common/LoadingOverlay';
+import SkeletonUI from '@/components/common/SkeletonUI';
+import UserResumeEditContainer from '@/containers/userResumeEditContainer';
+import { ErrorBoundary, ErrorComponent } from '@/error';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+export default function ResumeDetailEditPage() {
+  const router = useRouter();
+  const { authAtomState } = useAuth();
+
+  React.useEffect(() => {
+    if (authAtomState.certificationStatus !== 'VERIFIED') {
+      // alert('이력서 등록을 위해서는 본인인증이 필요합니다.');
+      // window.location.href = '/user/resume';
+    }
+  }, []);
+
+  return (
+    <ErrorBoundary
+      onError={(error) => {
+        if (axios.isAxiosError(error)) {
+          if (error.response?.status === 404) {
+            router.replace('/404');
+          }
+        }
+      }}
+      fallback={<ErrorComponent />}
+    >
+      <UserResumeEditContainer />
+    </ErrorBoundary>
+  );
+
+  if (authAtomState.certificationStatus !== 'VERIFIED') {
+  }
+
+  if (authAtomState.certificationStatus === 'VERIFIED') {
+  }
+}
+
+ResumeDetailEditPage.getLayout = (page: React.ReactElement) => {
+  return (
+    <Layout>
+      <Header>
+        <DesktopNavigation />
+        <MobileNavigation />
+      </Header>
+      <Main>{page}</Main>
+    </Layout>
+  );
+};
+
+ResumeDetailEditPage.authentication = true;
+ResumeDetailEditPage.allowedRoles = ['JOB_SEEKER'];
