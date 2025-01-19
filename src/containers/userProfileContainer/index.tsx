4ccab74fae9dc031b5e9b7bf355a4f1560f2fa7c
K-Soo <kanabun102@naver.com>
2025-01-19 20:49:24 +0900

    feat: 사용자 프로필에서 회원탈퇴 기능 추가 및 확인 알림 구현

--- src/containers/userProfileContainer/index.tsx
+++ src/containers/userProfileContainer/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import UserProfile from '@/components/userProfile';
-import ProfileTitle from '@/components/userProfile/ProfileTitle';
-import UserTitle from '@/components/common/user/UserTitle';
-import UserTemplate from '@/components/common/user/UserTemplate';
 import useFetchQuery from '@/hooks/useFetchQuery';
-import { Get } from '@/apis';
+import { Get, Delete, Auth } from '@/apis';
 import queryKeys from '@/constants/queryKeys';
 import { keepPreviousData } from '@tanstack/react-query';
 import useAuth from '@/hooks/useAuth';
+import useAlertWithConfirm from '@/hooks/useAlertWithConfirm';
 
 export default function UserProfileContainer() {
   const { authAtomState } = useAuth();
+  const { setAlertWithConfirmAtom } = useAlertWithConfirm();
 
   const { data, isLoading, isSuccess } = useFetchQuery({
     queryKey: [queryKeys.USER_PROFILE, { nickname: authAtomState.nickname }],
@@ -25,5 +24,35 @@ export default function UserProfileContainer() {
 
   console.info('프로필 정보 API : ', data);
 
-  return <UserProfile isLoading={isLoading} isSuccess={isSuccess} data={data?.result} />;
+  const fetchDeactivate = async () => {
+    try {
+      const response = await Delete.deactivateApplicantUser();
+      console.log('계정 삭제 요청 API : ', response);
+      if (response.result.status !== 'success') {
+        throw new Error();
+      }
+      alert('회원탈퇴가 완료되었습니다.');
+      Auth.signOut();
+      window.location.href = '/sign-in';
+    } catch (error) {
+      console.log('error: ', error);
+      alert('회원탈퇴에 실패했습니다. 문제가 지속될 경우 고객센터로 문의해주세요.');
+    } finally {
+    }
+  };
+
+  const handleClickWithdrawal = () => {
+    setAlertWithConfirmAtom((prev) => ({
+      ...prev,
+      type: 'CONFIRM',
+      title: 'TITLE_7',
+      subTitle: 'DESC_8',
+      cancelLabel: '취소',
+      confirmLabel: '회원탈퇴',
+      confirmVariant: 'delete',
+      onClickConfirm: () => fetchDeactivate(),
+    }));
+  };
+
+  return <UserProfile isLoading={isLoading} isSuccess={isSuccess} data={data?.result} handleClickWithdrawal={handleClickWithdrawal} />;
 }
