fa5ac09adf4e7d5288c2d4d865da3fd40d7c4905
K-Soo <kanabun102@naver.com>
2025-01-20 22:47:28 +0900

    feat: 프로덕션 환경에서 유지보수 페이지 표시 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import Layout, { Header, Footer, Main, DesktopNavigation, MobileNavigation } from '@/components/layout';
 import HomeContainer from '@/containers/homeContainer';
 import BottomNavigation from '@/components/layout/BottomNavigation';
+import environment from '@/environment';
+import Maintenance from '@/components/common/Maintenance';
 
 export default function HomePage() {
+  if (environment.isProd) {
+    return <Maintenance />;
+  }
+
   return <HomeContainer />;
 }
 
