8edde95da03ad6d1c907b61a8c22c1fe72a446af
K-Soo <kanabun102@naver.com>
2025-01-20 22:47:40 +0900

    feat: 사용자 이력서 및 프로필 페이지로 이동하는 버튼 추가

--- src/containers/userContainer/index.tsx
+++ src/containers/userContainer/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import User from '@/components/user';
 import UserAsideMenu from '@/components/common/user/UserAsideMenu';
+import { useRouter } from 'next/router';
+import path from '@/constants/path';
 
 export default function UserContainer() {
+  const router = useRouter();
+
   return (
     <User>
       <UserAsideMenu />
+      <div>
+        <button onClick={() => router.push(path.USER_RESUME)}>이력서 페이지 </button>
+        <div>
+          <button onClick={() => router.push(path.USER_PROFILE)}>프로필 페이지</button>
+        </div>
+      </div>
     </User>
   );
 }
