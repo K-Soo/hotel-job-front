b8d5357a107ab88374a5f17c4d4a4eae6dda43eb
K-Soo <kanabun102@naver.com>
2025-01-20 18:04:59 +0900

    feat: 지원자 탭에서 전형 단계 상태에 따른 버튼 동적 렌더링 및 로딩 상태 처리 추가

--- src/components/EmployerRecruitmentDetailApplicant/ApplicantTab.tsx
+++ src/components/EmployerRecruitmentDetailApplicant/ApplicantTab.tsx
@@ -1,31 +1,47 @@
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import Button from '@/components/common/style/Button';
-import path from '@/constants/path';
 import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
 import { Get } from '@/apis';
 import queryKeys from '@/constants/queryKeys';
 import useFetchQuery from '@/hooks/useFetchQuery';
 import { keepPreviousData } from '@tanstack/react-query';
+import { motion } from 'framer-motion';
 import SkeletonUI from '@/components/common/SkeletonUI';
-import { RecruitmentApplicantQueryStep, RecruitmentQueryStatus } from '@/types/API';
+import { RecruitmentApplicantQueryStep } from '@/types/API';
+import { EMPLOYER_REVIEW_STAGE_STATUS_WIDTH_TOTAL } from '@/constants/application';
 
 interface Query extends ParsedUrlQuery {
   step?: RecruitmentApplicantQueryStep;
+  slug: string;
 }
 
 interface ApplicantTabProps {}
 
 export default function ApplicantTab({}: ApplicantTabProps) {
   const router = useRouter();
-  const { step = 'all' } = router.query as Query;
+  const { step = 'TOTAL', slug } = router.query as Query;
+
+  const { data, isLoading, isSuccess } = useFetchQuery({
+    queryKey: [queryKeys.APPLICATION_RECRUITMENT_STATUS_COUNT, { slug }].filter(Boolean),
+    queryFn: Get.recruitmentApplicationStatusCount,
+    options: {
+      enabled: !!slug,
+      throwOnError: true,
+      staleTime: 60 * 1000 * 1,
+      gcTime: 60 * 1000 * 2,
+      placeholderData: keepPreviousData,
+    },
+    requestQuery: {
+      id: slug as string,
+    },
+  });
+  console.log('지원자 상태별 수량 API : ', data);
 
   const handleTabClick = (newStatus: string) => {
     const searchParams = new URLSearchParams(router.query as Record<string, string>);
     const lowerCaseStatus = newStatus.toLowerCase();
 
-    if (lowerCaseStatus === 'all') {
+    if (lowerCaseStatus === 'total') {
       searchParams.delete('step');
     } else {
       searchParams.set('step', lowerCaseStatus);
@@ -39,29 +55,39 @@ export default function ApplicantTab({}: ApplicantTabProps) {
 
   return (
     <S.ApplicantTab>
-      <div className="tab-container">
-        <button className="tab-container__item" onClick={() => handleTabClick('ALL')}>
-          <span>전체</span>
-          <span>1</span>
-          <StyledUnderLine />
-        </button>
-        <button className="tab-container__item" onClick={() => handleTabClick('DOCUMENT')}>
-          <span>서류전형</span>
-          <span>1</span>
-        </button>
-        <button className="tab-container__item" onClick={() => handleTabClick('INTERVIEW')}>
-          <span>면접</span>
-          <span>1</span>
-        </button>
-        <button className="tab-container__item" onClick={() => handleTabClick('ACCEPTED')}>
-          <span>최종합격</span>
-          <span>1</span>
-        </button>
-        <button className="tab-container__item" onClick={() => handleTabClick('REJECTED')}>
-          <span>불합격</span>
-          <span>1</span>
-        </button>
-      </div>
+      {isLoading && <SkeletonUI.Tabs />}
+      {isSuccess && data && (
+        <div className="tab-container">
+          {Object.entries(data.result)
+            .sort(([keyA], [keyB]) => {
+              if (keyA === 'TOTAL') return -1;
+              if (keyB === 'TOTAL') return 1;
+              return 0;
+            })
+            .map(([key, value]) => (
+              <button key={key} className="tab-container__item" onClick={() => handleTabClick(key)}>
+                <motion.span
+                  animate={{
+                    color: step.toUpperCase() === key ? '#333' : '#4e5968',
+                    fontWeight: step.toUpperCase() === key ? '600' : '400',
+                  }}
+                >
+                  {EMPLOYER_REVIEW_STAGE_STATUS_WIDTH_TOTAL[key as keyof typeof EMPLOYER_REVIEW_STAGE_STATUS_WIDTH_TOTAL]}
+                </motion.span>
+                <motion.span
+                  animate={{
+                    color: step.toUpperCase() === key ? '#333' : '#4e5968',
+                    fontWeight: step.toUpperCase() === key ? '600' : '400',
+                  }}
+                  className="tab-container__item--count"
+                >
+                  {value}
+                </motion.span>
+                {step.toUpperCase() === key && <StyledUnderLine />}
+              </button>
+            ))}
+        </div>
+      )}
     </S.ApplicantTab>
   );
 }
@@ -99,6 +125,9 @@ const S = {
         width: 150px;
         height: 100%;
         color: ${(props) => props.theme.colors.gray700};
+        &--count {
+          padding-left: 3px;
+        }
       }
     }
   `,
