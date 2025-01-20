5150af28dd72be109bc9bc50b5add2cd5df97e71
K-Soo <kanabun102@naver.com>
2025-01-20 22:47:42 +0900

    feat: 유지보수 페이지 표시 로직 제거 및 ReactQueryDevtools 주석 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,17 +34,6 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
   const [queryClient] = React.useState(() => new QueryClient(queryClientDefaultOption));
 
-  if (environment.isProd && router.pathname === path.HOME) {
-    return (
-      <AppThemeProvider>
-        <div>
-          <Maintenance />
-          <Footer />
-        </div>
-      </AppThemeProvider>
-    );
-  }
-
   return (
     <AppThemeProvider>
       <RecoilRoot>
@@ -58,7 +47,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
                 <Component {...pageProps} />
               </GuardComponent>,
             )}
-          <ReactQueryDevtools initialIsOpen={false} />
+          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
         </QueryClientProvider>
       </RecoilRoot>
     </AppThemeProvider>
