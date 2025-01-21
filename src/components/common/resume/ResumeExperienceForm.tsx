09bb13b329a3f7dd2285ad644bfc17537de4ed9a
K-Soo <kanabun102@naver.com>
2025-01-21 09:23:59 +0900

    feat: 이력서 경험 폼의 필드 이름 수정 및 퇴사사유 입력 기능 추가

--- src/components/common/resume/ResumeExperienceForm.tsx
+++ src/components/common/resume/ResumeExperienceForm.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useFormContext, useFieldArray } from 'react-hook-form';
-import { ResumeRegisterForm } from '@/types';
+import { useFieldArray } from 'react-hook-form';
+import { ResumeDetailForm } from '@/types';
 import FormInput from '@/components/common/form/FormInput';
 import FormSelect from '@/components/common/form/FormSelect';
 import FormArea from '@/components/common/form/FormArea';
 import FormToggle from '@/components/common/form/FormToggle';
 import FormDate from '@/components/common/form/FormDate';
 import Button from '@/components/common/style/Button';
-import { optionalJobOptions, positionOptions, salaryTypeOptions } from '@/constants/options';
-import { v4 as uuidv4 } from 'uuid';
+import { optionalJobOptions, positionOptions } from '@/constants/options';
 
 export default function ResumeExperienceForm() {
   const [isOnResignationReason, setIsOnResignationReason] = React.useState(false);
-  const { watch } = useFormContext<ResumeRegisterForm>();
-  const { fields } = useFieldArray({ name: 'experiences' });
+  const { fields } = useFieldArray({ name: 'experience' });
 
   return (
     <S.ResumeExperienceForm>
-      {/* {fields.map((field, index) => (
+      {fields.map((field, index) => (
         <div key={field.id} className="experience-item">
-          <FormInput<ResumeRegisterForm>
-            name={`experiences.${index}.companyName`}
+          <FormInput<ResumeDetailForm>
+            name={`experience.${index}.companyName`}
             placeholder="회사명"
             label="회사명"
             required
@@ -30,44 +28,62 @@ export default function ResumeExperienceForm() {
           />
 
           <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-            <FormDate<ResumeRegisterForm>
-              maxWidth="120px"
-              name={`experiences.${index}.startDate`}
+            <FormDate<ResumeDetailForm>
+              maxWidth="100px"
+              name={`experience.${index}.startDate`}
               label="입사년월"
               margin="0 15px 0 0"
               required
+              placeholder="입사년월"
             />
-            <FormDate<ResumeRegisterForm> maxWidth="120px" name={`experiences.${index}.endDate`} label="퇴사년월" margin="0 10px 0 0" />
-            <FormToggle<ResumeRegisterForm> label="재직중" name={`experiences.${index}.isEmployed`} />
+            <FormDate<ResumeDetailForm>
+              maxWidth="100px"
+              name={`experience.${index}.endDate`}
+              label="퇴사년월"
+              margin="0 10px 0 0"
+              placeholder="퇴사년월"
+            />
+            <FormToggle<ResumeDetailForm> label="재직중" name={`experience.${index}.isEmployed`} />
           </div>
 
           <div style={{ display: 'flex' }}>
             <FormSelect
-              name={`experiences.${index}.job`}
+              name={`experience.${index}.job`}
               label="직무"
               options={optionalJobOptions}
               required
               margin="0 15px 0 0"
               maxWidth="200px"
             />
-            <FormSelect name={`experiences.${index}.position`} label="직급/직책" options={positionOptions} maxWidth="180px" />
+            <FormSelect name={`experience.${index}.position`} label="직급/직책" options={positionOptions} maxWidth="180px" />
           </div>
 
-          <FormArea name={`experiences.${index}.responsibility`} label="담당업무" maxLength={200} />
+          <FormArea name={`experience.${index}.responsibility`} label="담당업무" maxLength={200} />
 
-          {isOnResignationReason && (
-            <FormSelect name={`experiences.${index}.salaryType`} options={salaryTypeOptions} maxWidth="130px" margin="30px 0 0 0" />
-          )}
-          <Button
-            label="퇴사사유"
-            variant="secondary100"
-            width="90px"
-            height="35px"
-            fontSize="14px"
-            onClick={() => setIsOnResignationReason((prev) => !prev)}
-          />
+          <div className="experience-item__resignation">
+            {isOnResignationReason && (
+              <FormInput<ResumeDetailForm>
+                name={`experience.${index}.reasonForLeaving`}
+                placeholder="퇴사사유"
+                required
+                maxLength={30}
+                errorPosition="absolute"
+                maxWidth="280px"
+                margin="0 0 15px 0"
+              />
+            )}
+
+            <Button
+              label="퇴사사유"
+              variant="tertiary"
+              width="90px"
+              height="35px"
+              fontSize="14px"
+              onClick={() => setIsOnResignationReason((prev) => !prev)}
+            />
+          </div>
         </div>
-      ))} */}
+      ))}
     </S.ResumeExperienceForm>
   );
 }
@@ -78,6 +94,9 @@ const S = {
       border-bottom: 1px solid ${(props) => props.theme.colors.gray200};
       padding-top: 30px;
       padding-bottom: 30px;
+      &__resignation {
+        margin-top: 15px;
+      }
       &:first-child {
         padding-top: 0;
       }
