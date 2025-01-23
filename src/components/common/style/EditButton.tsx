e477219f4a08f8a01f78de270d7f5cee49489901
K-Soo <kanabun102@naver.com>
2025-01-23 20:14:47 +0900

    feat: EditButton 컴포넌트 추가

---
+++ src/components/common/style/EditButton.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface EditButtonProps {}
+
+export default function EditButton({}: EditButtonProps) {
+  return <S.EditButton>EditButton</S.EditButton>;
+}
+
+const S = {
+  EditButton: styled.div``,
+};
