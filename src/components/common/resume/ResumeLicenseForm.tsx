27378c76f34118a43dcff079a75c99a9141eded4
K-Soo <kanabun102@naver.com>
2025-01-23 20:14:34 +0900

    feat: 이력서 자격증 폼 컴포넌트 수정 및 스타일 개선

--- src/components/common/resume/ResumeLicenseForm.tsx
+++ src/components/common/resume/ResumeLicenseForm.tsx
@@ -1,37 +1,35 @@
 import styled from 'styled-components';
-import { ResumeRegisterForm } from '@/types';
-// import { v4 as uuidv4 } from 'uuid';
+import { ResumeDetailForm } from '@/types';
 import FormInput from '@/components/common/form/FormInput';
-import FormDate from '@/components/common/form/FormDate';
-import ResumeDeleteButton from '@/components/userResumeDetail/ResumeDeleteButton';
-import { useFormContext, useFieldArray } from 'react-hook-form';
+import { useFieldArray } from 'react-hook-form';
 import FormMapSelect from '@/components/common/form/FormMapSelect';
 import { LICENSE_STAGE } from '@/constants/resume';
+import RemoveButton from '@/components/common/style/RemoveButton';
 
 export default function ResumeLicenseForm() {
-  const { fields, remove } = useFieldArray<ResumeRegisterForm>({ name: 'licenses' });
+  const { fields, remove } = useFieldArray<ResumeDetailForm>({ name: 'licenses' });
 
   return (
     <S.ResumeLicenseForm>
       {fields.map((field, index) => (
         <div key={field.id} className="license-item">
-          <FormInput<ResumeRegisterForm>
-            name={`licenses.${index}.licenseName`}
-            placeholder="자격증 명"
+          <FormMapSelect<ResumeDetailForm>
+            name={`licenses.${index}.licenseStage`}
+            options={LICENSE_STAGE}
             required
             maxWidth="250px"
-            margin="0 15px 0 0"
+            margin="0 0 15px 0"
           />
           <div className="license-item__stage">
-            {/* <FormDate<ResumeRegisterForm> name={`licenses.${index}.licenseStage`} required width="150px" placeholder="취득일" /> */}
-            <FormMapSelect<ResumeRegisterForm>
-              name={`licenses.${index}.licenseStage`}
-              options={LICENSE_STAGE}
+            <FormInput<ResumeDetailForm>
+              name={`licenses.${index}.licenseName`}
+              placeholder="자격증 명"
               required
-              maxWidth="200px"
+              maxWidth="250px"
               margin="0 15px 0 0"
+              maxLength={15}
             />
-            <ResumeDeleteButton onClick={() => remove(index)} />
+            <RemoveButton onClick={() => remove(index)} />
           </div>
         </div>
       ))}
@@ -43,11 +41,13 @@ const S = {
   ResumeLicenseForm: styled.div`
     .license-item {
       display: flex;
+      flex-direction: column;
+      margin-bottom: 15px;
       &__stage {
         width: 100%;
         display: flex;
         align-items: center;
-        justify-content: space-between;
+        /* justify-content: space-between; */
       }
       ${(props) => props.theme.media.mobile`
         flex-direction: column-reverse;
