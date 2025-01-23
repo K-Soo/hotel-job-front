dbd5a950d50e9a3a92a34801c5980736d84cf9a7
K-Soo <kanabun102@naver.com>
2025-01-23 20:15:08 +0900

    feat: UserResumeDetail 컴포넌트 수정 - 이력서 미리보기 섹션 추가 및 편집 상태에 따른 렌더링 로직 개선

--- src/components/userResumeDetail/index.tsx
+++ src/components/userResumeDetail/index.tsx
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 import { useFormContext, useFieldArray } from 'react-hook-form';
-import { ResumeDetailForm, ResumeRegisterForm, ResumeStatusKey } from '@/types';
+import { LanguageKey, ResumeDetailForm, ResumeRegisterForm, ResumeStatusKey } from '@/types';
 import ResumeSection from '@/components/common/resume/ResumeSection';
 import ResumePolicy from '@/components/common/resume/ResumePolicy';
 import ResumeProfileSection from '@/components/common/resume/ResumeProfileSection';
+import ResumeProfileSectionPreview from '@/components/userResumeDetail/ResumeProfileSectionPreview';
 import ResumeExperienceForm from '@/components/common/resume/ResumeExperienceForm';
 import ResumeLicenseForm from '@/components/common/resume/ResumeLicenseForm';
-import FormInput from '@/components/common/form/FormInput';
-import FormInputB from '@/components/common/form/FormInputB';
 import FormSelect from '@/components/common/form/FormSelect';
 import FormMapSelect from '@/components/common/form/FormMapSelect';
 import FormToggle from '@/components/common/form/FormToggle';
@@ -16,34 +15,38 @@ import FormArea from '@/components/common/form/FormArea';
 import { LANGUAGE, LANGUAGE_LEVEL } from '@/constants/language';
 import useToast from '@/hooks/useToast';
 import ResumeTitleForm from '@/components/userResumeDetail/ResumeTitleForm';
+import ResumePrevuesNavigation from '@/components/userResumeDetail/ResumePrevuesNavigation';
+import ResumeSummarySectionPreview from '@/components/userResumeDetail/ResumeSummarySectionPreview';
+import ResumeExperienceSectionPreview from '@/components/userResumeDetail/ResumeExperienceSectionPreview';
+import ResumeLicenseSectionPreview from '@/components/userResumeDetail/ResumeLicenseSectionPreview';
+import ResumeLanguagesSectionPreview from '@/components/userResumeDetail/ResumeLanguagesSectionPreview';
+import { useResumeContext } from '@/context/ResumeProvider';
 
 interface UserResumeDetailProps {
-  status: ResumeStatusKey;
   children: React.ReactNode;
 }
 
-export default function UserResumeDetail({ status, children }: UserResumeDetailProps) {
+export default function UserResumeDetail({ children }: UserResumeDetailProps) {
+  const { resumeStatus, isEditing } = useResumeContext();
   const { register, watch, setValue, getValues } = useFormContext<ResumeDetailForm>();
   const { fields, remove, append } = useFieldArray<ResumeDetailForm>({ name: 'languages' });
   const { addToast } = useToast();
+
+  const careerLevelValue = watch('careerLevel');
+  const summaryValue = watch('summary');
+  const licensesValue = watch('licenses');
+  const languagesValue = watch('languages');
 
   const handleClickAddLanguage = () => {
     if (fields.length >= 4) {
       return addToast({ message: '최대 4개까지 선택 가능합니다.', type: 'warning' });
     }
-    append({ name: null, level: null });
+    append({ name: null as any, level: null as any });
   };
 
   const handleClickAddLicense = () => {
     const values = getValues('licenses');
-
-    setValue('licenses', [
-      ...values,
-      {
-        licenseName: '',
-        licenseStage: 'FINAL',
-      },
-    ]);
+    setValue('licenses', [...values, { licenseName: '', licenseStage: 'FINAL' }]);
   };
 
   const handleClickExperiences = () => {
@@ -67,54 +70,97 @@ export default function UserResumeDetail({ status, children }: UserResumeDetailP
   return (
     <S.UserResumeDetail>
       <article className="resume-container">
+        <ResumePrevuesNavigation />
+
         <ResumeTitleForm name="title" />
 
-        <ResumeSection title="기본정보" visibleAddButton={false}>
-          <ResumeProfileSection status={status} />
-        </ResumeSection>
+        {isEditing && (
+          <ResumeSection title="기본정보" visibleAddButton={false}>
+            <ResumeProfileSection />
+          </ResumeSection>
+        )}
 
-        <ResumeSection title="간단소개" handleClickAdd={() => {}} visibleAddButton={false} name="summary">
-          <FormArea<ResumeDetailForm> name="summary" maxLength={200} />
-        </ResumeSection>
+        {!isEditing && <ResumeProfileSectionPreview />}
 
-        <ResumeSection title="최종 학력" isRequired={true} handleClickAdd={() => {}} visibleAddButton={false} name="education">
+        {isEditing && (
+          <ResumeSection title="간단소개" visibleAddButton={false} name="summary">
+            <FormArea<ResumeDetailForm> name="summary" maxLength={500} />
+          </ResumeSection>
+        )}
+
+        {!isEditing && summaryValue.length !== 0 && (
+          <ResumeSection title="간단소개" visibleAddButton={false} name="summary">
+            <ResumeSummarySectionPreview text={summaryValue} />
+          </ResumeSection>
+        )}
+
+        <ResumeSection title="최종 학력" isRequired={isEditing ? true : false} visibleAddButton={false} name="education">
           <FormSelect<ResumeDetailForm> name="education" options={resumeEducationLevelOptions} maxWidth="220px" />
         </ResumeSection>
 
-        <ResumeSection title="경력" isRequired={true} handleClickAdd={handleClickExperiences} name="experiences">
-          <ResumeExperienceForm />
-        </ResumeSection>
+        {/* 경력 */}
+        {isEditing && (
+          <ResumeSection
+            title="경력"
+            isRequired={careerLevelValue === 'EXPERIENCED' ? true : false}
+            handleClickAdd={handleClickExperiences}
+            name="experiences"
+            visibleAddButton={isEditing ? true : false}
+          >
+            <ResumeExperienceForm />
+          </ResumeSection>
+        )}
+        {!isEditing && (
+          <ResumeSection title="경력" isRequired={false} name="experiences" visibleAddButton={true}>
+            <ResumeExperienceSectionPreview />
+          </ResumeSection>
+        )}
 
-        <ResumeSection title="자격증" handleClickAdd={handleClickAddLicense} name="licenses">
-          <ResumeLicenseForm />
-        </ResumeSection>
+        {/* 자격증 */}
+        {isEditing && (
+          <ResumeSection title="자격증" handleClickAdd={handleClickAddLicense} name="licenses" visibleAddButton={isEditing ? true : false}>
+            <ResumeLicenseForm />
+          </ResumeSection>
+        )}
+        {!isEditing && licensesValue.length !== 0 && (
+          <ResumeSection title="자격증" name="licenses" visibleAddButton={false}>
+            <ResumeLicenseSectionPreview />
+          </ResumeSection>
+        )}
 
-        <ResumeSection title="외국어" handleClickAdd={handleClickAddLanguage}>
-          {fields.map((field, index) => (
-            <div key={field.id} style={{ display: 'flex' }} className="language-item">
-              <FormMapSelect<ResumeDetailForm>
-                name={`languages.${index}.name`}
-                options={{ '': '선택', ...LANGUAGE }}
-                required
-                maxWidth="180px"
-                margin="0 15px 15px 0"
-              />
-              <FormMapSelect<ResumeDetailForm>
-                name={`languages.${index}.level`}
-                options={{ '': '선택', ...LANGUAGE_LEVEL }}
-                required
-                maxWidth="150px"
-                margin="0 15px 15px 0"
-              />
-            </div>
-          ))}
-        </ResumeSection>
+        {isEditing && (
+          <ResumeSection title="외국어" handleClickAdd={handleClickAddLanguage} visibleAddButton={isEditing ? true : false}>
+            {fields.map((field, index) => (
+              <div key={field.id} style={{ display: 'flex' }} className="language-item">
+                <FormMapSelect<ResumeDetailForm>
+                  name={`languages.${index}.name`}
+                  options={{ '': '선택', ...LANGUAGE }}
+                  required
+                  maxWidth="180px"
+                  margin="0 15px 15px 0"
+                />
+                <FormMapSelect<ResumeDetailForm>
+                  name={`languages.${index}.level`}
+                  options={{ '': '선택', ...LANGUAGE_LEVEL }}
+                  required
+                  maxWidth="120px"
+                  margin="0 15px 15px 0"
+                />
+              </div>
+            ))}
+          </ResumeSection>
+        )}
+        {!isEditing && languagesValue.length !== 0 && (
+          <ResumeSection title="외국어" handleClickAdd={handleClickAddLanguage} visibleAddButton={isEditing ? true : false}>
+            <ResumeLanguagesSectionPreview />
+          </ResumeSection>
+        )}
 
-        <ResumeSection title="병역상태" handleClickAdd={() => {}}>
+        {/* <ResumeSection title="병역상태" >
           <div></div>
-        </ResumeSection>
+          </ResumeSection> */}
 
-        {status !== 'PUBLISH' && <ResumePolicy />}
+        {resumeStatus !== 'PUBLISH' && <ResumePolicy />}
       </article>
       {children}
     </S.UserResumeDetail>
