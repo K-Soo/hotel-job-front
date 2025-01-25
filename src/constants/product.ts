230a936b012c80287ecf45c88f260dcf2983e6cc
K-Soo <kanabun102@naver.com>
2025-01-26 08:26:28 +0900

    feat: 채용 관련 상수 추가 - 제품 유형, 이름 및 설명 정의

---
+++ src/constants/product.ts
@@ -0,0 +1,33 @@
+export const RECRUITMENT_PRODUCT_TYPE = {
+  MAIN: '메인',
+  RECRUIT: '지면',
+};
+
+export const RECRUITMENT_PRODUCT_NAME = {
+  PREMIUM: '프리미엄',
+  SPECIAL: '스페셜',
+  URGENT: '급구',
+  BASIC: '기본',
+};
+
+export const RECRUITMENT_PRODUCT_OPTION_NAME = {
+  LIST_UP: '리스트업',
+  TAG: '태그',
+  HIGHLIGHT: '형광펜',
+  BOLD: '볼드',
+};
+
+export const RECRUITMENT_PRODUCT_DESCRIPTION = {
+  PREMIUM:
+    '확실한 공고 효과를 보장하는 프리미엄 서비스! 메인 페이지 최상단 노출로 최고의 주목도를 제공합니다. 다양한 옵션으로 공고 효과를 극대화하세요.',
+  SPECIAL: '효율적인 비용으로 높은 가성비를 제공하는 스페셜 공고! 메인 페이지와 채용 페이지에서 균형 잡힌 노출을 보장합니다',
+  URGENT: '빠른 채용이 필요하다면 급구 공고! 즉각적인 노출로 짧은 시간 안에 최대의 지원자를 확보하세요.',
+  BASIC: '기본에 충실한 기본 공고! 적절한 비용으로 안정적인 공고 노출을 제공합니다.',
+};
+
+export const RECRUITMENT_PRODUCT_OPTIONS_DESCRIPTION = {
+  LIST_UP: '',
+  HIGHLIGHT: '형광펜 효과로 공고를 강조!',
+  TAG: '인기 태그로 공고를 돋보이게!',
+  BOLD: '볼드효과로 공고를 돋보이게!',
+};
