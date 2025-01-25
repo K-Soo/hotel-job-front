381b4c7bfefdc0280283207c501f1ed19dc86e13
K-Soo <kanabun102@naver.com>
2025-01-26 08:26:06 +0900

    feat: EmployerProductRecruitment 컴포넌트 개선 - 로딩 상태 추가 및 광고 유형에 따른 제목 및 설명 동적 변경

--- src/components/employerProductRecruitment/index.tsx
+++ src/components/employerProductRecruitment/index.tsx
@@ -1,42 +1,58 @@
 import styled from 'styled-components';
-import ChoiceDeviceFilter from '@/components/common/employer/ChoiceDeviceFilter';
 import ProductPreview from '@/components/employerProductRecruitment/ProductPreview';
-import ProductCard from '@/components/employerProductRecruitment/ProductCard';
+import ProductRecruitmentTab from '@/components/employerProductRecruitment/ProductRecruitmentTab';
+import { ParsedUrlQuery } from 'querystring';
+import { ProductRecruitmentQuery } from '@/types/API';
+import { useRouter } from 'next/router';
+
+interface Query extends ParsedUrlQuery {
+  type?: ProductRecruitmentQuery;
+}
 
 interface EmployerProductRecruitmentProps {
+  isLoading: boolean;
   children: React.ReactNode;
 }
 
-export default function EmployerProductRecruitment({ children }: EmployerProductRecruitmentProps) {
+export default function EmployerProductRecruitment({ isLoading, children }: EmployerProductRecruitmentProps) {
+  const router = useRouter();
+  const { type = 'MAIN' } = router.query as Query;
+
   return (
     <S.EmployerProductRecruitment>
-      {children}
+      <ProductRecruitmentTab />
       <S.Header>
-        <h1 className="title">주목받는 지면 광고 - 채용 페이지</h1>
-        <p className="sub-title">대표 채용정보 TOP 3로 가장 효과적인 홍보를 시작하세요. 구직자의 눈길을 사로잡는 최적의 선택!</p>
+        <h1 className="title">{type.toUpperCase() === 'MAIN' ? '채용 성공의 첫걸음 - 메인 페이지' : '주목받는 지면 광고 - 채용 페이지'}</h1>
+        <p className="sub-title">
+          {type.toUpperCase() === 'MAIN'
+            ? '구직자들이 가장 먼저 만나는 채용 공고, 지금 바로 시작하세요. 최고의 노출 효과를 보장합니다!'
+            : '가성비 최고의 채용 광고! 적은 비용으로 더 많은 지원자를 확보하세요. 오늘 시작하는 채용, 내일의 성공을 보장합니다.'}
+        </p>
+        {/* 구직자에게 가장 먼저 보이는 공고! 탁월한 노출 효과로 당신의 채용 공고를 돋보이게 만드세요. */}
       </S.Header>
 
-      <S.ProductFormContainer>
+      <div className="product-container">
         <ProductPreview />
-        <div className="card-container">
-          <ChoiceDeviceFilter margin="0 0 15px 0" />
-          <ProductCard title="프리미엄 공고" margin="0 0 15px 0" />
-          <ProductCard title="급구 공고" margin="0 0 15px 0" />
-          <ProductCard title="기본 공고" />
-        </div>
-      </S.ProductFormContainer>
+        <div className="product-container__list">{children}</div>
+      </div>
     </S.EmployerProductRecruitment>
   );
 }
 
 const S = {
   EmployerProductRecruitment: styled.section`
-    width: 100%;
+    .product-container {
+      display: flex;
+      &__list {
+        width: 100%;
+        margin-left: 30px;
+      }
+    }
   `,
   Header: styled.div`
     margin-bottom: 30px;
     .title {
-      font-size: 30px;
+      font-size: 22px;
       font-weight: 500;
     }
     .sub-title {
@@ -44,14 +60,6 @@ const S = {
       font-size: 16px;
       color: ${(props) => props.theme.colors.gray700};
       font-weight: 500;
-    }
-  `,
-  ProductFormContainer: styled.div`
-    display: flex;
-    justify-content: center;
-    .card-container {
-      flex: 0 1 800px;
-      margin-left: 30px;
     }
   `,
 };
