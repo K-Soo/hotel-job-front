014dc566bed04706028c13ff9bf0810e42b11b97
K-Soo <kanabun102@naver.com>
2025-01-26 08:26:51 +0900

    feat: Employer 컴포넌트에서 EmployerQuickMenu 제거 및 구조 단순화

--- src/components/employer/index.tsx
+++ src/components/employer/index.tsx
@@ -1,14 +1,9 @@
 import styled from 'styled-components';
-import EmployerQuickMenu from '@/components/common/employer/EmployerQuickMenu';
 
 interface EmployerProps {}
 
 export default function Employer({}: EmployerProps) {
-  return (
-    <S.Employer>
-      <EmployerQuickMenu />
-    </S.Employer>
-  );
+  return <S.Employer></S.Employer>;
 }
 
 const S = {
