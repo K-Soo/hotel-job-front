68b5dbf57b74770b7696174cc9cd7684ea89f88d
K-Soo <kanabun102@naver.com>
2025-01-27 00:19:46 +0900

    feat: EmployerProductRecruitment 컴포넌트의 제목 및 부제목 상수화

--- src/components/employerProductRecruitment/index.tsx
+++ src/components/employerProductRecruitment/index.tsx
@@ -9,11 +9,22 @@ interface Query extends ParsedUrlQuery {
   type?: ProductRecruitmentQuery;
 }
 
+const TITLE = {
+  MAIN: '채용 성공의 첫걸음 - 메인 페이지',
+  RECRUIT: '주목받는 지면 광고 - 채용 페이지',
+};
+
+const SUB_TITLE = {
+  MAIN: '구직자들이 가장 먼저 만나는 채용 공고, 지금 바로 시작하세요. 최고의 노출 효과를 보장합니다!',
+  RECRUIT: '가성비 최고의 채용 광고! 적은 비용으로 더 많은 지원자를 확보하세요. 오늘 시작하는 채용, 내일의 성공을 보장합니다.',
+} as const;
+
 interface EmployerProductRecruitmentProps {
   isLoading: boolean;
   children: React.ReactNode;
 }
 
+// TODO  - loading
 export default function EmployerProductRecruitment({ isLoading, children }: EmployerProductRecruitmentProps) {
   const router = useRouter();
   const { type = 'MAIN' } = router.query as Query;
@@ -21,14 +32,10 @@ export default function EmployerProductRecruitment({ isLoading, children }: Empl
   return (
     <S.EmployerProductRecruitment>
       <ProductRecruitmentTab />
+
       <S.Header>
-        <h1 className="title">{type.toUpperCase() === 'MAIN' ? '채용 성공의 첫걸음 - 메인 페이지' : '주목받는 지면 광고 - 채용 페이지'}</h1>
-        <p className="sub-title">
-          {type.toUpperCase() === 'MAIN'
-            ? '구직자들이 가장 먼저 만나는 채용 공고, 지금 바로 시작하세요. 최고의 노출 효과를 보장합니다!'
-            : '가성비 최고의 채용 광고! 적은 비용으로 더 많은 지원자를 확보하세요. 오늘 시작하는 채용, 내일의 성공을 보장합니다.'}
-        </p>
-        {/* 구직자에게 가장 먼저 보이는 공고! 탁월한 노출 효과로 당신의 채용 공고를 돋보이게 만드세요. */}
+        <h1 className="title">{type.toUpperCase() === 'MAIN' ? TITLE.MAIN : TITLE.RECRUIT}</h1>
+        <p className="sub-title">{type.toUpperCase() === 'MAIN' ? SUB_TITLE.MAIN : SUB_TITLE.RECRUIT}</p>
       </S.Header>
 
       <div className="product-container">
