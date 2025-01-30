507dcdb64771a5dd2b136f7e6c32be4b660fdfc4
K-Soo <kanabun102@naver.com>
2025-01-30 20:48:30 +0900

    feat: Summary 컴포넌트 추가 및 이력서 미리보기에서 간단소개 표시 기능 구현

---
+++ src/components/common/resume/resumePreview/Summary.tsx
@@ -0,0 +1,29 @@
+import { ResumeDetail, ResumeDetailForm } from '@/types';
+import styled from 'styled-components';
+
+interface SummaryProps {
+  resumePreviewData: ResumeDetail & ResumeDetailForm;
+}
+
+export default function Summary({ resumePreviewData }: SummaryProps) {
+  return (
+    <S.Summary>
+      <h6 className="title">간단소개</h6>
+      <p>{resumePreviewData.summary}</p>
+    </S.Summary>
+  );
+}
+
+const S = {
+  Summary: styled.div`
+    color: ${({ theme }) => theme.colors.black500};
+    line-height: 1.5;
+    font-size: 15px;
+    margin-bottom: 80px;
+    .title {
+      margin-bottom: 5px;
+      font-size: 18px;
+      font-weight: 500;
+    }
+  `,
+};
