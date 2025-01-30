072218c3876812a9ebd5b91109b4e16a858ad56c
K-Soo <kanabun102@naver.com>
2025-01-30 20:50:42 +0900

    feat: 이미지 도메인 및 원격 패턴 추가하여 외부 이미지 로드 지원

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,22 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
-
+  images: {
+    domains: ['hotel-job-connect.s3.amazonaws.com', 'pds.saramin.co.kr', 'cdn.hotel-job-connect.com'],
+    // loader: 'custom',
+    // unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.hotel-job-connect.com',
+        pathname: '/resources/resume/profile/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd202cjflxkflxp.cloudfront.net',
+      },
+    ],
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -19,6 +34,14 @@ const nextConfig = {
 
     return config;
   },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/v1/upload/:path*',
+  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default withInterceptStdout(nextConfig, (text) => (text.includes('Duplicate atom key') ? '' : text));
