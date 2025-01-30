1d76e2cbaf2299e4bafd4f0310c9cffe9fba59d8
K-Soo <kanabun102@naver.com>
2025-01-30 20:49:49 +0900

    feat: EmployerRecruitmentDetailApplicantContainer에서 ApplicantController 및 ApplicantListContainer 추가

--- src/containers/employerRecruitmentDetailApplicantContainer/index.tsx
+++ src/containers/employerRecruitmentDetailApplicantContainer/index.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import EmployerRecruitmentDetailApplicant from '@/components/EmployerRecruitmentDetailApplicant';
-import SectionTitle from '@/components/common/employer/SectionTitle';
 import ApplicantStatusPanel from '@/components/EmployerRecruitmentDetailApplicant/ApplicantStatusPanel';
 import ApplicantTab from '@/components/EmployerRecruitmentDetailApplicant/ApplicantTab';
-import ApplicantTable from '@/components/EmployerRecruitmentDetailApplicant/ApplicantTable';
-import useFetchQuery from '@/hooks/useFetchQuery';
-import { Get, Patch } from '@/apis';
-import queryKeys from '@/constants/queryKeys';
-import { keepPreviousData } from '@tanstack/react-query';
-import { useRouter } from 'next/router';
+import ApplicantController from '@/components/EmployerRecruitmentDetailApplicant/ApplicantController';
 import { ParsedUrlQuery } from 'querystring';
 import { RecruitmentApplicantQueryStep } from '@/types/API';
-import EmptyComponent from '@/components/common/EmptyComponent';
-import { EMPLOYER_REVIEW_STAGE_STATUS } from '@/constants/application';
-import { EmployerReviewStageStatusKey, ResumeDetail } from '@/types';
-import useToast from '@/hooks/useToast';
-import { useQueryClient } from '@tanstack/react-query';
-import ResumePreview from '@/components/common/resume/ResumePreview';
+import { ResumeDetail } from '@/types';
+import ResumePreview from '@/components/common/resume/resumePreview';
 import ApplicantNoticeForm from '@/components/EmployerRecruitmentDetailApplicant/ApplicantNoticeForm';
+import ApplicantListContainer from '@/containers/employerRecruitmentDetailApplicantContainer/ApplicantListContainer';
 import { ErrorBoundary, ErrorComponent } from '@/error';
+import { Patch } from '@/apis';
+import queryKeys from '@/constants/queryKeys';
+import { useQueryClient } from '@tanstack/react-query';
 
 interface Query extends ParsedUrlQuery {
   step?: RecruitmentApplicantQueryStep;
@@ -27,30 +21,8 @@ interface Query extends ParsedUrlQuery {
 // TODO
 export default function EmployerRecruitmentDetailApplicantContainer() {
   const [resumePreviewData, setResumePreviewData] = React.useState<ResumeDetail | null>(null);
-  const [isOpenNoticeForm, setIsOpenNoticeForm] = React.useState(false);
 
-  const { addToast } = useToast();
-  const router = useRouter();
-  const { slug, step } = router.query as Query;
   const queryClient = useQueryClient();
-
-  const { data, isLoading, isSuccess } = useFetchQuery({
-    queryKey: [queryKeys.RECRUITMENT_LIST, { slug, step }].filter(Boolean),
-    queryFn: Get.getRecruitmentDetailApplicantList,
-    options: {
-      enabled: !!slug,
-      throwOnError: true,
-      staleTime: 60 * 1000 * 1,
-      gcTime: 60 * 1000 * 2,
-      placeholderData: keepPreviousData,
-    },
-    requestQuery: {
-      recruitmentId: slug as string,
-      step: step ? (step.toLocaleUpperCase() as RecruitmentApplicantQueryStep) : undefined,
-    },
-  });
-
-  console.log('지원자 목록 API : ', data);
 
   const handleClickResumePreview = (applicationId: number, isView: boolean, data: ResumeDetail) => {
     setResumePreviewData((prev) => (prev ? null : data));
@@ -58,26 +30,6 @@ export default function EmployerRecruitmentDetailApplicantContainer() {
     if (isView) return;
     fetchApplicationResumeView(applicationId);
   };
-
-  //API - 채용공고 지원자 전형 변경
-  const fetchUpdateEmployerReviewStageStatus = React.useCallback(async (id: number, stage: EmployerReviewStageStatusKey) => {
-    try {
-      const response = await Patch.updateEmployerReviewStageStatus({
-        applicationId: id,
-        stage,
-      });
-      console.log('지원자 전형 변경 API : ', response);
-      if (response.result.status !== 'success') {
-        throw new Error();
-      }
-      await queryClient.invalidateQueries({ queryKey: [queryKeys.RECRUITMENT_LIST], refetchType: 'all' });
-      await queryClient.invalidateQueries({ queryKey: [queryKeys.APPLICATION_RECRUITMENT_STATUS_COUNT], refetchType: 'all' });
-      addToast({ type: 'success', message: '전형 상태가 변경되었습니다.\n 전형 변경은 지원자에게 공개되지않습니다.' });
-    } catch (error) {
-      addToast({ type: 'error', message: '에러가 발생했습니다.' });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   //API - 지원자 이력서 열람처리
   const fetchApplicationResumeView = React.useCallback(async (id: number) => {
@@ -99,24 +51,17 @@ export default function EmployerRecruitmentDetailApplicantContainer() {
   return (
     <>
       {resumePreviewData && <ResumePreview resumePreviewData={resumePreviewData} closeResume={() => setResumePreviewData(null)} />}
-      {isOpenNoticeForm && <ApplicantNoticeForm />}
 
       <EmployerRecruitmentDetailApplicant>
         <ApplicantStatusPanel />
 
         <ApplicantTab />
 
-        <ApplicantTable>
-          <ApplicantTable.Header />
-          {data?.result.length === 0 && <EmptyComponent message="해당 전형에 데이터가 없어요." />}
-          <ApplicantTable.Body
-            data={data?.result}
-            isLoading={isLoading}
-            isSuccess={isSuccess}
-            fetchUpdateEmployerReviewStageStatus={fetchUpdateEmployerReviewStageStatus}
-            handleClickResumePreview={handleClickResumePreview}
-          />
-        </ApplicantTable>
+        <ApplicantController />
+
+        <ErrorBoundary fallback={<ErrorComponent visibleBackButton={false} message="지원자 정보를 가져올 수 없습니다." />}>
+          <ApplicantListContainer handleClickResumePreview={handleClickResumePreview} />
+        </ErrorBoundary>
       </EmployerRecruitmentDetailApplicant>
     </>
   );
