40ad8a37eda41a786e5d39776c73e650192b3f5c
K-Soo <kanabun102@naver.com>
2025-01-30 20:49:44 +0900

    feat: ApplicantNoticeFormContainer 컴포넌트 추가

---
+++ src/containers/employerRecruitmentDetailApplicantContainer/ApplicantNoticeFormContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ApplicantNoticeForm from '@/components/EmployerRecruitmentDetailApplicant/ApplicantNoticeForm';
+
+export default function ApplicantNoticeFormContainer() {
+  return (
+    <>
+      <ApplicantNoticeForm />
+    </>
+  );
+}
