d43dd583a580217c39295ca691a26c7418282c28
K-Soo <kanabun102@naver.com>
2025-01-30 20:48:26 +0900

    feat: License 컴포넌트 추가 및 이력서 미리보기에서 자격증 정보 표시 기능 구현

---
+++ src/components/common/resume/resumePreview/License.tsx
@@ -0,0 +1,41 @@
+import { LICENSE_STAGE } from '@/constants/resume';
+import { ResumeDetail, ResumeDetailForm } from '@/types';
+import styled from 'styled-components';
+
+interface LicenseProps {
+  resumePreviewData: ResumeDetail & ResumeDetailForm;
+}
+
+export default function License({ resumePreviewData }: LicenseProps) {
+  const { licenses } = resumePreviewData;
+  return (
+    <S.License>
+      {licenses.map((license, index) => (
+        <div key={index} className="item">
+          <span className="item__name">{license.licenseName}</span>
+          <span className="item__stage">{LICENSE_STAGE[license.licenseStage]}</span>
+        </div>
+      ))}
+    </S.License>
+  );
+}
+
+const S = {
+  License: styled.div`
+    margin-bottom: 80px;
+    .item {
+      height: 35px;
+      display: flex;
+      align-items: center;
+      width: fit-content;
+      &__name {
+        min-width: 160px;
+        color: ${({ theme }) => theme.colors.black400};
+      }
+      &__stage {
+        color: ${({ theme }) => theme.colors.black900};
+        font-size: 14px;
+      }
+    }
+  `,
+};
