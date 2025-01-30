03a91a9ffe236b4e1e19293056657f442e24f950
K-Soo <kanabun102@naver.com>
2025-01-30 20:49:05 +0900

    feat: ApplicantNoticeForm 컴포넌트에 헤더, 내용 및 버튼 추가

--- src/components/EmployerRecruitmentDetailApplicant/ApplicantNoticeForm.tsx
+++ src/components/EmployerRecruitmentDetailApplicant/ApplicantNoticeForm.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Portal from '@/components/common/Portal';
 import Background from '@/components/common/Background';
+import Button from '@/components/common/style/Button';
 
 interface ApplicantNoticeFormProps {}
 
@@ -8,7 +9,18 @@ export default function ApplicantNoticeForm({}: ApplicantNoticeFormProps) {
   return (
     <Portal>
       <Background>
-        <S.ApplicantNoticeForm>ApplicantNoticeForm</S.ApplicantNoticeForm>
+        <S.ApplicantNoticeForm>
+          <S.Header>xx</S.Header>
+          <S.Content>
+            <div>대상자</div>
+            <div>전형</div>
+            <div>발표 문구</div>
+          </S.Content>
+          <S.Bottom>
+            <Button label="취소" variant="secondary" />
+            <Button label="알림 보내기" variant="primary" margin="0 0 0 15px" />
+          </S.Bottom>
+        </S.ApplicantNoticeForm>
       </Background>
     </Portal>
   );
@@ -20,8 +32,28 @@ const S = {
     position: fixed;
     top: 0;
     right: 0;
-    width: 330px;
+    width: 450px;
     height: 100vh;
     background-color: white;
+    display: flex;
+    flex-direction: column;
+  `,
+  Header: styled.div`
+    height: 50px;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.gray300};
+    padding: 15px;
+  `,
+  Content: styled.div`
+    flex: 1;
+    overflow-y: auto;
+    padding: 15px;
+  `,
+  Bottom: styled.div`
+    border-top: 1px solid ${({ theme }) => theme.colors.gray300};
+    height: 70px;
+    padding: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   `,
 };
