62c797934793ef1ffdb3f3b7bf26c2536c476e72
K-Soo <kanabun102@naver.com>
2025-01-31 21:38:17 +0900

    feat: RecruitSearchPanel을 div로 대체하여 홈 컨테이너 수정

--- src/containers/homeContainer/index.tsx
+++ src/containers/homeContainer/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Home from '@/components/home';
-import RecruitSearchPanel from '@/components/common/RecruitSearchPanel';
 
 export default function HomeContainer() {
   return (
     <Home>
-      <RecruitSearchPanel />
+      <div>asd</div>
     </Home>
   );
 }
