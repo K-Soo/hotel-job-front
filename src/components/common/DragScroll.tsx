8ed02e8b0ac37a42d8c45541c31f9f82dcdbe40c
K-Soo <kanabun102@naver.com>
2025-01-31 21:38:02 +0900

    feat: DragScroll 컴포넌트를 forwardRef로 수정하고 스타일 개선

--- src/components/common/DragScroll.tsx
+++ src/components/common/DragScroll.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 import ScrollContainer from 'react-indiana-drag-scroll';
 
@@ -5,11 +6,19 @@ interface DragScrollProps {
   children: React.ReactNode;
 }
 
-export default function DragScroll({ children }: DragScrollProps) {
-  return <StyledScrollContainer ignoreElements=".ignore-scroll">{children}</StyledScrollContainer>;
-}
+const DragScroll = React.forwardRef<HTMLElement, DragScrollProps>(({ children }, ref) => {
+  return (
+    <StyledScrollContainer innerRef={ref} ignoreElements=".ignore-scroll">
+      {children}
+    </StyledScrollContainer>
+  );
+});
+
+export default DragScroll;
 
 const StyledScrollContainer = styled(ScrollContainer)`
+  width: 100%;
   cursor: grab;
+  scroll-behavior: smooth;
   display: flex;
 `;
