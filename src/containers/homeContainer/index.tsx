f15b6f3c297f4e0d59fd8cce7ead3221a97ab09a
K-Soo <kanabun102@naver.com>
2025-02-02 13:34:48 +0900

    feat(RecruitSearch.tsx):  JobSearch 및 LocationSearch 기능 통합

--- src/containers/homeContainer/index.tsx
+++ src/containers/homeContainer/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Home from '@/components/home';
+import RecruitSearch from '@/components/recruit/recruitSearch';
+import LocationSearch from '@/components/recruit/recruitSearch/LocationSearch';
+import JobSearch from '@/components/recruit/recruitSearch/JobSearch';
 
 export default function HomeContainer() {
   return (
     <Home>
-      <div>asd</div>
+      <RecruitSearch>
+        <JobSearch />
+        <LocationSearch />
+      </RecruitSearch>
     </Home>
   );
 }
