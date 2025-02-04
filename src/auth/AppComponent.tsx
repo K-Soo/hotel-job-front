4bec129a6b58ed5f54eb854e63192b4a06080527
K-Soo <kanabun102@naver.com>
2025-02-04 09:50:48 +0900

    feat(AppComponent): FCM 권한 요청 및 PWA 정보 설정 추가

---
+++ src/auth/AppComponent.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import useAuth from '@/hooks/useAuth';
+import { UAParser } from 'ua-parser-js';
+import useRequestFCMPermission from '@/hooks/useRequestFCMPermission';
+import { isStandalonePWA } from 'ua-parser-js/helpers';
+import { appAtom } from '@/recoil/app';
+import { useSetRecoilState } from 'recoil';
+
+export default function AppComponent() {
+  const setAtomApp = useSetRecoilState(appAtom);
+  const { isAuthenticated } = useAuth();
+
+  // TODO : FCM 토큰 요청 로그인으로
+
+  React.useEffect(() => {
+    const parser = new UAParser();
+    const result = parser.getResult();
+
+    setAtomApp({
+      isPWA: isStandalonePWA(),
+      browser: result?.browser?.name ?? undefined,
+      os: result?.os?.name ?? undefined,
+      device: result?.device?.type ?? undefined,
+      engine: result?.engine?.name ?? undefined,
+    });
+  }, []);
+
+  useRequestFCMPermission({ isAuthenticated });
+
+  return <></>;
+}
