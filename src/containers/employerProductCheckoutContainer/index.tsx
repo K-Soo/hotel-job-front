e3d5926d2d9da50b57b7ccd3b59570d966ca4885
K-Soo <kanabun102@naver.com>
2025-02-04 22:04:57 +0900

    feat(EmployerProductCheckout): EmployerProductCheckout 관련 컴포넌트 및 페이지 삭제

--- src/containers/employerProductCheckoutContainer/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import EmployerProductCheckout from '@/components/employerProductCheckout';
-
-export default function EmployerProductCheckoutContainer() {
-  return <EmployerProductCheckout />;
-}
