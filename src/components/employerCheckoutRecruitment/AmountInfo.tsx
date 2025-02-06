6688c22d6a0dea918b6d1d125c550c2fa3ea64af
K-Soo <kanabun102@naver.com>
2025-02-06 13:49:12 +0900

    feat(AmountInfo): 결제 정보 표시 컴포넌트 추가

---
+++ src/components/employerCheckoutRecruitment/AmountInfo.tsx
@@ -0,0 +1,101 @@
+import { PaymentRecruitmentDetailAmountInfo } from '@/types';
+import styled from 'styled-components';
+import Line from '@/components/common/Line';
+import { priceComma } from '@/utils';
+import SkeletonUI from '@/components/common/SkeletonUI';
+
+interface AmountInfoProps {
+  amountInfo: PaymentRecruitmentDetailAmountInfo | undefined;
+  isLoading: boolean;
+  isSuccess: boolean;
+  children: React.ReactNode;
+}
+
+export default function AmountInfo({ amountInfo, isLoading, isSuccess, children }: AmountInfoProps) {
+  return (
+    <S.AmountInfo>
+      <h2 className="title">결제 정보</h2>
+      <S.AmountContainer>
+        <div className="item">
+          <span className="item__title">주문금액</span>
+          <p>{priceComma(amountInfo?.originalAmount)}원</p>
+        </div>
+
+        <div className="item">
+          <span className="item__title">상품할인</span>
+          <p className="item__discount">
+            <span className="item__discount--text">{amountInfo?.productDiscountAmount === 0 ? '' : '-'}</span>
+            <span className="item__discount--text">{priceComma(amountInfo?.productDiscountAmount)}</span>원
+          </p>
+        </div>
+
+        <div className="item">
+          <span className="item__title">멤버십 할인</span>
+          <p className="item__discount">
+            <span className="item__discount--text">{amountInfo?.membershipDiscountAmount === 0 ? '' : '-'}</span>
+            <span className="item__discount--text">{priceComma(amountInfo?.membershipDiscountAmount)}</span>원
+          </p>
+        </div>
+
+        <div className="item">
+          <span className="item__title">쿠폰 할인</span>
+          <p className="item__discount">
+            <span className="item__discount--text">{amountInfo?.couponDiscountAmount === 0 ? '' : '-'}</span>
+            <span className="item__discount--text">{priceComma(amountInfo?.couponDiscountAmount)}</span>원
+          </p>
+        </div>
+
+        <div className="item">
+          <span className="item__title">최종 할인금액</span>
+          <p className="item__discount">
+            <span className="item__discount--text">-</span>
+            <span className="item__discount--text">{priceComma(amountInfo?.totalDiscountAmount)}</span>원
+          </p>
+        </div>
+
+        <Line margin="20px 0" />
+
+        <div className="total-amount">
+          <span>결제금액</span>
+          <strong>{priceComma(amountInfo?.finalTotalAmount)}원</strong>
+        </div>
+      </S.AmountContainer>
+      {children}
+    </S.AmountInfo>
+  );
+}
+
+const S = {
+  AmountInfo: styled.div`
+    .title {
+      font-size: 20px;
+      font-weight: 600;
+      margin-bottom: 20px;
+    }
+  `,
+  AmountContainer: styled.div`
+    margin-bottom: 30px;
+    .item {
+      display: flex;
+      justify-content: space-between;
+      padding: 5px 0;
+      color: ${(props) => props.theme.colors.black400};
+      font-weight: 500;
+      &__title {
+        color: ${(props) => props.theme.colors.gray700};
+      }
+      &__discount {
+        &--text {
+          color: ${(props) => props.theme.colors.red500};
+          padding-right: 2px;
+        }
+      }
+    }
+    .total-amount {
+      display: flex;
+      justify-content: space-between;
+      font-weight: 600;
+      font-size: 20px;
+    }
+  `,
+};
