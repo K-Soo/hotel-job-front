94ea02b757a5d0b38a08134f2d9d97874439d946
K-Soo <kanabun102@naver.com>
2025-02-06 17:49:05 +0900

    feat(DiscountInfo): 로딩 상태에 따른 결제 예정금액 표시 개선 및 스켈레톤 UI 추가

--- src/components/employerCheckoutRecruitment/DiscountInfo.tsx
+++ src/components/employerCheckoutRecruitment/DiscountInfo.tsx
@@ -2,40 +2,53 @@ import styled from 'styled-components';
 import Button from '@/components/common/style/Button';
 import Line from '@/components/common/Line';
 import { priceComma } from '@/utils';
+import SkeletonUI from '@/components/common/SkeletonUI';
 
 interface DiscountInfoProps {
   finalTotalAmount: number | undefined;
+  isLoading: boolean;
 }
 
-export default function DiscountInfo({ finalTotalAmount }: DiscountInfoProps) {
+export default function DiscountInfo({ finalTotalAmount, isLoading }: DiscountInfoProps) {
   return (
     <S.DiscountInfo>
       <h2 className="title">할인선택</h2>
-      <S.DiscountContainer>
-        <S.DiscountItem>
-          <span className="discount-text">결제 예정금액</span>
-          <div className="discount-content">{priceComma(finalTotalAmount)}원</div>
-        </S.DiscountItem>
+      {isLoading && <SkeletonUI.Line style={{ height: '80px' }} />}
+      {!isLoading && (
+        <S.DiscountContainer>
+          <S.DiscountItem>
+            <span className="discount-text">결제 예정금액</span>
+            <div className="discount-content">{priceComma(finalTotalAmount)}원</div>
+          </S.DiscountItem>
 
-        <Line margin="0" />
+          <Line margin="0" />
 
-        <S.DiscountItem>
-          <span className="discount-text">쿠폰 적용</span>
-          <div className="discount-content">
-            <div className="discount-content__wrapper">
-              <Button label="쿠폰선택" variant="tertiary" height="30px" width="90px" fontSize="14px" disabled={true} margin="0 10px 0 0" />
-              <p>(사용 가능 쿠폰 : 0장 | 보유 쿠폰 : 0장)</p>
+          <S.DiscountItem>
+            <span className="discount-text">쿠폰 적용</span>
+            <div className="discount-content">
+              <div className="discount-content__wrapper">
+                <Button
+                  label="쿠폰선택"
+                  variant="tertiary"
+                  height="30px"
+                  width="90px"
+                  fontSize="14px"
+                  disabled={true}
+                  margin="0 10px 0 0"
+                />
+                <p>(사용 가능 쿠폰 : 0장 | 보유 쿠폰 : 0장)</p>
+              </div>
             </div>
-          </div>
-        </S.DiscountItem>
-      </S.DiscountContainer>
+          </S.DiscountItem>
+        </S.DiscountContainer>
+      )}
     </S.DiscountInfo>
   );
 }
 
 const S = {
   DiscountInfo: styled.div`
-    padding: 15px 30px;
+    padding: 30px;
     .title {
       font-size: 20px;
       font-weight: 600;
