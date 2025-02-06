d763bec61dc3c7e7b96897bc5b29de861c03674f
K-Soo <kanabun102@naver.com>
2025-02-06 20:45:55 +0900

    feat(EmployerCheckout): 채용공고 결제 성공 페이지 레이아웃 및 컨테이너 추가

--- src/pages/employer/checkout/recruitment/success.tsx
+++ src/pages/employer/checkout/recruitment/success.tsx
@@ -1,3 +1,15 @@
-export default function SuccessPage() {
-  return <>success</>;
+import Layout, { EmployerMain, EmployerHeader, EmployerFooter } from '@/components/layout';
+import EmployerCheckoutRecruitmentSuccessContainer from '@/containers/employerCheckoutRecruitmentSuccessContainer';
+export default function EmployerCheckoutRecruitmentSuccessPage() {
+  return <EmployerCheckoutRecruitmentSuccessContainer />;
 }
+
+EmployerCheckoutRecruitmentSuccessPage.getLayout = (page: React.ReactElement) => {
+  return (
+    <Layout>
+      <EmployerHeader />
+      <EmployerMain>{page}</EmployerMain>
+      <EmployerFooter />
+    </Layout>
+  );
+};
