3c995f5b930a6afe1dece21be9cb7bb031d242e6
K-Soo <kanabun102@naver.com>
2025-02-06 17:13:36 +0900

    feat(DateFormat): 게시 가능 기간 계산 기능 추가

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import 'moment-timezone';
+import 'moment/locale/ko';
 
 type DateFormatType =
   | 'YYYY.MM.DD'
@@ -25,5 +26,18 @@ export const dateFormat = {
     const formattedEnd = moment(end, 'HHmm').format('HH:mm');
 
     return `${formattedStart} ~ ${formattedEnd}`;
+  },
+  // 게시 가능 기간 (종료일 23:59:59로 설정)
+  dateRange: (startDate: Date | string, duration: number) => {
+    if (!startDate || duration <= 0) return 'unknown';
+
+    const start = moment(startDate).startOf('day'); // 시작일 00:00:00
+    const end = moment(startDate)
+      .add(duration - 1, 'days') // 종료일 계산 (기간 - 1일)
+      .endOf('day'); // 종료일 23:59:59로 설정
+
+    const formatWithDay = (date: moment.Moment) => date.format('YY.MM.DD(ddd)');
+
+    return `${formatWithDay(start)} ~ ${formatWithDay(end)}`;
   },
 };
