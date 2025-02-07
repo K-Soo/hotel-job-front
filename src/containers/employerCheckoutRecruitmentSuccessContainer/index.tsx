747dff82d1adfc479195d587db9ae3cef8c4b9d3
K-Soo <kanabun102@naver.com>
2025-02-07 12:26:25 +0900

    feat(EmployerCheckout): 결제 승인 요청 후 로딩 상태 및 뒤로가기 버튼 처리 추가

--- src/containers/employerCheckoutRecruitmentSuccessContainer/index.tsx
+++ src/containers/employerCheckoutRecruitmentSuccessContainer/index.tsx
@@ -6,6 +6,9 @@ import { queryKeyChecker } from '@/utils';
 import { Post } from '@/apis';
 import axios from 'axios';
 import { errorMessages } from '@/error';
+import { PaymentRecruitmentConfirmData } from '@/types';
+import SkeletonUI from '@/components/common/SkeletonUI';
+import LoadingOverlay from '@/components/common/LoadingOverlay';
 
 interface Query extends ParsedUrlQuery {
   paymentType?: 'NORMAL';
@@ -16,6 +19,7 @@ interface Query extends ParsedUrlQuery {
 
 export default function EmployerCheckoutRecruitmentSuccessContainer() {
   const [isLoading, setIsLoading] = React.useState(true);
+  const [confirmForm, setConfirmForm] = React.useState<PaymentRecruitmentConfirmData | null>(null);
 
   const router = useRouter();
 
@@ -26,15 +30,19 @@ export default function EmployerCheckoutRecruitmentSuccessContainer() {
     try {
       const response = await Post.paymentRecruitmentConfirm({ orderId, paymentKey, amount });
       console.log('결제 승인요청 API : ', response);
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        const customErrorcode = error.response?.data?.error?.code;
-        const errorMessage = errorMessages[customErrorcode];
-        if (errorMessage) {
-          alert(errorMessage);
-          // window.location.href = '/employer/checkout/recruitment';
-          return;
-        }
+      if (!response.result) {
+        throw new Error();
+      }
+      setConfirmForm(response.result);
+    } catch (error: any) {
+      const customErrorcode = error.response?.data?.error?.code;
+      const errorMessage = errorMessages[customErrorcode];
+
+      // 파악한 에러 코드가 있을경우
+      if (errorMessage) {
+        alert(errorMessage);
+        // window.location.href = '/employer/checkout/recruitment';
+        return;
       }
       alert('결제 승인에 실패했습니다. 다시 시도해주세요.');
       // window.location.href = '/employer/checkout/recruitment';
@@ -60,5 +68,31 @@ export default function EmployerCheckoutRecruitmentSuccessContainer() {
     }
   }, [amount, orderId, paymentKey, paymentType, router]);
 
-  return <EmployerCheckoutRecruitmentSuccess isLoading={isLoading} />;
+  React.useEffect(() => {
+    const handleBackButton = (event: PopStateEvent) => {
+      event.preventDefault();
+      // 홈으로 리다이렉트
+      window.location.replace('/employer/dashboard');
+    };
+
+    // 뒤로 가기 감지
+    window.addEventListener('popstate', handleBackButton);
+
+    return () => {
+      window.removeEventListener('popstate', handleBackButton);
+    };
+  }, []);
+
+  if (isLoading && !confirmForm) {
+    return (
+      <>
+        <div style={{ width: '400px', margin: '50px auto' }}>
+          <LoadingOverlay message="결제 진행중입니다. 잠시만 기다려주세요." />
+          <SkeletonUI.Line style={{ margin: '15px 0', height: '600px' }} />
+        </div>
+      </>
+    );
+  }
+
+  return <EmployerCheckoutRecruitmentSuccess confirmForm={confirmForm} />;
 }
