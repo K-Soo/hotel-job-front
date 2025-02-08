383be3bbda29a43612b1faf3a31993b4ef3797b3
K-Soo <kanabun102@naver.com>
2025-02-08 11:23:45 +0900

    fix(ProductOverview): 결제 상품 정보 표시 개선 및 상태에 따른 스타일 적용

--- src/components/employerRecruitment/ProductOverview.tsx
+++ src/components/employerRecruitment/ProductOverview.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Button from '@/components/common/style/Button';
 import { useRouter } from 'next/router';
 import path from '@/constants/path';
-import { RECRUITMENT_PRODUCT_NAME, RECRUITMENT_PRODUCT_OPTION_NAME } from '@/constants/product';
+import { RECRUITMENT_PRODUCT_NAME, RECRUITMENT_PRODUCT_OPTION_NAME, RECRUITMENT_PRODUCT_TYPE } from '@/constants/product';
 import { dateFormat } from '@/utils';
 interface ProductOverviewProps {
   item: RecruitmentItem;
@@ -14,18 +14,30 @@ export default function ProductOverview({ item }: ProductOverviewProps) {
 
   return (
     <S.ProductOverview>
-      {item.recruitmentStatus === 'PROGRESS' && item.paymentRecruitment && (
-        <S.Progress>
+      {item.paymentRecruitment && (item.recruitmentStatus === 'PROGRESS' || item.recruitmentStatus === 'CLOSED') && (
+        <S.Progress $isClosed={item.recruitmentStatus === 'CLOSED'}>
           <div className="content">
-            <span>{RECRUITMENT_PRODUCT_NAME[item.paymentRecruitment.name]}상품</span>
-            {' | '}
-            {item.postingEndDate && <span>{dateFormat.date(item.postingEndDate, 'YY.MM.DD')} 마감</span>}
+            <div className="content__product">
+              <span>{`(${RECRUITMENT_PRODUCT_TYPE[item.paymentRecruitment.type]})`}</span>
+              <span>{RECRUITMENT_PRODUCT_NAME[item.paymentRecruitment.name]}상품</span>
+            </div>
+
+            <div className="content__options">
+              {item.paymentRecruitment.options.map((option) => (
+                <span className="content__options--item" key={option.id}>
+                  {RECRUITMENT_PRODUCT_OPTION_NAME[option.name]}
+                </span>
+              ))}
+            </div>
+
+            <div className="content__date">
+              {item.postingStartDate && item.postingEndDate && (
+                <span>
+                  {dateFormat.date(item.postingStartDate, 'YY.MM.DD')} ~ {dateFormat.date(item.postingEndDate, 'YY.MM.DD')}
+                </span>
+              )}
+            </div>
           </div>
-          {item.paymentRecruitment.options.map((option) => (
-            <span className="option" key={option.id}>
-              {RECRUITMENT_PRODUCT_OPTION_NAME[option.name]}
-            </span>
-          ))}
         </S.Progress>
       )}
 
@@ -42,8 +54,6 @@ export default function ProductOverview({ item }: ProductOverviewProps) {
         </S.Published>
       )}
 
-      {item.recruitmentStatus === 'CLOSED' && <S.Closed>마감된 공고</S.Closed>}
-
       {item.recruitmentStatus === 'REVIEWING' && <S.Reviewing>확인중</S.Reviewing>}
 
       {item.recruitmentStatus === 'DRAFT' && <S.Draft>-</S.Draft>}
@@ -52,19 +62,37 @@ export default function ProductOverview({ item }: ProductOverviewProps) {
 }
 
 const S = {
-  ProductOverview: styled.div`
-    user-select: none;
-  `,
-  Progress: styled.div`
+  ProductOverview: styled.div``,
+  Progress: styled.div<{ $isClosed: boolean }>`
     .content {
-      font-size: 13px;
-      color: ${(props) => props.theme.colors.black300};
-    }
-    .option {
-      padding-top: 3px;
-      font-size: 11px;
-      padding-right: 3px;
-      color: ${(props) => props.theme.colors.gray700};
+      display: flex;
+      flex-direction: column;
+      color: ${(props) => (props.$isClosed ? props.theme.colors.gray600 : props.theme.colors.black300)};
+      &__product {
+        font-size: 13px;
+      }
+      &__options {
+        &--item {
+          color: ${(props) => props.theme.colors.gray700};
+          font-size: 12px;
+          font-weight: 300;
+          &::after {
+            content: '|';
+            padding: 0 3px;
+          }
+          &:last-child {
+            &::after {
+              content: '';
+            }
+          }
+        }
+      }
+      &__date {
+        margin-top: 2px;
+        font-size: 13px;
+        letter-spacing: 0.2px;
+        font-weight: 300;
+      }
     }
   `,
   Published: styled.div`
