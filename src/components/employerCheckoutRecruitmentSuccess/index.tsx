b6597556214ef5b9499027f6f72a3f07a7eece4c
K-Soo <kanabun102@naver.com>
2025-02-08 11:23:33 +0900

    fix(EmployerCheckoutRecruitmentSuccess): confirmForm 타입을 null에서 필수로 변경하고 메시지 수정

--- src/components/employerCheckoutRecruitmentSuccess/index.tsx
+++ src/components/employerCheckoutRecruitmentSuccess/index.tsx
@@ -6,64 +6,61 @@ import { priceComma } from '@/utils';
 import PercentageProgressBar from '@/components/common/PercentageProgressBar';
 import { useRouter } from 'next/router';
 interface EmployerCheckoutRecruitmentSuccessProps {
-  confirmForm: PaymentRecruitmentConfirmData | null;
+  confirmForm: PaymentRecruitmentConfirmData;
 }
 
 export default function EmployerCheckoutRecruitmentSuccess({ confirmForm }: EmployerCheckoutRecruitmentSuccessProps) {
   const router = useRouter();
 
-  if (confirmForm) {
-    return (
-      <S.EmployerCheckoutRecruitmentSuccess>
-        <div className="success-container">
-          <S.SuccessContainer>
-            <StyledSuccessIcon>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
-                <path d="M9 16.2l-5.2-5.2L3 12.8l6 6 12-12-1.4-1.4-10.6 10.8z" />
-              </svg>
-            </StyledSuccessIcon>
+  return (
+    <S.EmployerCheckoutRecruitmentSuccess>
+      <div className="success-container">
+        <S.SuccessContainer>
+          <StyledSuccessIcon>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
+              <path d="M9 16.2l-5.2-5.2L3 12.8l6 6 12-12-1.4-1.4-10.6 10.8z" />
+            </svg>
+          </StyledSuccessIcon>
 
-            <h2 className="success-text">주문이 완료되었습니다.</h2>
-            <p className="description">공고가 게시되었습니다.</p>
-            <p className="description">공고가 잘 등록되었는지 채용공고 페이지에서 살펴보세요!</p>
+          <h2 className="success-text">주문이 완료되었습니다.</h2>
+          <p className="description">10분 이내 공고가 게시됩니다.</p>
+          <p className="description">공고가 잘 등록되었는지 채용공고 페이지에서 살펴보세요!</p>
 
-            <S.Content>
-              <S.AmountInfo>
-                <div className="order">
-                  <span className="order__text">주문번호</span>
-                  <span className="order__value">{confirmForm.paymentInfo.orderId}</span>
-                </div>
-                <div className="price">
-                  <span className="price__text">결제금액</span>
-                  <span className="price__value">{priceComma(confirmForm.paymentInfo.totalAmount)}원</span>
-                </div>
-              </S.AmountInfo>
+          <S.Content>
+            <S.AmountInfo>
+              <div className="order">
+                <span className="order__text">주문번호</span>
+                <span className="order__value">{confirmForm.paymentInfo.orderId}</span>
+              </div>
+              <div className="price">
+                <span className="price__text">결제금액</span>
+                <span className="price__value">{priceComma(confirmForm.paymentInfo.totalAmount)}원</span>
+              </div>
+              <div className="price">
+                <span className="price__text">포인트 적립</span>
+                <span className="price__value">{priceComma(confirmForm.pointInfo.earnedPoint)}P</span>
+              </div>
+            </S.AmountInfo>
 
-              <S.PointInfo>
-                <span className="text">포인트 적립</span>
-                <strong className="value">10000</strong>
-              </S.PointInfo>
+            <S.Membership>
+              <h2 className="title">멤버십 적립</h2>
+              <PercentageProgressBar membershipInfo={confirmForm.membershipInfo} />
+            </S.Membership>
+          </S.Content>
 
-              <S.Membership>
-                <h2 className="title">멤버십</h2>
-                <PercentageProgressBar membershipInfo={confirmForm.membershipInfo} />
-              </S.Membership>
-            </S.Content>
-
-            <Button
-              label="채용공고 페이지"
-              variant="primary"
-              width="300px"
-              fontSize="16px"
-              margin="80px 0 0 0"
-              borderRadius="30px"
-              onClick={() => router.replace(path.EMPLOYER_RECRUITMENT)}
-            />
-          </S.SuccessContainer>
-        </div>
-      </S.EmployerCheckoutRecruitmentSuccess>
-    );
-  }
+          <Button
+            label="채용공고 페이지"
+            variant="primary"
+            width="300px"
+            fontSize="16px"
+            margin="80px 0 0 0"
+            borderRadius="30px"
+            onClick={() => router.replace(path.EMPLOYER_RECRUITMENT)}
+          />
+        </S.SuccessContainer>
+      </div>
+    </S.EmployerCheckoutRecruitmentSuccess>
+  );
 }
 
 const StyledSuccessIcon = styled.div`
@@ -131,32 +128,11 @@ const S = {
       }
     }
   `,
-  PointInfo: styled.div`
-    padding: 30px 0;
-    display: flex;
-    justify-content: start;
-    .text {
-      display: inline-block;
-      font-size: 20px;
-      width: 100px;
-      color: ${(props) => props.theme.colors.black400};
-    }
-    .value {
-      font-size: 20px;
-      color: ${(props) => props.theme.colors.blue500};
-      &::after {
-        content: 'P';
-        color: ${(props) => props.theme.colors.black400};
-        font-size: 18px;
-        padding-left: 1px;
-      }
-    }
-  `,
   Membership: styled.div`
-    margin-top: 10px;
+    margin-top: 30px;
     .title {
       font-size: 20px;
-      margin-bottom: 5px;
+      margin-bottom: 8px;
       color: ${(props) => props.theme.colors.black500};
       text-align: center;
     }
