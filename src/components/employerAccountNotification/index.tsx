19eb38412d25d729dafa919dc6aea13d71039b36
K-Soo <kanabun102@naver.com>
2025-02-09 22:43:27 +0900

    feat(EmployerAccountNotification): 새로운 고용주 계정 알림 컴포넌트 추가

---
+++ src/components/employerAccountNotification/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface EmployerAccountNotificationProps {}
+
+export default function EmployerAccountNotification({}: EmployerAccountNotificationProps) {
+  return <S.EmployerAccountNotification>EmployerAccountNotification</S.EmployerAccountNotification>;
+}
+
+const S = {
+  EmployerAccountNotification: styled.section``,
+};
