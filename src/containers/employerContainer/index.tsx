4fc1e25cfe2f4bc382338efd4ed1042bea5c3f15
K-Soo <kanabun102@naver.com>
2025-02-10 19:19:42 +0900

    feat(EmployerContainer): 최근 채용 정보 컴포넌트 추가 및 계정 정보 API 연동

--- src/containers/employerContainer/index.tsx
+++ src/containers/employerContainer/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import Employer from '@/components/employer';
+import RecentlyRecruitment from '@/components/employer/RecentlyRecruitment';
+import { Get } from '@/apis';
+import useFetchQuery from '@/hooks/useFetchQuery';
+import queryKeys from '@/constants/queryKeys';
 
 export default function EmployerContainer() {
-  return <Employer />;
+  const { data, isLoading, isSuccess } = useFetchQuery({
+    queryKey: [queryKeys.EMPLOYER_ACCOUNT],
+    queryFn: Get.employerAccountInfo,
+    options: {
+      throwOnError: false,
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+    },
+  });
+
+  console.log('계정정보 API : ', data);
+
+  return (
+    <Employer isLoading={isLoading} data={data?.result}>
+      <RecentlyRecruitment />
+    </Employer>
+  );
 }
