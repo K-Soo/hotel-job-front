87f7687745017a4c33072ff4c8d465231528a8d6
K-Soo <kanabun102@naver.com>
2025-02-10 19:19:54 +0900

    feat(employer): Employer 컴포넌트에 최근 채용, 지원 정보, 복리후생 정보 추가 및 로딩 상태 처리 개선

--- src/components/employer/index.tsx
+++ src/components/employer/index.tsx
@@ -1,11 +1,58 @@
 import styled from 'styled-components';
+import RecentlyRecruitment from '@/components/employer/RecentlyRecruitment';
+import SupportInfo from '@/components/employer/SupportInfo';
+import RecruitmentCount from '@/components/employer/RecruitmentCount';
+import BenefitInfo from '@/components/employer/BenefitInfo';
+import { EmployerAccountInfo } from '@/types';
+import SkeletonUI from '@/components/common/SkeletonUI';
+import CompanyInfo from '@/components/employer/CompanyInfo';
+import CompanyInfoContainer from '@/containers/employerContainer/CompanyInfoContainer';
+interface EmployerProps {
+  isLoading: boolean;
+  data: EmployerAccountInfo | undefined;
+  children: React.ReactNode;
+}
 
-interface EmployerProps {}
+export default function Employer({ isLoading, data }: EmployerProps) {
+  console.log('data: ', data);
+  return (
+    <S.Employer>
+      <S.TopDashboard>
+        {isLoading && <SkeletonUI.Line style={{ height: '180px', flex: '1', borderRadius: '10px' }} />}
+        {data && <RecruitmentCount />}
+        <CompanyInfoContainer certificationStatus={data?.certificationStatus} />
+      </S.TopDashboard>
 
-export default function Employer({}: EmployerProps) {
-  return <S.Employer></S.Employer>;
+      <S.MiddleDashboard>
+        {isLoading && (
+          <>
+            <SkeletonUI.Line style={{ height: '180px', flex: '1', borderRadius: '10px' }} />
+            <SkeletonUI.Line style={{ height: '180px', flexBasis: '500px', borderRadius: '10px' }} />
+          </>
+        )}
+        {!isLoading && data && (
+          <>
+            <SupportInfo data={data} />
+            <BenefitInfo />
+          </>
+        )}
+      </S.MiddleDashboard>
+
+      <RecentlyRecruitment />
+    </S.Employer>
+  );
 }
 
 const S = {
   Employer: styled.section``,
+  TopDashboard: styled.section`
+    display: flex;
+    gap: 30px;
+    margin-bottom: 30px;
+  `,
+  MiddleDashboard: styled.section`
+    display: flex;
+    gap: 30px;
+    margin-bottom: 30px;
+  `,
 };
