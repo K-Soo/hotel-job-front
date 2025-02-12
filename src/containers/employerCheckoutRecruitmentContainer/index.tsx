73ca79e3f8155a56b9b5a9a5d642365fa7b92ad1
K-Soo <kanabun102@naver.com>
2025-02-13 05:36:45 +0900

    feat(employerCheckoutRecruitmentContainer): 결제 위젯 초기화 및 무료 결제 처리 로직 추가

--- src/containers/employerCheckoutRecruitmentContainer/index.tsx
+++ src/containers/employerCheckoutRecruitmentContainer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import EmployerCheckoutRecruitment from '@/components/employerCheckoutRecruitment';
 import { loadTossPayments, TossPaymentsWidgets } from '@tosspayments/tosspayments-sdk';
 import useFetchQuery from '@/hooks/useFetchQuery';
-import { Get } from '@/apis';
+import { Get, Post } from '@/apis';
 import queryKeys from '@/constants/queryKeys';
 import { useRouter } from 'next/router';
 import Button from '@/components/common/style/Button';
@@ -16,18 +16,21 @@ import TossPaymentInfo from '@/components/employerCheckoutRecruitment/TossPaymen
 import DiscountInfo from '@/components/employerCheckoutRecruitment/DiscountInfo';
 import { v4 as uuidv4 } from 'uuid';
 import { RECRUITMENT_PRODUCT_NAME, RECRUITMENT_PRODUCT_TYPE } from '@/constants/product';
+import useAlertWithConfirm from '@/hooks/useAlertWithConfirm';
+import { useQueryClient } from '@tanstack/react-query';
 
 export default function EmployerCheckoutRecruitmentContainer() {
   const [widgets, setWidgets] = React.useState<TossPaymentsWidgets | null>(null);
   const [ready, setReady] = React.useState(false);
+  const [amount, setAmount] = React.useState({ currency: 'KRW', value: 0 });
+
+  const { setAlertWithConfirmAtom } = useAlertWithConfirm();
+  const queryClient = useQueryClient();
+
   const router = useRouter();
   const { query } = router;
-  const [amount, setAmount] = React.useState({
-    currency: 'KRW',
-    value: 0,
-  });
 
-  const { data, isLoading, error } = useFetchQuery({
+  const { data, isLoading, error, refetch, isSuccess, isRefetching } = useFetchQuery({
     queryKey: [queryKeys.PAYMENT_RECRUITMENT_DETAIL, { orderId: query.slug }],
     queryFn: Get.getPaymentRecruitmentDetail,
     options: {
@@ -49,7 +52,7 @@ export default function EmployerCheckoutRecruitmentContainer() {
         value: data.result.amountInfo.TotalAmount,
       });
     }
-  }, [data]);
+  }, [data, isRefetching]);
 
   React.useEffect(() => {
     if (error) {
@@ -78,75 +81,70 @@ export default function EmployerCheckoutRecruitmentContainer() {
     }
   }, [error]);
 
+  // ------  결제위젯 초기화 ------
   React.useEffect(() => {
     async function fetchPaymentWidgets() {
-      // ------  결제위젯 초기화 ------
       const tossPayments = await loadTossPayments(environment.tossClientKey);
-      // 회원 결제
-      const widgets = tossPayments.widgets({
-        customerKey: uuidv4(),
-      });
+
+      const widgets = tossPayments.widgets({ customerKey: uuidv4() });
 
       setWidgets(widgets);
     }
 
-    if (widgets !== null) {
-      return;
-    }
-    if (!data) {
-      return;
-    }
+    if (widgets !== null) return;
+    if (!data) return;
+
     fetchPaymentWidgets();
-  }, [data]);
+  }, [data, widgets]);
 
   React.useEffect(() => {
     async function renderPaymentWidgets() {
-      if (widgets === null) {
-        return;
-      }
-
-      if (!data) {
-        return;
-      }
+      if (widgets === null) return;
+      if (!data) return;
 
       await widgets.setAmount(amount);
 
       await Promise.all([
-        widgets.renderPaymentMethods({
-          selector: '#payment-method',
-          variantKey: 'default',
-        }),
-        widgets.renderAgreement({
-          selector: '#agreement',
-          variantKey: 'AGREEMENT',
-        }),
+        widgets.renderPaymentMethods({ selector: '#payment-method', variantKey: 'default' }),
+        widgets.renderAgreement({ selector: '#agreement', variantKey: 'AGREEMENT' }),
       ]);
+
       setReady(true);
     }
+
+    if (isSuccess && data && !ready) {
+      renderPaymentWidgets();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [widgets, data, ready, isSuccess]);
+
+  React.useEffect(() => {
+    async function renderPaymentWidgets() {
+      if (widgets === null) return;
+      if (!data) return;
+    }
+
     if (data && !ready) {
       renderPaymentWidgets();
     }
   }, [widgets, data, ready]);
 
-  const handlePayment = async () => {
-    if (widgets == null) {
-      return;
-    }
-
-    if (!data) {
-      return;
-    }
+  const fetchConfirmPayment = async () => {
+    if (widgets == null) return;
+    if (!data) return;
 
     try {
-      if (data.result.amountInfo.TotalAmount === 0) {
+      if (data.result.amountInfo.TotalAmount === 0 && data.result.appliedCouponId === null) {
         throw new Error('결제 금액이 0원입니다.');
       }
 
       const productName = data.result.productInfo.name;
       const type = data.result.productInfo.type;
       const optionCount = data.result.productInfo.options.length;
-      const orderName = `${RECRUITMENT_PRODUCT_TYPE[type]}-${RECRUITMENT_PRODUCT_NAME[productName]}`;
+      const orderName = `${RECRUITMENT_PRODUCT_TYPE[type]} ${RECRUITMENT_PRODUCT_NAME[productName]}`;
       const orderOptionName = optionCount > 0 ? ` 외 옵션 ${optionCount}건` : '';
+
+      await widgets.setAmount(amount);
 
       await widgets.requestPayment({
         orderId: data.result.orderId,
@@ -155,8 +153,36 @@ export default function EmployerCheckoutRecruitmentContainer() {
         failUrl: window.location.origin + '/employer/checkout/recruitment/fail',
         customerEmail: data.result.certificationInfo?.managerEmail || 'unknown',
         customerName: data.result.certificationInfo?.userName || 'unknown',
-        // customerMobilePhone: data.result.certificationInfo?.phone || 'unknown', // 가상계좌 안내, 퀵계좌이체
       });
+    } catch (error: any) {
+      console.log('error: ', error.message);
+      if (error.message === '취소되었습니다.') {
+        return;
+      }
+      alert(error.message || '결제에 실패했습니다. 잠시 후 다시 시도해주세요.');
+    }
+  };
+
+  const fetchConfirmFreePayment = async () => {
+    if (widgets == null) return;
+    if (!data) return;
+
+    try {
+      if (data.result.amountInfo.TotalAmount === 0 && data.result.appliedCouponId === null) {
+        throw new Error('결제 금액이 0원입니다.');
+      }
+      const response = await Post.paymentFreeRecruitmentConfirm({ orderId: data.result.orderId, amount: amount.value });
+      console.log('결제 승인 API : ', response);
+      await queryClient.invalidateQueries({ queryKey: [queryKeys.COUPON_LIST], refetchType: 'all' });
+
+      setAlertWithConfirmAtom((prev) => ({
+        ...prev,
+        type: 'CONFIRM',
+        title: 'TITLE_9',
+        subTitle: 'DESC_10',
+        confirmLabel: '확인',
+        onClickConfirm: () => router.push(`${path.EMPLOYER_RECRUITMENT}?status=progress`),
+      }));
     } catch (error: any) {
       console.log('error: ', error.message);
       if (error.message === '취소되었습니다.') {
@@ -170,10 +196,20 @@ export default function EmployerCheckoutRecruitmentContainer() {
     <EmployerCheckoutRecruitment>
       <ProductInfo productInfo={data?.result.productInfo} isLoading={isLoading} />
       <RecruitmentInfo recruitmentInfo={data?.result.recruitmentInfo} isLoading={isLoading} />
-      <DiscountInfo TotalAmount={data?.result.amountInfo.TotalAmount} isLoading={isLoading} />
+      <DiscountInfo
+        TotalAmount={data?.result.amountInfo.TotalAmount}
+        orderId={data?.result.orderId}
+        refetch={refetch}
+        appliedCouponId={data?.result?.appliedCouponId ?? null}
+      />
       <TossPaymentInfo />
       <AmountInfo amountInfo={data?.result.amountInfo} isLoading={isLoading}>
-        <Button label="결제" variant="primary" onClick={handlePayment} isLoading={isLoading || !widgets} />
+        {amount.value !== 0 && (
+          <Button label="결제하기" variant="primary" onClick={fetchConfirmPayment} isLoading={!ready} fontSize="18px" />
+        )}
+        {amount.value === 0 && (
+          <Button label="무료등록" variant="primary" onClick={fetchConfirmFreePayment} isLoading={!ready} fontSize="18px" />
+        )}
       </AmountInfo>
     </EmployerCheckoutRecruitment>
   );
