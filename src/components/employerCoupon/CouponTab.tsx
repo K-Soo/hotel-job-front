e6b5a1ebecd0e3e787d3c4f0cc90affc6d837573
K-Soo <kanabun102@naver.com>
2025-02-13 05:35:00 +0900

    feat(employerCoupon): 쿠폰 탭 기능 추가 및 애니메이션 효과 적용

--- src/components/employerCoupon/CouponTab.tsx
+++ src/components/employerCoupon/CouponTab.tsx
@@ -1,12 +1,52 @@
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import { ParsedUrlQuery } from 'querystring';
+import { motion } from 'framer-motion';
 
-interface CouponTabProps {}
+export interface UrlQuery extends ParsedUrlQuery {
+  use?: 'Y' | 'N';
+}
 
-export default function CouponTab({}: CouponTabProps) {
+export default function CouponTab() {
+  const router = useRouter();
+  const { use = 'N' } = router.query as UrlQuery;
+  const [pathname, params] = router.asPath.split('?');
+
+  const handleClickTab = (value: string) => {
+    const urlSearchParams = new URLSearchParams(params);
+
+    urlSearchParams.set('use', value);
+
+    const paramsObj = Object.fromEntries(urlSearchParams);
+    router.replace(
+      {
+        pathname: pathname,
+        query: paramsObj,
+      },
+      undefined,
+      { scroll: false },
+    );
+  };
+
   return (
     <S.CouponTab>
-      <div>
-        <button>사용가능한 쿠폰</button>
+      <div className="tabs">
+        <motion.button
+          className="tabs__item"
+          onClick={() => handleClickTab('N')}
+          initial={{ color: '#000' }}
+          animate={{ color: use === 'N' ? '#000' : '#999' }}
+        >
+          보유쿠폰
+        </motion.button>
+        <motion.button
+          className="tabs__item"
+          onClick={() => handleClickTab('Y')}
+          initial={{ color: '#000' }}
+          animate={{ color: use === 'Y' ? '#000' : '#999' }}
+        >
+          사용한 쿠폰
+        </motion.button>
       </div>
     </S.CouponTab>
   );
@@ -20,5 +60,15 @@ const S = {
     align-items: center;
     border-bottom: 2px solid ${({ theme }) => theme.colors.black400};
     margin-bottom: 15px;
+    .tabs {
+      &__item {
+        margin-right: 30px;
+        cursor: pointer;
+        font-weight: 500;
+      }
+      :last-child {
+        margin-right: 0;
+      }
+    }
   `,
 };
