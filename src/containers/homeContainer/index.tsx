a2752fedab2de9228046bc15c71c724f84444503
K-Soo <kanabun102@naver.com>
2025-02-13 14:42:16 +0900

    feat(homeContainer): MainSpecialListContainer 및 MainUrgentListContainer 추가 및 오류 경계 처리

--- src/containers/homeContainer/index.tsx
+++ src/containers/homeContainer/index.tsx
@@ -3,6 +3,9 @@ import Home from '@/components/home';
 import RecruitSearch from '@/components/recruit/recruitSearch';
 import LocationSearch from '@/components/recruit/recruitSearch/LocationSearch';
 import JobSearch from '@/components/recruit/recruitSearch/JobSearch';
+import MainSpecialListContainer from '@/containers/homeContainer/MainSpecialListContainer';
+import MainUrgentListContainer from '@/containers/homeContainer/MainUrgentListContainer';
+import { ErrorBoundary, ErrorComponent } from '@/error';
 
 export default function HomeContainer() {
   return (
@@ -11,6 +14,14 @@ export default function HomeContainer() {
         <JobSearch />
         <LocationSearch />
       </RecruitSearch>
+
+      <ErrorBoundary fallback={null}>
+        <MainSpecialListContainer />
+      </ErrorBoundary>
+
+      <ErrorBoundary fallback={null}>
+        <MainUrgentListContainer />
+      </ErrorBoundary>
     </Home>
   );
 }
