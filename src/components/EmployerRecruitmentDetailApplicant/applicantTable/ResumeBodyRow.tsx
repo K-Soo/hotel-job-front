0ac7ea8df0e5ab4427b89cb320fc9695d7fe038a
K-Soo <kanabun102@naver.com>
2025-02-15 19:56:13 +0900

    feat(employerRecruitment): ResumeBodyRow 컴포넌트에 발표 수신자 처리 로직 추가

--- src/components/EmployerRecruitmentDetailApplicant/applicantTable/ResumeBodyRow.tsx
+++ src/components/EmployerRecruitmentDetailApplicant/applicantTable/ResumeBodyRow.tsx
@@ -1,13 +1,44 @@
-import { EmployerReviewStageStatusKey } from '@/types';
+import { AnnouncementType, EmployerReviewStageStatusKey } from '@/types';
 import styled from 'styled-components';
 
 interface ResumeBodyRowProps {
   id: number;
   employerReviewStageStatus: 'DOCUMENT' | 'INTERVIEW' | 'ACCEPT' | 'REJECT';
   fetchUpdateEmployerReviewStageStatus: (id: number, stage: EmployerReviewStageStatusKey) => Promise<void>;
+  announcementRecipients: { id: number; announcement: AnnouncementType }[];
 }
 
-export default function ResumeBodyRow({ id, employerReviewStageStatus, fetchUpdateEmployerReviewStageStatus }: ResumeBodyRowProps) {
+export default function ResumeBodyRow({
+  id,
+  employerReviewStageStatus,
+  announcementRecipients,
+  fetchUpdateEmployerReviewStageStatus,
+}: ResumeBodyRowProps) {
+  if (announcementRecipients.length > 0) {
+    // 불합격 포함될경우
+    const isIncludesRejected = announcementRecipients.some((item) => item.announcement.announcementType === 'REJECT');
+
+    const isIncludesAcceptWithFinalAccept = announcementRecipients.some(
+      (item) => item.announcement.announcementType === 'ACCEPT' && item.announcement.reviewStage === 'ACCEPT',
+    );
+
+    if (isIncludesRejected) {
+      return (
+        <S.ResumeBodyRow>
+          <span>-</span>
+        </S.ResumeBodyRow>
+      );
+    }
+
+    if (isIncludesAcceptWithFinalAccept) {
+      return (
+        <S.ResumeBodyRow>
+          <span>-</span>
+        </S.ResumeBodyRow>
+      );
+    }
+  }
+
   return (
     <S.ResumeBodyRow>
       {employerReviewStageStatus === 'DOCUMENT' && (
