39b4852c8f30be448ef5dfb27b3601d15c92affe
K-Soo <kanabun102@naver.com>
2025-02-15 19:56:22 +0900

    feat(employerRecruitment): StepBodyRow 컴포넌트 추가 및 발표 상태에 따른 버튼 기능 구현

--- src/components/common/EmptyComponent.tsx
+++ src/components/common/EmptyComponent.tsx
@@ -4,13 +4,14 @@ import Image from 'next/image';
 interface EmptyComponentProps {
   message?: string;
   height?: string;
+  isVisibleImage?: boolean;
 }
 
-export default function EmptyComponent({ message, height }: EmptyComponentProps) {
+export default function EmptyComponent({ message, height, isVisibleImage = true }: EmptyComponentProps) {
   return (
     <S.EmptyComponent $height={height}>
       <div className="empty-container">
-        <Image src="/images/box.png" width={60} height={60} alt="box" />
+        {isVisibleImage && <Image className="box-image" src="/images/box.png" width={60} height={60} alt="box" />}
         <span className="text">{message || '등록된 정보가 없습니다.'}</span>
       </div>
     </S.EmptyComponent>
@@ -26,9 +27,11 @@ const S = {
       flex-direction: column;
       align-items: center;
       justify-content: center;
+      .box-image {
+        margin-bottom: 20px;
+      }
       .text {
-        margin-top: 20px;
-        color: ${(props) => props.theme.colors.gray600};
+        color: ${(props) => props.theme.colors.black600};
         font-size: 16px;
         font-weight: 400;
       }
