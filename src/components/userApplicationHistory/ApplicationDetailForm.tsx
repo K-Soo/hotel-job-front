c6fd1c1db4ee7ff801d3165be9976a034a12dd8d
K-Soo <kanabun102@naver.com>
2025-02-19 07:19:05 +0900

    feat(components): 지원서 상세 폼에서 선택된 지원자 변수 이름 변경 및 스타일 수정

--- src/components/userApplicationHistory/ApplicationDetailForm.tsx
+++ src/components/userApplicationHistory/ApplicationDetailForm.tsx
@@ -1,50 +1,49 @@
 import { ApplicationHistory } from '@/types';
 import styled from 'styled-components';
 import { dateFormat } from '@/utils';
-import { APPLICATION_STATUS } from '@/constants/application';
 import { phoneNumberFormat } from '@/utils';
 
 interface ApplicationDetailFormProps {
-  selectedApplicant: ApplicationHistory;
+  selectedApplication: ApplicationHistory;
 }
 
-export default function ApplicationDetailForm({ selectedApplicant }: ApplicationDetailFormProps) {
+export default function ApplicationDetailForm({ selectedApplication }: ApplicationDetailFormProps) {
   return (
     <S.ApplicationDetailForm>
       <S.CompanyInfo>
-        <div className="hotel">{selectedApplicant.recruitmentSnapshot.hotelName}</div>
-        <div className="title">{selectedApplicant.recruitmentSnapshot.title}</div>
+        <div className="hotel">{selectedApplication.recruitmentSnapshot.hotelName}</div>
+        <div className="title">{selectedApplication.recruitmentSnapshot.title}</div>
       </S.CompanyInfo>
 
       <div>
         <S.ContentItem>
           <span className="title">이름</span>
-          <div className="value">{selectedApplicant.resumeSnapshot.name}</div>
+          <div className="value">{selectedApplication.resumeSnapshot.name}</div>
         </S.ContentItem>
 
         <S.ContentItem>
           <span className="title">연락처</span>
-          <div>{phoneNumberFormat(selectedApplicant.resumeSnapshot.phone)}</div>
+          <div>{phoneNumberFormat(selectedApplication.resumeSnapshot.phone)}</div>
         </S.ContentItem>
 
         <S.ContentItem>
           <span className="title">이력서</span>
-          <div>{selectedApplicant.resumeSnapshot.title}</div>
-          {selectedApplicant.resumeSnapshot.isDefault && <StyledDefaultResume>기본</StyledDefaultResume>}
+          <div>{selectedApplication.resumeSnapshot.title}</div>
+          {selectedApplication.resumeSnapshot.isDefault && <StyledDefaultResume>기본</StyledDefaultResume>}
         </S.ContentItem>
 
         <S.ContentItem>
           <span className="title">지원상태</span>
 
           <div className="date-box">
-            <span className="date-box__value">{dateFormat.date(selectedApplicant.applyAt, 'YY.MM.DD')}</span>
+            <span className="date-box__value">{dateFormat.date(selectedApplication.applyAt, 'YY.MM.DD')}</span>
             <span>접수</span>
           </div>
 
-          {selectedApplicant.cancelAt !== null && (
+          {selectedApplication.cancelAt !== null && (
             <div className="date-box cancel">
-              {selectedApplicant.cancelAt !== null && (
-                <span className="date-box__value">{dateFormat.date(selectedApplicant.cancelAt, 'YY.MM.DD')}</span>
+              {selectedApplication.cancelAt !== null && (
+                <span className="date-box__value">{dateFormat.date(selectedApplication.cancelAt, 'YY.MM.DD')}</span>
               )}
               <span>지원취소</span>
             </div>
@@ -53,7 +52,7 @@ export default function ApplicationDetailForm({ selectedApplicant }: Application
 
         <S.ContentItem>
           <span className="title">열람여부</span>
-          <div>{selectedApplicant.isView ? '열람' : '미열람'}</div>
+          <div>{selectedApplication.isView ? '열람' : '미열람'}</div>
         </S.ContentItem>
       </div>
     </S.ApplicationDetailForm>
@@ -72,7 +71,7 @@ const StyledDefaultResume = styled.span`
 
 const S = {
   ApplicationDetailForm: styled.div`
-    margin: 15px;
+    margin: 0 10px;
   `,
   CompanyInfo: styled.div`
     margin-bottom: 15px;
@@ -88,7 +87,7 @@ const S = {
   ContentItem: styled.div`
     display: flex;
     align-items: center;
-    height: 35px;
+    height: 30px;
     font-size: 14px;
     .title {
       min-width: 70px;
