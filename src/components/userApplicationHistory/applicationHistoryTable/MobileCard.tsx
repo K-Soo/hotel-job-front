78faf8b60f66ebfa8cffe1754020592eebd6a8f8
K-Soo <kanabun102@naver.com>
2025-02-19 07:18:37 +0900

    feat(components): 모바일 카드 컴포넌트에서 지원자 리뷰 단계 상태 시각화 및 상태 텍스트 스타일링 개선

--- src/components/userApplicationHistory/applicationHistoryTable/MobileCard.tsx
+++ src/components/userApplicationHistory/applicationHistoryTable/MobileCard.tsx
@@ -1,6 +1,6 @@
 import { APPLICANT_REVIEW_STAGE_STATUS, APPLICATION_STATUS } from '@/constants/application';
 import { ApplicationHistory } from '@/types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { dateFormat } from '@/utils';
 import { motion } from 'framer-motion';
 
@@ -9,7 +9,12 @@ interface MobileCardProps {
   handleClickApplicant: (applicant: ApplicationHistory) => void;
 }
 
+const PASS_STATUSES = ['INTERVIEW', 'INTERVIEW_PASS', 'ACCEPT'];
+
 export default function MobileCard({ item, handleClickApplicant }: MobileCardProps) {
+  const isPass = PASS_STATUSES.includes(item.reviewStageStatus);
+  const isReject = item.reviewStageStatus === 'REJECT';
+
   return (
     <S.MobileCard
       onClick={() => handleClickApplicant(item)}
@@ -18,15 +23,43 @@ export default function MobileCard({ item, handleClickApplicant }: MobileCardPro
       }}
     >
       <h6 className="hotel">{item.recruitmentSnapshot.hotelName}</h6>
+
       <p className="title">{item.recruitmentSnapshot.title}</p>
-      <div className="status">{APPLICANT_REVIEW_STAGE_STATUS[item.reviewStageStatus]}</div>
+
+      <div className="status">
+        <StyledReviewStageStatus $isPass={isPass} $isReject={isReject}>
+          {APPLICANT_REVIEW_STAGE_STATUS[item.reviewStageStatus]}
+        </StyledReviewStageStatus>
+      </div>
+
       <div className="bottom">
-        <div>{APPLICATION_STATUS[item.applicationStatus]}</div>
-        <p className="bottom__date">지원일: {dateFormat.date(item.applyAt, 'YY.MM.DD')}</p>
+        <p className="bottom__apply">
+          <span>{dateFormat.date(item.applyAt, 'YY.MM.DD')}</span>
+          <span>접수</span>
+        </p>
+        {item.applicationStatus === 'CANCELED' && (
+          <p className="bottom__cancel">
+            <span>{dateFormat.date(item.cancelAt, 'YY.MM.DD')}</span>
+            <span>지원취소</span>
+          </p>
+        )}
       </div>
     </S.MobileCard>
   );
 }
+const StyledReviewStageStatus = styled.span<{ $isPass: boolean; $isReject: boolean }>`
+  color: ${(props) => props.theme.colors.black400};
+  ${(props) =>
+    props.$isPass &&
+    css`
+      color: ${(props) => props.theme.colors.blue400};
+    `};
+  ${(props) =>
+    props.$isReject &&
+    css`
+      color: ${(props) => props.theme.colors.red400};
+    `};
+`;
 
 const S = {
   MobileCard: styled(motion.div)`
@@ -35,27 +68,35 @@ const S = {
     border-bottom: 1px solid ${(props) => props.theme.colors.gray200};
     padding: 10px 0;
     .hotel {
-      margin-bottom: 5px;
+      margin-bottom: 4px;
+      font-size: 14px;
     }
     .title {
-      font-size: 14px;
+      font-size: 13px;
       color: ${(props) => props.theme.colors.gray700};
     }
     .status {
+      margin: 10px 0;
       display: flex;
-      justify-content: flex-end;
-      color: ${(props) => props.theme.colors.blue400};
       font-size: 14px;
     }
     .bottom {
       display: flex;
-      justify-content: space-between;
       align-items: flex-end;
-      font-size: 14px;
-      margin-top: 10px;
-      color: ${(props) => props.theme.colors.gray700};
+      justify-content: flex-end;
+      font-size: 13px;
+      color: ${(props) => props.theme.colors.gray600};
       &__date {
-        font-size: 13px;
+      }
+      &__cancel {
+        &::before {
+          content: '';
+          display: inline-block;
+          width: 1px;
+          height: 10px;
+          background-color: ${(props) => props.theme.colors.gray600};
+          margin: 0 10px;
+        }
       }
     }
   `,
