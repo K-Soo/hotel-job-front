277ffb3f2145f88c7e4db99ab51bc5892729156f
K-Soo <kanabun102@naver.com>
2025-02-19 18:14:07 +0900

    feat(components): HistoryStatusContainer에서 불필요한 import 제거

--- src/containers/userApplicationHistoryContainer/HistoryStatusContainer.tsx
+++ src/containers/userApplicationHistoryContainer/HistoryStatusContainer.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import HistoryStatus from '@/components/userApplicationHistory/HistoryStatus';
-import SkeletonUI from '@/components/common/SkeletonUI';
 import useFetchQuery from '@/hooks/useFetchQuery';
 import queryKeys from '@/constants/queryKeys';
 import useAuth from '@/hooks/useAuth';
-import { Get, Patch } from '@/apis';
+import { Get } from '@/apis';
 import { keepPreviousData } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 
