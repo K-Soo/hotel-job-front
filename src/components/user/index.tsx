ac60dc298abd16e80270da642832f00361142b64
K-Soo <kanabun102@naver.com>
2025-02-19 18:14:03 +0900

    feat(components): User 컴포넌트에 UserAsideMenu 추가

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import UserAsideMenu from '@/components/common/user/UserAsideMenu';
+
 interface UserProps {
   children: React.ReactNode;
 }
@@ -6,8 +8,8 @@ interface UserProps {
 export default function User({ children }: UserProps) {
   return (
     <S.User>
+      <UserAsideMenu />
       {children}
-      <div className="container"></div>
     </S.User>
   );
 }
