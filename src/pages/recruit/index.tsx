658244c9c549cc29834f292d9d4e31480e7380b5
K-Soo <kanabun102@naver.com>
2025-02-20 11:26:24 +0900

    fix(RecruitPage): MobileNavigation에 제목 추가

--- src/pages/recruit/index.tsx
+++ src/pages/recruit/index.tsx
@@ -11,7 +11,7 @@ RecruitPage.getLayout = (page: React.ReactElement) => {
     <Layout>
       <Header>
         <DesktopNavigation />
-        <MobileNavigation />
+        <MobileNavigation title="채용" />
       </Header>
       <Main>{page}</Main>
       <Footer />
