96523aebbbb6fee886d062341722c9a620fcd73d
K-Soo <kanabun102@naver.com>
2025-02-21 21:23:30 +0900

    style(RemoveButton): 아이콘 추가 및 버튼 스타일 개선

--- src/components/common/style/RemoveButton.tsx
+++ src/components/common/style/RemoveButton.tsx
@@ -1,13 +1,32 @@
 import styled from 'styled-components';
+import Icon from '@/icons/Icon';
 
 interface RemoveButtonProps {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function RemoveButton({ onClick }: RemoveButtonProps) {
-  return <S.RemoveButton onClick={onClick}>RemoveButton</S.RemoveButton>;
+  return (
+    <S.RemoveButton onClick={onClick}>
+      <Icon name="CloseA24x24" width="20px" height="20px" />
+    </S.RemoveButton>
+  );
 }
 
 const S = {
-  RemoveButton: styled.button``,
+  RemoveButton: styled.button`
+    width: 25px;
+    height: 25px;
+    border-radius: 5px;
+    background-color: ${(props) => props.theme.colors.red100};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+      background-color: ${(props) => props.theme.colors.red200};
+    }
+    svg {
+      color: ${(props) => props.theme.colors.red400};
+    }
+  `,
 };
