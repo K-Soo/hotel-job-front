e92e83a8df1f1f4e175662993bb3a8d958fa17e3
K-Soo <kanabun102@naver.com>
2025-02-26 08:43:22 +0900

    test: push5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function HomePage() {
     <>
       {isMobile && <A2HS />}
       <HomeContainer />
-      <div>
+      {/* <div>
         <p>{JSON.stringify(notificationPermissionStatus)}</p>
         <p>{JSON.stringify(token)}</p>
-      </div>
+      </div> */}
     </>
   );
 }
