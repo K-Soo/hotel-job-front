62b1f1ae12bae8648234bb67f40fff2570a777a4
K-Soo <kanabun102@naver.com>
2025-02-25 09:35:55 +0900

    feat(Firebase): Firebase 메시징 기능 추가 및 토큰 가져오기 함수 구현

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -1,6 +1,6 @@
 import environment from '@/environment';
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging';
+import { getMessaging, isSupported, getToken, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
   apiKey: environment.apiKey,
@@ -11,7 +11,42 @@ const firebaseConfig = {
   appId: environment.appId,
 };
 
-const firebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-const firebaseMessaging = () => getMessaging(firebaseApp);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-export { firebaseApp, firebaseMessaging };
+const messaging = async () => {
+  const supported = await isSupported();
+  return supported ? getMessaging(app) : null;
+};
+
+export async function fetchToken() {
+  try {
+    const fcmMessaging = await messaging();
+
+    if (!fcmMessaging) {
+      throw new Error('is not supported');
+    }
+
+    const token = await getToken(fcmMessaging, { vapidKey: environment.vapiKey });
+    // console.log('FETCHED TOKEN: ', token);
+    if (!token) {
+      throw new Error('token is not exist');
+    }
+    return token;
+  } catch (error: any) {
+    console.error('An error occurred while fetching the token:', error?.message);
+    return null;
+  }
+}
+
+// 🔹 Foreground에서 푸시 알림 수신 (앱이 활성 상태일 때)
+// onMessage(firebaseMessaging(), (payload) => {
+//   console.log('Foreground 푸시 알림 수신: ', payload);
+//   const { notification } = payload;
+
+//   new Notification(notification?.title ?? 'title', {
+//     body: notification?.body ?? 'body',
+//     icon: '/icons/icon-192x192.png',
+//   });
+// });
+
+export { app, messaging };
