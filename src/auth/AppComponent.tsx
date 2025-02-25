b0fa4179f96ae45dc5b71c74fc5ab2da3cb2847d
K-Soo <kanabun102@naver.com>
2025-02-25 09:36:04 +0900

    fix: FCM 토큰 저장 API 경로 수정 및 Recoil 상태 관리 개선

--- src/auth/AppComponent.tsx
+++ src/auth/AppComponent.tsx
@@ -4,10 +4,10 @@ import { UAParser } from 'ua-parser-js';
 import useRequestFCMPermission from '@/hooks/useRequestFCMPermission';
 import { isStandalonePWA } from 'ua-parser-js/helpers';
 import { appAtom } from '@/recoil/app';
-import { useSetRecoilState } from 'recoil';
+import { useSetRecoilState, useRecoilState } from 'recoil';
 
 export default function AppComponent() {
-  const setAtomApp = useSetRecoilState(appAtom);
+  const [appAtomState, setAppAtomState] = useRecoilState(appAtom);
   const { isAuthenticated } = useAuth();
 
   // TODO : FCM 토큰 요청 로그인으로
@@ -15,14 +15,17 @@ export default function AppComponent() {
     const parser = new UAParser();
     const result = parser.getResult();
 
-    setAtomApp({
+    setAppAtomState({
       isPWA: isStandalonePWA(),
       browser: result?.browser?.name ?? undefined,
       os: result?.os?.name ?? undefined,
       device: result?.device?.type ?? undefined,
       engine: result?.engine?.name ?? undefined,
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  console.log('appAtomState: ', appAtomState);
 
   useRequestFCMPermission({ isAuthenticated });
 
