c77b8571f152b8f40ff9c8533d808d7fe8d2691b
K-Soo <kanabun102@naver.com>
2025-03-14 22:22:02 +0900

    feat(RecruitUrgentCard): 강조 및 하이라이트 효과 추가, 태그 조건 수정

--- src/components/recruit/RecruitUrgentCard.tsx
+++ src/components/recruit/RecruitUrgentCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Icon from '@/icons/Icon';
 import RecruitPrice from '@/components/recruit/RecruitPrice';
 import Tag from '@/components/common/Tag';
@@ -10,14 +10,45 @@ import { EXPERIENCE_CONDITION } from '@/constants/recruitment';
 import { addressFormat, employmentTypeFormat } from '@/utils';
 import IconDimmed from '@/components/common/IconDimmed';
 import { useRouter } from 'next/router';
+import useResponsive from '@/hooks/useResponsive';
 
 interface RecruitUrgentCardProps {
   item: RecruitListItem;
 }
 
 export default function RecruitUrgentCard({ item }: RecruitUrgentCardProps) {
+  const [isBold, setIsBold] = React.useState(false);
+  const [isHighlight, setIsHighlight] = React.useState(false);
+  const [isTag, setIsTag] = React.useState(false);
+
   const router = useRouter();
+  const { isTablet } = useResponsive();
   const { sido, sigungu } = addressFormat(item.address);
+
+  const { hasBoldEffect, hasHighlightEffect, hasTagEffect } = React.useMemo(() => {
+    const currentDate = new Date();
+    let hasBoldEffect = false;
+    let hasHighlightEffect = false;
+    let hasTagEffect = false;
+
+    item.paymentRecruitment.options?.forEach((option) => {
+      const postingEndDate = option.postingEndDate ? new Date(option.postingEndDate) : null;
+
+      if (postingEndDate && currentDate <= postingEndDate) {
+        if (option.name === 'BOLD') hasBoldEffect = true;
+        if (option.name === 'HIGHLIGHT') hasHighlightEffect = true;
+        if (option.name === 'TAG') hasTagEffect = true;
+      }
+    });
+
+    return { hasBoldEffect, hasHighlightEffect, hasTagEffect };
+  }, [item.paymentRecruitment]);
+
+  React.useEffect(() => {
+    setIsBold(hasBoldEffect);
+    setIsHighlight(hasHighlightEffect);
+    setIsTag(hasTagEffect);
+  }, [hasBoldEffect, hasHighlightEffect, hasTagEffect]);
 
   const handleClickBlank = (event: React.MouseEvent<HTMLElement>) => {
     event.stopPropagation();
@@ -30,24 +61,21 @@ export default function RecruitUrgentCard({ item }: RecruitUrgentCardProps) {
       whileHover={{ border: '1px solid #b0b8c1' }}
       onClick={() => router.push(`/recruit/${item.id}`)}
     >
-      <S.HeaderBox>
-        <div className="tags">
-          <Tag label="급구" type="URGENT" />
-        </div>
-      </S.HeaderBox>
       <S.ContentBox>
+        <StyledTitle $isBold={isBold} $isHighlight={isHighlight}>
+          {isTag && <Tag label="급구" type="URGENT" width="32px" margin="0 5px 0 0" fontSize="11px" height="17px" />}
+          <h5 className="recruitment-title">{item.recruitmentTitle}</h5>
+        </StyledTitle>
+
         <div className="info-box">
-          <h6 className="info-box__title">{item.recruitmentTitle}</h6>
-          <div className="info-box__detail">
-            <div className="info-box__detail--company">{item.hotelName}</div>
-            <address className="info-box__detail--address">
-              {sido} {sigungu}
-            </address>
-          </div>
+          <div className="info-box__company">{item.hotelName}</div>
+          <address className="info-box__address">
+            {sido} {sigungu}
+          </address>
         </div>
       </S.ContentBox>
 
-      <S.infoBox>
+      <S.JobDetailBox>
         <div className="jobs">
           {item.jobs.length > 1 ? (
             <span className="jobs__text">
@@ -64,28 +92,63 @@ export default function RecruitUrgentCard({ item }: RecruitUrgentCardProps) {
         <div className="price-wrapper">
           <RecruitPrice fonSize="13px" salaryAmount={item.salaryAmount} salary={item.salaryType} />
 
-          <IconDimmed padding="1px" onClick={handleClickBlank}>
-            <Icon className="title__icon" name="ExternalLinkB50x50" width="20px" height="20px" />
-          </IconDimmed>
+          {!isTablet && (
+            <IconDimmed padding="1px" onClick={handleClickBlank}>
+              <Icon className="title__icon" name="ExternalLinkB50x50" width="20px" height="20px" />
+            </IconDimmed>
+          )}
         </div>
-      </S.infoBox>
+      </S.JobDetailBox>
     </S.RecruitUrgentCard>
   );
 }
+
+const StyledTitle = styled.div<{ $isBold: boolean; $isHighlight: boolean }>`
+  margin-bottom: 8px;
+  .recruitment-title {
+    display: inline;
+    line-height: 1.1;
+    vertical-align: bottom;
+    color: ${(props) => props.theme.colors.gray800};
+    font-weight: ${(props) => (props.$isBold ? 600 : 400)};
+    font-size: 14px;
+    width: fit-content;
+    ${(props) =>
+      props.$isHighlight &&
+      css`
+        background-color: #ffee07;
+      `};
+    &:hover {
+      color: ${(props) => props.theme.colors.black};
+    }
+  }
+  ${(props) => props.theme.media.mobile`
+    display: flex;
+    flex-direction: column;
+    .recruitment-title {
+      margin-top: 4px;
+     }
+  `};
+`;
 
 const S = {
   RecruitUrgentCard: styled(motion.div)`
     width: calc(25% - 7.5px);
     aspect-ratio: 4 / 3;
+    max-height: 165px;
     display: flex;
     flex-direction: column;
     padding: 10px;
     border: 1px solid ${(props) => props.theme.colors.gray300};
     cursor: pointer;
+    border-radius: 4px;
     user-select: none;
     ${(props) => props.theme.media.tablet`
       aspect-ratio: 5 / 3;
       width: calc(50% - 5px);
+    `};
+    ${(props) => props.theme.media.mobile`
+      max-height: 220px;
     `};
   `,
   HeaderBox: styled.div`
@@ -96,55 +159,33 @@ const S = {
     margin-bottom: 10px;
     font-weight: 500;
     color: ${(props) => props.theme.colors.gray700};
-    .tags {
-      display: flex;
-      flex-wrap: wrap;
-    }
   `,
   ContentBox: styled.div`
     flex: 1;
     display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     .info-box {
-      flex: 1;
-      &__title {
-        color: ${(props) => props.theme.colors.gray800};
-        font-size: 14px;
-        font-weight: 500;
-        margin-bottom: 10px;
-        overflow: hidden;
-        white-space: nowrap;
-        white-space: break-spaces;
-        display: -webkit-box;
-        word-break: break-all;
-        text-overflow: ellipsis;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 2;
-        ${(props) => props.theme.media.laptop`
-          font-size: 15px;
-        `};
+      /* margin-bottom: 15px; */
+      &__company {
+        font-size: 13px;
+        font-weight: 400;
+        color: ${(props) => props.theme.colors.black100};
+        margin-bottom: 5px;
       }
-      &__detail {
-        display: flex;
-        flex-direction: column;
-        &--company {
-          font-size: 13px;
-          font-weight: 400;
-          color: ${(props) => props.theme.colors.black100};
-          margin-bottom: 5px;
-        }
-        &--address {
-          font-size: 13px;
-          font-weight: 300;
-          color: ${(props) => props.theme.colors.gray700};
-        }
+      &__address {
+        font-size: 13px;
+        font-weight: 300;
+        color: ${(props) => props.theme.colors.gray700};
       }
     }
   `,
-  infoBox: styled.div`
+  JobDetailBox: styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     margin-top: 10px;
+    /* border: 1px solid red; */
     .jobs {
       &__text {
         font-size: 14px;
@@ -161,7 +202,7 @@ const S = {
       }
     }
     .price-wrapper {
-      margin-top: 5px;
+      margin-top: 10px;
       display: flex;
       align-items: center;
       justify-content: space-between;
