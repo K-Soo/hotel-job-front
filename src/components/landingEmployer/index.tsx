57ed36a2a6dfdea8d9bcd578750e178c716c4cbc
K-Soo <kanabun102@naver.com>
2025-03-14 23:33:52 +0900

    feat(LandingEmployer.tsx): 새로운 구성 요소 추가 및 스타일링 개선

--- src/components/landingEmployer/index.tsx
+++ src/components/landingEmployer/index.tsx
@@ -1,57 +1,76 @@
 import styled from 'styled-components';
-import Button from '@/components/common/style/Button';
-import path from '@/constants/path';
-import { useRouter } from 'next/router';
+import Image from 'next/image';
+import RecruitFlow from '@/components/landingEmployer/RecruitFlow';
+import Announcement from '@/components/landingEmployer/Announcement';
+import Benefit from '@/components/landingEmployer/Benefit';
+import DifferencePoint from '@/components/landingEmployer/DifferencePoint';
+import Advantage from '@/components/landingEmployer/Advantage';
 
 export default function LandingEmployer() {
-  const router = useRouter();
-
   return (
     <S.LandingEmployer>
-      <S.Announcement>
-        <div>
-          <p className="desc">훌륭한 인재를 빠르게 채용해 보세요.</p>
-          <p className="desc">첫 채용 공고 무료 등록!</p>
-          <p>지금 바로 회원가입하고 필요한 인재를 찾아보세요.</p>
-          <Button
-            label="지금 무료로 시작하기"
-            variant="primary"
-            width="180px"
-            margin="30px 0 0 0"
-            onClick={() => router.push(path.SIGN_UP)}
-          />
-          {/* <Button label="미리보기" variant="primary" width="180px" margin="30px 0 0 0" onClick={() => router.push(path.EMPLOYER)} /> */}
-        </div>
-      </S.Announcement>
+      <Announcement />
 
-      {/* <S.RecruitFlow>
-        <p>채용 플로우</p>
-      </S.RecruitFlow>
+      <Benefit />
 
-      <S.Advantage>
-        <p>✅ 빠른 채용: 효과적인 채용 공고로 즉시 지원자 확보!</p>
-        <p>✅ 간단한 절차: 누구나 손쉽게 공고 등록 가능!</p>
+      <DifferencePoint />
+
+      <Advantage />
+
+      {/* <S.Advantage>
+        <S.AdvantageItem>
+          <div className="image-box">
+            <Image src="/images/landing/landing1.png" fill alt="landing" quality={100} />
+          </div>
+          <div className="description">
+            <p className="description__text">✅ 빠른 채용: 효과적인 채용 공고로 즉시 지원자 확보!</p>
+            <p className="description__text">✅ 간단한 절차: 누구나 손쉽게 공고 등록 가능!</p>
+          </div>
+        </S.AdvantageItem>
       </S.Advantage> */}
+
+      <RecruitFlow />
     </S.LandingEmployer>
   );
 }
 
 const S = {
-  LandingEmployer: styled.section`
+  LandingEmployer: styled.div`
     width: 100%;
+    height: 100%;
   `,
-  Announcement: styled.article`
-    width: 100%;
-    padding: 100px 0;
+  Advantage: styled.article`
+    padding: 100px 20px;
+    min-height: 500px;
+    position: relative;
+  `,
+  AdvantageItem: styled.div`
     display: flex;
-    align-items: center;
     justify-content: center;
-    margin: 0 auto;
-    .desc {
-      font-size: 32px;
-      padding: 5px 0;
+    /* border: 1px solid red; */
+    .image-box {
+      position: relative;
+      width: 560px;
+      max-height: 380px;
+      height: 100%;
+      aspect-ratio: 16 / 9;
+      /* border: 1px solid red; */
+      margin-right: 30px;
+      font-size: 0;
+      border-radius: 10px;
+      background-color: ${(props) => props.theme.colors.gray100};
+      overflow: hidden;
+      img {
+        /* scale: 1.2; */
+        /* border: 1px solid red; */
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    .description {
+      /* flex: 1; */
+      border: 1px solid red;
     }
   `,
-  RecruitFlow: styled.article``,
-  Advantage: styled.article``,
 };
