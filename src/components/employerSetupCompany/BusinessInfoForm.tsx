a4e9c08f8e7941b94b747d9120bc7b7483485fc2
K-Soo <kanabun102@naver.com>
2025-03-16 03:51:52 +0900

    refactor(BusinessInfoForm): StepHeader 컴포넌트로 헤더 변경 및 스타일 수정

--- src/components/employerSetupCompany/BusinessInfoForm.tsx
+++ src/components/employerSetupCompany/BusinessInfoForm.tsx
@@ -4,9 +4,8 @@ import Button from '@/components/common/style/Button';
 import { SetupCompanyForm } from '@/types';
 import { useSetRecoilState } from 'recoil';
 import { daumPostAtom } from '@/recoil/daumPost';
-import { useRecoilValue } from 'recoil';
-import DaumPost from '@/components/common/DaumPost';
 import React from 'react';
+import StepHeader from '@/components/employerSetupCompany/StepHeader';
 
 interface BusinessInfoFormProps {
   children: React.ReactNode;
@@ -17,9 +16,7 @@ export default function BusinessInfoForm({ children }: BusinessInfoFormProps) {
 
   return (
     <S.BusinessInfoForm>
-      <S.Header>
-        <h2 className="title">사업자 기본 정보</h2>
-      </S.Header>
+      <StepHeader text="사업자 기본정보 입력" />
 
       <FormInput<SetupCompanyForm> required label="대표자명" name="businessOwner" placeholder="대표자" isFocusing errorPosition="static" />
 
@@ -31,7 +28,7 @@ export default function BusinessInfoForm({ children }: BusinessInfoFormProps) {
           placeholder="주소"
           disabled
           errorPosition="static"
-          inputStyle={{ fontSize: '14px', color: 'gray' }}
+          inputStyle={{ fontSize: '13px', color: 'gray' }}
         />
         <Button
           label="검색"
@@ -52,31 +49,14 @@ export default function BusinessInfoForm({ children }: BusinessInfoFormProps) {
 
 const S = {
   BusinessInfoForm: styled.div`
-    max-width: 450px;
-    margin: 0 auto;
     padding: 15px;
     .address-wrapper {
       display: flex;
       align-items: center;
     }
   `,
-  Header: styled.div`
-    margin-bottom: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 100px;
-    padding: 0 15px;
-    border-radius: 8px;
-    background-color: ${(props) => props.theme.colors.gray100};
-    .title {
-      font-size: 18px;
-      font-weight: 500;
-      color: ${(props) => props.theme.colors.gray800};
-    }
-  `,
   ButtonBox: styled.div`
-    margin-top: 30px;
+    margin-top: 60px;
     display: flex;
     justify-content: flex-end;
   `,
