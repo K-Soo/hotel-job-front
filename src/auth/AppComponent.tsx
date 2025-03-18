f1468ca31f529dd74509143a1ef0125654877af7
K-Soo <kanabun102@naver.com>
2025-03-19 00:01:03 +0900

    refactor(auth): useSetRecoilState로 상태 관리 방식 변경 및 FCM 권한 요청 개선

--- src/auth/AppComponent.tsx
+++ src/auth/AppComponent.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { UAParser } from 'ua-parser-js';
 import { isStandalonePWA } from 'ua-parser-js/helpers';
 import { appAtom } from '@/recoil/app';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import useRequestFCMPermission from '@/hooks/useRequestFCMPermission';
 
 export default function AppComponent() {
-  const [appAtomState, setAppAtomState] = useRecoilState(appAtom);
+  const setAppAtomState = useSetRecoilState(appAtom);
 
-  // TODO : FCM 토큰 요청 로그인으로
   React.useEffect(() => {
     const parser = new UAParser();
     const result = parser.getResult();
@@ -23,7 +22,7 @@ export default function AppComponent() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const { notificationPermissionStatus, token } = useRequestFCMPermission();
+  useRequestFCMPermission();
 
   return null;
 }
