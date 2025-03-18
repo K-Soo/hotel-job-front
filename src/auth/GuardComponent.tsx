2d00c0a5d1b6dfc95838b04cc42337d859145f26
K-Soo <kanabun102@naver.com>
2025-03-19 00:01:29 +0900

    refactor(auth): EmployerGuardComponent 제거 및 GuardComponent에 인증 상태 처리 로직 통합

--- src/auth/GuardComponent.tsx
+++ src/auth/GuardComponent.tsx
@@ -11,20 +11,18 @@ interface GuardComponentProps {
 }
 
 export default function GuardComponent({ allowedRoles, children }: GuardComponentProps) {
-  const [showLoading, setShowLoading] = React.useState(false);
   const router = useRouter();
-  const { isAuthFailure, isAuthIdle, isAuthenticated, authAtomState, role } = useAuth();
+  const { isAuthFailure, isAuthIdle, isAuthenticated, authAtomState, role, isUnAuthenticated } = useAuth();
 
   React.useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowLoading(true);
-    }, 500);
+    if (isUnAuthenticated) {
+      alert('잘못된 접근입니다.');
+      router.replace(path.SIGN_IN);
+      return;
+    }
 
-    return () => clearTimeout(timer);
-  }, []);
-
-  React.useEffect(() => {
     if (isAuthFailure) {
+      alert('잘못된 접근입니다.');
       router.replace(path.SIGN_IN);
       return;
     }
@@ -37,22 +35,30 @@ export default function GuardComponent({ allowedRoles, children }: GuardComponen
 
     if (role && !allowedRoles.includes(role)) {
       alert('접근 권한이 없습니다.');
-      router.replace(path.SIGN_IN);
+      router.replace(path.HOME);
     }
 
     // 미 인증 기업정보 미 인증 시 기업정보 등록 페이지로 이동
     if (role === 'EMPLOYER' && authAtomState.companyVerificationStatus === 'NOT_REQUESTED') {
       router.replace(path.EMPLOYER_SETUP_COMPANY);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [allowedRoles, role, authAtomState.companyVerificationStatus]);
+  }, [allowedRoles, role, authAtomState.companyVerificationStatus, isUnAuthenticated, isAuthFailure]);
 
-  if (isAuthIdle && showLoading) {
-    // if (true) {
+  if (!allowedRoles) {
+    return <LoadingOverlay />;
+  }
+
+  if (role && !allowedRoles.includes(role)) {
+    return <LoadingOverlay />;
+  }
+
+  if (isAuthIdle || isUnAuthenticated) {
     return <LoadingOverlay />;
   }
 
   if (isAuthenticated) {
     return <React.Fragment>{children}</React.Fragment>;
   }
+
+  return null;
 }
