a43b13f46a52b483283a1453b52addbfd823984f
K-Soo <kanabun102@naver.com>
2025-03-25 05:56:32 +0900

    update(next-seo.config.ts): 사이트 이름에서 중복된 하이픈 제거 및 구글 메타태그 삭제

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,7 +34,6 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <meta name="naver-site-verification" content="a9a4b611e36279377a48b19d8a7dbc00e300e595" />
-          <meta name="google-site-verification" content="svIZqjG56IVQDB3A5DyMY2Yz9ZiuhIFy3j-dWAyp5SQ" />
           <style />
         </Head>
         <body>
