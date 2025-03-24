bf4e70a0b91eeda9fe2fc2f20c7ec3d64315496f
K-Soo <kanabun102@naver.com>
2025-03-25 04:58:23 +0900

    update(RecruitUrgentListContainer): 중복된 RecruitUrgentCard 렌더링 코드 제거

--- src/containers/recruitContainer/RecruitUrgentListContainer.tsx
+++ src/containers/recruitContainer/RecruitUrgentListContainer.tsx
@@ -74,18 +74,6 @@ export default function RecruitUrgentListContainer() {
             {data.pages.map((page) => {
               return page.result.items.map((item) => <RecruitUrgentCard key={item.id} item={item} />);
             })}
-            {data.pages.map((page) => {
-              return page.result.items.map((item) => <RecruitUrgentCard key={item.id} item={item} />);
-            })}
-            {data.pages.map((page) => {
-              return page.result.items.map((item) => <RecruitUrgentCard key={item.id} item={item} />);
-            })}
-            {data.pages.map((page) => {
-              return page.result.items.map((item) => <RecruitUrgentCard key={item.id} item={item} />);
-            })}
-            {data.pages.map((page) => {
-              return page.result.items.map((item) => <RecruitUrgentCard key={item.id} item={item} />);
-            })}
           </RecruitUrgentList>
           {!isEmptyFirstPage && isFirstPage && nextPage && (
             <div style={{ display: 'flex', justifyContent: 'center' }}>
