327408a10adffd8e7e0ce7415f7bd88ae8b1da29
K-Soo <kanabun102@naver.com>
2025-03-27 02:25:39 +0900

    update(RecruitContainer): 반응형 디자인을 위한 PC 및 모바일 컨테이너 추가

--- src/containers/recruitContainer/index.tsx
+++ src/containers/recruitContainer/index.tsx
@@ -4,42 +4,47 @@ import RecruitFilterPanel from '@/components/recruit/RecruitFilterPanel';
 import RecruitSearch from '@/components/recruit/recruitSearch';
 import LocationSearch from '@/components/recruit/recruitSearch/LocationSearch';
 import JobSearch from '@/components/recruit/recruitSearch/JobSearch';
-import RecruitListContainer from '@/containers/recruitContainer/RecruitListContainer';
 import RecruitUrgentListContainer from '@/containers/recruitContainer/RecruitUrgentListContainer';
 import RecruitSpecialListContainer from '@/containers/recruitContainer/RecruitSpecialListContainer';
+
+import RecruitBasicPcContainer from '@/containers/recruitContainer/RecruitBasicPcContainer';
+import RecruitBasicMobileContainer from '@/containers/recruitContainer/RecruitBasicMobileContainer';
+
 import Line from '@/components/common/Line';
 import { ErrorBoundary, ErrorComponent } from '@/error';
+import useResponsive from '@/hooks/useResponsive';
 
 export default function RecruitContainer() {
+  const { isTablet } = useResponsive();
+
   const handleClickFilterButton = (event: React.MouseEvent<HTMLButtonElement>) => {};
 
   return (
-    <>
-      <Recruit>
-        <RecruitSearch>
-          <JobSearch />
-          {/* <LocationSearch /> */}
-        </RecruitSearch>
+    <Recruit>
+      <RecruitSearch>
+        <JobSearch />
+        {/* <LocationSearch /> */}
+      </RecruitSearch>
 
-        <RecruitFilterPanel handleClickFilterButton={handleClickFilterButton} />
+      <RecruitFilterPanel handleClickFilterButton={handleClickFilterButton} />
 
-        <Line margin="20px 0" color="#e5e8eb" />
+      <Line margin="20px 0" color="#e5e8eb" />
 
-        {/* TODO - ERROR 높이값 */}
-        <ErrorBoundary fallback={null}>
-          <RecruitSpecialListContainer />
-        </ErrorBoundary>
+      {/* TODO - ERROR 높이값 */}
+      <ErrorBoundary fallback={null}>
+        <RecruitSpecialListContainer />
+      </ErrorBoundary>
 
-        {/* TODO - ERROR 높이값 */}
-        <ErrorBoundary fallback={null}>
-          <RecruitUrgentListContainer />
-        </ErrorBoundary>
+      {/* TODO - ERROR 높이값 */}
+      <ErrorBoundary fallback={null}>
+        <RecruitUrgentListContainer />
+      </ErrorBoundary>
 
-        {/* TODO - ERROR 높이값 */}
-        <ErrorBoundary fallback={<ErrorComponent visibleBackButton={false} height="200px" />}>
-          <RecruitListContainer />
-        </ErrorBoundary>
-      </Recruit>
-    </>
+      {/* TODO - ERROR 높이값 */}
+      <ErrorBoundary fallback={<ErrorComponent visibleBackButton={false} height="200px" />}>
+        {!isTablet && <RecruitBasicPcContainer />}
+        {isTablet && <RecruitBasicMobileContainer />}
+      </ErrorBoundary>
+    </Recruit>
   );
 }
