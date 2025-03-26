7aec277c0f5ae7979b8f5e83cc4dddb536cee629
K-Soo <kanabun102@naver.com>
2025-03-27 02:21:39 +0900

    feat(ScrollToTop): 스크롤 위치에 따라 상단으로 이동 버튼 추가

---
+++ src/components/layout/ScrollToTop.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import { AnimatePresence, motion, useMotionValueEvent, useScroll } from 'framer-motion';
+import Icon from '@/icons/Icon';
+
+export function ScrollToTop() {
+  const [visible, setVisible] = React.useState(false);
+  console.log('visible: ', visible);
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    setVisible(latest > 200);
+  });
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <AnimatePresence>
+      {visible && (
+        <S.ScrollToTop
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          onClick={scrollToTop}
+        >
+          <Icon name="ArrowToTop24x24" width="28px" height="28px" />
+        </S.ScrollToTop>
+      )}
+    </AnimatePresence>
+  );
+}
+
+const S = {
+  ScrollToTop: styled(motion.div)`
+    position: fixed;
+    bottom: 95px;
+    right: 15px;
+    z-index: 15;
+    padding: 10px;
+    border-radius: 50%;
+    background-color: white;
+    color: white;
+    cursor: pointer;
+    width: 50px;
+    height: 50px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    ${(props) => props.theme.media.tablet`
+      width: 40px;
+      height: 40px;
+    `};
+  `,
+};
