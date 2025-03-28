f369c2ac0ff388465ef830af14da95f4b4137b4f
K-Soo <kanabun102@naver.com>
2025-03-29 03:35:58 +0900

    update(pretendard): 폰트 디스플레이 속성을 'swap'으로 설정

--- src/fonts/pretendard.ts
+++ src/fonts/pretendard.ts
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
   variable: '--font-pretendard',
+  display: 'swap',
 });
 
 export default pretendard;
