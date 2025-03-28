f7356c10ef8624ce2f3224104e13b2b07f7c7a37
K-Soo <kanabun102@naver.com>
2025-03-29 03:35:19 +0900

    update(MainTopPanel): 메인 상단 패널 컴포넌트 추가 및 레이아웃 정의

---
+++ src/components/home/MainTopPanel.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import JobCategory from '@/components/home/JobCategory';
+import Member from '@/components/home/Member';
+import EmployerBenefitBanner from '@/components/common/banner/EmployerBenefitBanner';
+import WageBanner from '@/components/common/banner/WageBanner';
+import JobSearch from '@/components/recruit/recruitSearch/JobSearch';
+import RecruitSearch from '@/components/recruit/recruitSearch';
+
+export default function MainTopPanel() {
+  return (
+    <article className="bg-gray-30 px-[15px] py-[35px] lg:px-0">
+      <RecruitSearch>
+        <JobSearch />
+        {/* <LocationSearch /> */}
+      </RecruitSearch>
+
+      <div className="mx-auto flex w-full max-w-[1024px] flex-col gap-[20px] md:flex-row">
+        <JobCategory />
+        <div className="flex shrink-0 grow-1 flex-col gap-[10px] md:gap-[20px]">
+          <div className="flex flex-1 gap-[20px]">
+            <WageBanner />
+
+            <Member />
+          </div>
+
+          <EmployerBenefitBanner />
+        </div>
+      </div>
+    </article>
+  );
+}
