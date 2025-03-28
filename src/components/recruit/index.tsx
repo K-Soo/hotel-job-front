d7a3569dd38f8fc3cfd5becc115164e281ac3f35
K-Soo <kanabun102@naver.com>
2025-03-29 03:35:29 +0900

    update(Recruit): 패딩 속성을 수정하여 레이아웃 개선

--- src/components/recruit/index.tsx
+++ src/components/recruit/index.tsx
@@ -12,9 +12,9 @@ const S = {
   Recruit: styled.section`
     height: 100px;
     ${(props) => props.theme.media.laptop`
-      padding: 0 15px;
+      padding: 15px;
     `};
-    ${(props) => props.theme.media.tablet`
+    ${(props) => props.theme.media.mobile`
       background-color: ${props.theme.colors.gray50};
     `};
   `,
