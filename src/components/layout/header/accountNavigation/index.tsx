93c5716c72279253e94c6ede9d40fbba2c61d600
K-Soo <kanabun102@naver.com>
2025-04-02 02:20:43 +0900

    add(AccountNavigation): 새로운 계정 네비게이션 컴포넌트 추가

---
+++ src/components/layout/header/accountNavigation/index.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import Logo from '@/components/common/Logo';
+import { motion } from 'framer-motion';
+import path from '@/constants/path';
+import { useRouter } from 'next/router';
+
+interface AccountNavigationProps {}
+
+export function AccountNavigation({}: AccountNavigationProps) {
+  const router = useRouter();
+
+  return (
+    <S.AccountNavigation>
+      <Logo size="small" />
+      <S.HelpButton
+        whileHover={{ border: '1px solid #4593fc', color: '#4593fc' }}
+        whileTap={{ scale: 0.99 }}
+        onClick={() => router.push(path.HELP__NOTICE)}
+      >
+        고객센터
+      </S.HelpButton>
+    </S.AccountNavigation>
+  );
+}
+
+const S = {
+  AccountNavigation: styled.div`
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1024px;
+    margin: 0 auto;
+    padding: 0 15px;
+  `,
+  HelpButton: styled(motion.button)`
+    border-radius: 30px;
+    border: 1px solid ${(props) => props.theme.colors.blue600};
+    color: ${(props) => props.theme.colors.blue600};
+    height: 30px;
+    padding: 0 15px;
+    font-size: 14px;
+    cursor: pointer;
+  `,
+};
