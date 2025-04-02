4cddecfd75d8f6c9735cae07c70d5551c77a0445
K-Soo <kanabun102@naver.com>
2025-04-03 02:04:29 +0900

    update(HomePage): 이메일 인증 완료 메시지 수신 기능 추가 및 테스트 버튼 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import Layout, { Header, Footer, Main, DesktopNavigation, MobileNavigation, ScrollToTopButton } from '@/components/layout';
 import HomeContainer from '@/containers/homeContainer';
 import BottomNavigation from '@/components/layout/BottomNavigation';
@@ -7,10 +8,29 @@ import useResponsive from '@/hooks/useResponsive';
 export default function HomePage() {
   const { isMobile } = useResponsive();
 
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      console.log('event: ', event);
+
+      if (event.data.status === '@@@@') {
+        alert('이메일 인증이 완료되었습니다!');
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    return () => window.removeEventListener('message', handleMessage);
+  }, []);
+
+  const handleTest = () => {
+    window.open('/test', '_blank');
+  };
+
   return (
     <>
       {isMobile && <A2HS />}
       <HomeContainer />
+      <button onClick={() => handleTest()}>asad</button>
     </>
   );
 }
