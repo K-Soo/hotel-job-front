11ce6c7169104a99bb582b4bd80d0afb84d34ec8
K-Soo <kanabun102@naver.com>
2025-04-05 16:35:15 +0900

    update(ResumeProfileSection): 프로필 섹션 구성 요소 개선 및 불필요한 코드 제거

--- src/components/common/resume/ResumeProfileSection.tsx
+++ src/components/common/resume/ResumeProfileSection.tsx
@@ -1,42 +1,42 @@
 import styled from 'styled-components';
 import ResumeUploadProfileImageForm from '@/components/common/resume/ResumeUploadProfileImageForm';
 import FormInput from '@/components/common/form/FormInput';
-import FormMapSelect from '@/components/common/form/FormMapSelect';
 import Button from '@/components/common/style/Button';
 import { ResumeDetailForm } from '@/types';
 import { useSetRecoilState } from 'recoil';
 import { daumPostAtom } from '@/recoil/daumPost';
-import { SEX_CODE } from '@/constants';
-import { CAREER_LEVEL } from '@/constants/resume';
+import ResumeTitleForm from '@/components/userResumeDetail/ResumeTitleForm';
+import FormChipsRadio from '@/components/common/form/FormChipsRadio';
+// import FormMapSelect from '@/components/common/form/FormMapSelect';
+// import { LOCAL_CODE, SEX_CODE } from '@/constants';
+// import { CAREER_LEVEL } from '@/constants/resume';
+// import ResumeFormTitle from '@/components/common/resume/ResumeFormTitle';
+// import Profile from './resumePreview/Profile';
 
 export default function ResumeProfileSection() {
   const setDaumPostState = useSetRecoilState(daumPostAtom);
 
   return (
     <S.ResumeProfileSection>
-      <article className="profile-form">
-        <div className="profile-form__inner">
-          <S.NameBox>
-            <FormInput<ResumeDetailForm>
-              name="name"
-              maxWidth="150px"
-              readOnly={false}
-              errorPosition="absolute"
-              placeholder="이름"
-              label="이름"
-              margin="0 15px 0 0"
-            />
-            <FormMapSelect<ResumeDetailForm>
-              name="careerLevel"
-              required
-              maxWidth="130px"
-              label="경력여부"
-              options={{ '': '선택', ...CAREER_LEVEL }}
-            />
-          </S.NameBox>
+      <ResumeTitleForm name="title" />
 
+      <S.ProfileInfo>
+        <ResumeUploadProfileImageForm<ResumeDetailForm> name="profileImage" />
+
+        <div className="detail">
+          <div className="mb-[20px]">
+            <FormChipsRadio value={'NEWBIE'} name="careerLevel" label={'신입'} margin="0 15px 0 0" palette="blue" />
+            <FormChipsRadio value={'EXPERIENCED'} name="careerLevel" label={'경력'} margin="0 15px 0 0" palette="blue" />
+          </div>
+
+          <div className="flex flex-col md:flex-row">
+            <FormInput<ResumeDetailForm> name="name" placeholder="이름" label="이름" required margin="0 15px 0 0" />
+            <FormInput<ResumeDetailForm> name="email" label="이메일" maxLength={30} required placeholder="이메일" />
+          </div>
+
+          {/* 
           <S.CertBox>
-            {/* <FormMapSelect<ResumeDetailForm>
+            <FormMapSelect<ResumeDetailForm>
               name="localCode"
               options={LOCAL_CODE}
               required
@@ -44,7 +44,7 @@ export default function ResumeProfileSection() {
               disabled
               label="내국인"
               margin="0 15px 0 0"
-            /> */}
+            />
 
             <FormMapSelect<ResumeDetailForm>
               name="sexCode"
@@ -66,67 +66,48 @@ export default function ResumeProfileSection() {
               disabled
               mask={'9999-99-99'}
             />
-          </S.CertBox>
+          </S.CertBox> */}
 
-          <FormInput<ResumeDetailForm>
-            label="휴대폰"
-            name="phone"
-            maxWidth="190px"
-            readOnly={false}
-            mask="999-9999-9999"
-            errorPosition="static"
-            disabled
-          />
+          <div className="flex">
+            <FormInput<ResumeDetailForm> label="연락처" name="phone" mask="999-9999-9999" errorPosition="static" disabled />
+          </div>
 
-          <FormInput<ResumeDetailForm> name="email" label="이메일" maxWidth="295px" maxLength={30} />
+          <div className="flex items-center">
+            <FormInput<ResumeDetailForm> label="주소" name="address" placeholder="주소" disabled required />
+            <Button
+              variant="primary"
+              label="검색"
+              width="90px"
+              height="40px"
+              onClick={() => setDaumPostState({ isOpen: true })}
+              margin="0 0 0 15px"
+            />
+          </div>
+          <FormInput<ResumeDetailForm> name="addressDetail" placeholder="상세주소" />
         </div>
-
-        <ResumeUploadProfileImageForm<ResumeDetailForm> name="profileImage" />
-      </article>
-
-      <S.AddressBox>
-        <div className="address">
-          <FormInput<ResumeDetailForm> label="주소" name="address" maxWidth="350px" errorPosition="static" placeholder="주소" disabled />
-          <Button
-            variant="primary"
-            label="검색"
-            width="90px"
-            height="40px"
-            onClick={() => setDaumPostState({ isOpen: true })}
-            margin="0 0 0 15px"
-          />
-        </div>
-        <FormInput<ResumeDetailForm> name="addressDetail" maxWidth="350px" errorPosition="absolute" placeholder="상세주소" />
-      </S.AddressBox>
+      </S.ProfileInfo>
     </S.ResumeProfileSection>
   );
 }
 
 const S = {
-  ResumeProfileSection: styled.div`
-    .profile-form {
-      display: flex;
-      ${(props) => props.theme.media.mobile`
-        flex-direction: column-reverse;
-      `};
-      &__inner {
-        flex: 1;
-      }
-    }
-  `,
-  NameBox: styled.div`
+  ResumeProfileSection: styled.div``,
+  ProfileInfo: styled.div`
     display: flex;
-    margin-bottom: 15px;
-  `,
-  AddressBox: styled.div`
-    margin-top: 15px;
-    .address {
-      display: flex;
+    width: 100%;
+    .detail {
+      flex: 1;
+      margin-left: 50px;
+      width: 100%;
+    }
+    ${(props) => props.theme.media.mobile`
+      flex-direction: column;
       align-items: center;
-    }
-  `,
-  CertBox: styled.div`
-    display: flex;
-    margin-bottom: 15px;
+      .detail {
+        flex: 1;
+        margin-top: 30px;
+        margin-left: 0;
+      }
+    `};
   `,
 };
