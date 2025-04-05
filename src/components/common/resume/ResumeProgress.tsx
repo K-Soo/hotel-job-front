efd82db81dcfd4be0653e5260adbd1ca0a307334
K-Soo <kanabun102@naver.com>
2025-04-05 16:35:18 +0900

    update(ResumeProgress): 이력서 목록으로 돌아가는 버튼 추가 및 스타일 개선

--- src/components/common/resume/ResumeProgress.tsx
+++ src/components/common/resume/ResumeProgress.tsx
@@ -1,25 +1,27 @@
 import styled from 'styled-components';
 import Button from '@/components/common/style/Button';
 import { useResumeContext } from '@/context/ResumeProvider';
+import Icon from '@/icons/Icon';
+import { useRouter } from 'next/router';
+import path from '@/constants/path';
+
 interface ResumeProgressProps {
   handleClickPreview: () => void;
 }
 
 export default function ResumeProgress({ handleClickPreview }: ResumeProgressProps) {
   const { isEditing } = useResumeContext();
+  const router = useRouter();
 
   return (
     <S.ResumeProgress>
       {/* {isEditing && <div className="progress-container"></div>} */}
-      <Button
-        height="45px"
-        label="이력서 미리보기"
-        variant="tertiary"
-        disabled={false}
-        type="button"
-        margin="15px 0 0 0"
-        onClick={handleClickPreview}
-      />
+      <button className="back" onClick={() => router.push(path.USER_RESUME)}>
+        <Icon name="ArrowLeft24x24" width="20px" height="20px" margin="0 8px 0 0" />
+        <span>이력서 목록</span>
+      </button>
+
+      <Button height="50px" label="이력서 미리보기" variant="tertiary" disabled={false} type="button" onClick={handleClickPreview} />
       {/* <Button height="45px" label="임시저장" variant="tertiary" disabled={false} type="button" margin="10px 0 0 0" /> */}
     </S.ResumeProgress>
   );
@@ -30,11 +32,26 @@ const S = {
     position: sticky;
     top: 80px;
     width: 250px;
-    margin-left: 30px;
     height: 50vh;
+    margin-left: 50px;
     ${(props) => props.theme.media.tablet`
       display: none;
     `};
+    .back {
+      height: 50px;
+      margin-bottom: 10px;
+      border-radius: 8px;
+      width: 100%;
+      padding: 10px;
+      display: flex;
+      align-items: center;
+      font-size: 18px;
+      cursor: pointer;
+      color: ${(props) => props.theme.colors.gray700};
+      &:hover {
+        color: ${(props) => props.theme.colors.black};
+      }
+    }
     .progress-container {
       height: 300px;
       border-radius: 8px;
