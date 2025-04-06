3eeb3a6033eb2a875a0bb9390f9f6bf41325bb9b
K-Soo <kanabun102@naver.com>
2025-04-06 13:40:49 +0900

    update(EmployerProductRecruitmentContainer): 쿼리 파라미터 제거 및 프리미엄 제품 필터링 로직 수정

--- src/containers/employerProductRecruitmentContainer/index.tsx
+++ src/containers/employerProductRecruitmentContainer/index.tsx
@@ -3,28 +3,17 @@ import EmployerProductRecruitment from '@/components/employerProductRecruitment'
 import queryKeys from '@/constants/queryKeys';
 import { Get } from '@/apis';
 import useFetchQuery from '@/hooks/useFetchQuery';
-import { ParsedUrlQuery } from 'querystring';
-import { ProductRecruitmentQuery } from '@/types/API';
-import { useRouter } from 'next/router';
 import ProductCard from '@/components/employerProductRecruitment/ProductCard';
 import { keepPreviousData } from '@tanstack/react-query';
 import ProductRecruitmentSideMenuContainer from '@/containers/employerProductRecruitmentContainer/ProductRecruitmentSideMenuContainer';
 import SkeletonUI from '@/components/common/SkeletonUI';
-import ProductRecruitmentTab from '@/components/employerProductRecruitment/ProductRecruitmentTab';
-
-interface Query extends ParsedUrlQuery {
-  type?: ProductRecruitmentQuery;
-}
 
 // TODO - 메인 프리미엄 상품
 export default function EmployerProductRecruitmentContainer() {
   const [isOpenSideMenu, setIsOpenSideMenu] = React.useState(false);
 
-  const router = useRouter();
-  const { type = 'MAIN' } = router.query as Query;
-
   const { data, isLoading, isSuccess } = useFetchQuery({
-    queryKey: [queryKeys.PRODUCT_RECRUITMENT, { type }],
+    queryKey: [queryKeys.PRODUCT_RECRUITMENT],
     queryFn: Get.getProductRecruitmentList,
     options: {
       throwOnError: true,
@@ -32,15 +21,11 @@ export default function EmployerProductRecruitmentContainer() {
       gcTime: 1000 * 60 * 10,
       placeholderData: keepPreviousData,
     },
-    requestQuery: {
-      type: type.toUpperCase() as ProductRecruitmentQuery,
-    },
   });
 
   if (isLoading) {
     return (
       <section>
-        <ProductRecruitmentTab />
         <div style={{ display: 'flex' }}>
           <SkeletonUI.RecruitMentProductPreview />
           <SkeletonUI.RecruitMentProductList />
@@ -55,7 +40,6 @@ export default function EmployerProductRecruitmentContainer() {
         {isOpenSideMenu && <ProductRecruitmentSideMenuContainer setIsOpenSideMenu={setIsOpenSideMenu} />}
         <EmployerProductRecruitment>
           {data.result.map((product) => {
-            if (product.name === 'PREMIUM') return;
             return <ProductCard key={product.id} product={product} setIsOpenSideMenu={setIsOpenSideMenu} />;
           })}
         </EmployerProductRecruitment>
