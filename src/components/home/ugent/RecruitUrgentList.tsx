47dd0d88c48f3efc0128566ae71a09f462c0392a
K-Soo <kanabun102@naver.com>
2025-04-07 20:27:39 +0900

    update(RecruitUrgentList): 그리드 레이아웃 및 반응형 스타일 수정

--- src/components/home/ugent/RecruitUrgentList.tsx
+++ src/components/home/ugent/RecruitUrgentList.tsx
@@ -11,24 +11,34 @@ export default function RecruitUrgentList({ children }: RecruitUrgentListProps) 
 const S = {
   RecruitUrgentList: styled.div`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-auto-rows: minmax(180px, auto);
+    grid-template-columns: repeat(5, 1fr);
     margin-bottom: 100px;
     gap: 1px;
     ${(props) => props.theme.media.laptop`
       margin: 0 15px 100px 15px;
     `};
-    @media (max-width: 800px) {
+    @media (max-width: 1024px) {
+      grid-auto-rows: minmax(180px, auto);
+      grid-template-columns: repeat(4, 1fr);
+    }
+
+    @media (max-width: 900px) {
+      grid-template-columns: repeat(4, 1fr);
+      grid-auto-rows: minmax(205px, auto);
+    }
+    @media (max-width: 768px) {
       grid-template-columns: repeat(3, 1fr);
+      grid-auto-rows: minmax(205px, auto);
     }
 
     @media (max-width: 600px) {
       grid-template-columns: repeat(2, 1fr);
-      grid-auto-rows: minmax(160px, auto);
+      grid-auto-rows: minmax(190px, auto);
     }
 
     @media (max-width: 400px) {
-      grid-template-columns: repeat(2, 1fr);
+      grid-template-columns: repeat(1, 1fr);
+      grid-auto-rows: minmax(180px, auto);
     }
   `,
 };
