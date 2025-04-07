2e48b8450bb3dd5f970c1c8a169a61ec98696988
K-Soo <kanabun102@naver.com>
2025-04-08 01:58:51 +0900

    update(RecruitDetailDateTime): 등록일 및 수정일 표시 방식 개선, 스타일 조정

--- src/components/recruitDetail/RecruitDetailDateTime.tsx
+++ src/components/recruitDetail/RecruitDetailDateTime.tsx
@@ -5,16 +5,42 @@ interface RecruitDetailDateTimeProps {}
 export default function RecruitDetailDateTime({}: RecruitDetailDateTimeProps) {
   return (
     <S.RecruitDetailDateTime>
-      <span>등록 24.12.12</span>
-      {/* <span>수정일</span> */}
+      <span className="create">24.12.12</span>
+      <span className="update">2024.12.12</span>
     </S.RecruitDetailDateTime>
   );
 }
 
 const S = {
   RecruitDetailDateTime: styled.div`
-    text-align: right;
-    font-size: 12px;
-    color: ${(props) => props.theme.colors.black600};
+    font-size: 13px;
+    color: ${(props) => props.theme.colors.black800};
+    ${(props) => props.theme.media.tablet`
+      display: none;
+    `};
+    .create {
+      &::before {
+        content: '등록일';
+        padding-right: 4px;
+        font-weight: 300;
+        color: ${(props) => props.theme.colors.gray500};
+      }
+      &::after {
+        content: '';
+        display: inline-block;
+        width: 1px;
+        height: 10px;
+        background-color: ${({ theme }) => theme.colors.gray400};
+        margin: 0 8px;
+      }
+    }
+    .update {
+      &::before {
+        content: '수정일';
+        padding-right: 4px;
+        font-weight: 300;
+        color: ${(props) => props.theme.colors.gray500};
+      }
+    }
   `,
 };
