621f088570316d0827044328b8c245db0e03f74a
K-Soo <kanabun102@naver.com>
2025-04-08 11:03:05 +0900

    add(SlideRecruitPanel): SlideRecruitPanel 컴포넌트 추가

---
+++ src/components/recruitDetail/SlideRecruitPanel.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+interface SlideRecruitPanelProps {}
+
+export default function SlideRecruitPanel({}: SlideRecruitPanelProps) {
+  return <S.SlideRecruitPanel>SlideRecruitPanel</S.SlideRecruitPanel>;
+}
+
+const S = {
+  SlideRecruitPanel: styled.div``,
+};
