c61285b868b41006a1e153680b2bcfe9d85168fe
K-Soo <kanabun102@naver.com>
2025-04-09 14:43:18 +0900

    update(AppComponent): 불필요한 코드 제거 및 FCM 권한 요청 유지

--- src/auth/AppComponent.tsx
+++ src/auth/AppComponent.tsx
@@ -1,27 +1,6 @@
-import React from 'react';
-import { UAParser } from 'ua-parser-js';
-import { isStandalonePWA } from 'ua-parser-js/helpers';
-import { appAtom } from '@/recoil/app';
-import { useSetRecoilState } from 'recoil';
 import useRequestFCMPermission from '@/hooks/useRequestFCMPermission';
 
 export default function AppComponent() {
-  const setAppAtomState = useSetRecoilState(appAtom);
-
-  React.useEffect(() => {
-    const parser = new UAParser();
-    const result = parser.getResult();
-
-    setAppAtomState({
-      isPWA: isStandalonePWA(),
-      browser: result?.browser?.name ?? undefined,
-      os: result?.os?.name ?? undefined,
-      device: result?.device?.type ?? undefined,
-      engine: result?.engine?.name ?? undefined,
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   useRequestFCMPermission();
 
   return null;
